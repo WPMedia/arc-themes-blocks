07f4d96eb05f54c88417c8d71ef074c5930ae52e
Brent Miller <brent.miller@washport.com>
2019-08-13 15:50:54 -0500

    Updated headline block

--- blocks/headline-block/features/headline/default.jsx
+++ blocks/headline-block/features/headline/default.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import Theme from 'fusion:themes';
 import './headline.scss';
 
-export default ({headline}) => {
-  return (headline && headline.length > 0) ? (
-    <h1 className={Theme.headline}>{headline}</h1>
-  ) : null;
+export default ({children}) => {
+  return  (
+    <h1 className="headline">{children}</h1>
+  );
 };
-

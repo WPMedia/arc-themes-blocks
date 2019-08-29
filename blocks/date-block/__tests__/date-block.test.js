18638fb4d5df6a004016589c43d4882e4a157e6b
SangHee <sangheekim22@gmail.com>
2019-08-29 12:22:50 -0400

    PEN-257 Adding unit tests

--- blocks/date-block/__tests__/date-block.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const dateBlock = require('../features/date/default');
-
-describe('@arc-test-org/date-block', () => {
-    it('needs tests');
-});
--- blocks/date-block/features/date/default.jsx
+++ blocks/date-block/features/date/default.jsx
@@ -1,13 +1,6 @@
-'use strict';
-
 import React, { Component } from 'react';
 import Consumer from 'fusion:consumer';
-
-function cleanTimeString(timeString) {
-  return timeString
-    .replace('PM', 'p.m.')
-    .replace('AM', 'a.m.');
-}
+import './date.scss';
 
 @Consumer
 class ArticleDate extends Component {
@@ -16,35 +9,31 @@ class ArticleDate extends Component {
 
     // Inherit global content
     const { globalContent: content } = this.props;
+
     const { display_date: dateString } = content;
 
     // Convert the time to browser's local time using the ECMAScript Internationalization API
     // Browser support found here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
-    const displayDate = (dateString && Date.parse(dateString))
-      ? new Date(dateString).toLocaleDateString(
-        'default',
-        {
-          dateStyle: 'long',
-          timeStyle: 'long',
-        },
-      )
+    const displayDate = (dateString && Date.parse(dateString)) // check if it's a valid time string
+      ? new Date(dateString)
+        .toLocaleString('default', {
+          year: 'numeric',
+          day: 'numeric',
+          month: 'long',
+          hour: 'numeric',
+          minute: 'numeric',
+          timeZoneName: 'short',
+        }).replace(/(,)(.*?)(,)/, '$1$2 at')
+        .replace('PM', 'p.m.')
+        .replace('AM', 'a.m.')
       : '';
 
     this.state = { displayDate };
   }
 
-  componentDidMount() {
-    const { displayDate } = this.state;
-    const updatedDate = cleanTimeString(displayDate);
-
-    this.setState({
-      displayDate: updatedDate,
-    });
-  }
-
   render() {
     const { displayDate } = this.state;
-    
+
     return (
       <div key={displayDate} className="date">
         {displayDate}

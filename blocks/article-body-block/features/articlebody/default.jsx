2db13d6dfed6275b13edf9124fc3c95212d50dd7
Joseph Grosspietsch <joe.grosspietsch@washpost.com>
2019-10-11 16:00:36 -0500

    Changed all fusion:theme usage to getThemeStyle (#17)

--- blocks/article-body-block/features/articlebody/default.jsx
+++ blocks/article-body-block/features/articlebody/default.jsx
@@ -3,18 +3,21 @@
 /* eslint-disable react/no-typos */
 import React, { Component } from 'react';
 import Consumer from 'fusion:consumer';
-import Theme from 'fusion:themes';
+import { getThemeStyle } from 'fusion:themes';
 import PropTypes from 'prop-types';
 import './articlebody.scss';
 import Body from './render';
 
-
 @Consumer
 class ArticleBody extends Component {
   render() {
-    const { globalContent: content, customFields } = this.props;
+    const { globalContent: content, customFields, arcSite } = this.props;
     return (
-      <Body data={content} params={customFields} correctionTitle={Theme.correctionTitle} />
+      <Body
+        data={content}
+        params={customFields}
+        correctionTitle={getThemeStyle(arcSite).correctionTitle}
+      />
     );
   }
 }
--- blocks/header-nav-block/features/navigation/default.test.jsx
+++ blocks/header-nav-block/features/navigation/default.test.jsx
@@ -1,25 +1,24 @@
-const React = require('react');
-const { shallow } = require('enzyme');
+import React from 'react';
+import { shallow } from 'enzyme';
+import { getThemeStyle } from 'fusion:themes';
+import Navigation from './default';
 
-jest.mock('fusion:themes', () => ({}));
+jest.mock('fusion:themes', () => ({ getThemeStyle: jest.fn(() => ({})) }));
 
 describe('the header navigation feature for the default output type', () => {
   it('should be a nav element', () => {
-    const { default: Navigation } = require('./default');
     const wrapper = shallow(<Navigation />);
 
     expect(wrapper.at(0).type()).toBe('nav');
   });
 
   it('should contain two buttons', () => {
-    const { default: Navigation } = require('./default');
     const wrapper = shallow(<Navigation />);
 
     expect(wrapper.find('nav > button')).toHaveLength(2);
   });
 
   it('should contain a logo image', () => {
-    const { default: Navigation } = require('./default');
     const wrapper = shallow(<Navigation />);
 
     expect(wrapper.find('nav > img')).toHaveLength(1);
@@ -27,14 +26,12 @@ describe('the header navigation feature for the default output type', () => {
 
   describe('the search button', () => {
     it('should have an image that imports the search icon', () => {
-      const { default: Navigation } = require('./default');
       const wrapper = shallow(<Navigation />);
 
       expect(wrapper.find('nav > button.nav-search > img')).toHaveProp('src', 'search.svg');
     });
 
     it('should have the default alt text', () => {
-      const { default: Navigation } = require('./default');
       const wrapper = shallow(<Navigation />);
 
       expect(wrapper.find('nav > button.nav-search > img')).toHaveProp('alt', 'Navigation bar search');
@@ -43,14 +40,12 @@ describe('the header navigation feature for the default output type', () => {
 
   describe('the sections button', () => {
     it('should contain a span with "Sections" text', () => {
-      const { default: Navigation } = require('./default');
       const wrapper = shallow(<Navigation />);
 
       expect(wrapper.find('nav > button.nav-sections > span')).toHaveText('Sections');
     });
 
     it('should contain a image that imports the hamburger icon', () => {
-      const { default: Navigation } = require('./default');
       const wrapper = shallow(<Navigation />);
 
       expect(wrapper.find('nav > button.nav-sections > img')).toHaveProp('src', 'hamburger.svg');
@@ -60,18 +55,16 @@ describe('the header navigation feature for the default output type', () => {
   describe('the navigation bar image/logo', () => {
     describe('when the theme manifest provides a logo url', () => {
       it('should make the src of the logo the provided image', () => {
-        jest.mock('fusion:themes', () => ({ primaryLogo: 'my-nav-logo.svg' }));
-        const { default: Navigation } = require('./default');
+        getThemeStyle.mockImplementation(() => ({ primaryLogo: 'my-nav-logo.svg' }));
         const wrapper = shallow(<Navigation />);
 
         expect(wrapper.find('nav > img')).toHaveProp('src', 'my-nav-logo.svg');
       });
     });
 
-    describe('when the theme does not provide a logo ur', () => {
+    describe('when the theme does not provide a logo url', () => {
       it('should make the src of the logo the placeholder image', () => {
-        jest.mock('fusion:themes', () => ({}));
-        const { default: Navigation } = require('./default');
+        getThemeStyle.mockImplementation(() => ({}));
         const wrapper = shallow(<Navigation />);
 
         expect(wrapper.find('nav > img')).toHaveProp('src', 'arc-placeholder-logo.svg');
@@ -80,8 +73,7 @@ describe('the header navigation feature for the default output type', () => {
 
     describe('when the theme manifest provides alt text', () => {
       it('should make the alt text of the logo the provided text', () => {
-        jest.mock('fusion:themes', () => ({ primaryLogoAlt: 'my alt text' }));
-        const { default: Navigation } = require('./default');
+        getThemeStyle.mockImplementation(() => ({ primaryLogoAlt: 'my alt text' }));
         const wrapper = shallow(<Navigation />);
 
         expect(wrapper.find('nav > img')).toHaveProp('alt', 'my alt text');
@@ -90,8 +82,7 @@ describe('the header navigation feature for the default output type', () => {
 
     describe('when the theme manifest does not provide alt text', () => {
       it('should make the alt text of the logo the default text', () => {
-        jest.mock('fusion:themes', () => ({}));
-        const { default: Navigation } = require('./default');
+        getThemeStyle.mockImplementation(() => ({}));
         const wrapper = shallow(<Navigation />);
 
         expect(wrapper.find('nav > img')).toHaveProp('alt', 'Navigation bar logo');

9b1cc7cf098b0ed527e6e54d8b89ab007674b644
SangHee <sangheekim22@gmail.com>
2019-10-15 09:31:29 -0500

    PEN-341 Remove custom fields from tests

--- blocks/headline-block/features/headline/default.test.jsx
+++ blocks/headline-block/features/headline/default.test.jsx
@@ -6,70 +6,7 @@ describe('the headline feature for the default output type', () => {
     jest.resetModules();
   });
 
-  describe('when "includeHeadline" is set to true', () => {
-    describe('when headline content from globalContent is present', () => {
-      beforeEach(() => {
-        jest.mock('fusion:context', () => ({
-          useComponentContext: jest.fn(() => ({
-            globalContent: {
-              headlines: {
-                basic: 'headline for our story',
-              },
-            },
-          })),
-        }));
-      });
-
-      it('should render an h1', () => {
-        const { default: Headline } = require('./default');
-        const wrapper = shallow(<Headline customFields={{ includeHeadline: true }} />);
-
-        expect(wrapper.at(0).type()).toBe('h1');
-        expect(wrapper.at(0).hasClass('headline')).toBe(true);
-      });
-
-      it('should dangerously set the innerHTML to the headline content', () => {
-        const { default: Headline } = require('./default');
-        const wrapper = shallow(<Headline customFields={{ includeHeadline: true }} />);
-
-        expect(wrapper.at(0).prop('dangerouslySetInnerHTML')).toStrictEqual({ __html: 'headline for our story' });
-      });
-    });
-
-    describe('when headline content from globalContent is NOT present', () => {
-      beforeEach(() => {
-        jest.mock('fusion:context', () => ({
-          useComponentContext: jest.fn(() => ({
-            globalContent: {},
-          })),
-        }));
-      });
-
-      it('should render nothing', () => {
-        const { default: Headline } = require('./default');
-        const wrapper = shallow(<Headline customFields={{ includeHeadline: true }} />);
-
-        expect(wrapper).toBeEmptyRender();
-      });
-    });
-  });
-
-  describe('when "includeHeadline" is set to false', () => {
-    beforeEach(() => {
-      jest.mock('fusion:context', () => ({
-        useComponentContext: jest.fn(() => ({})),
-      }));
-    });
-
-    it('should render nothing', () => {
-      const { default: Headline } = require('./default');
-      const wrapper = shallow(<Headline customFields={{ includeHeadline: false }} />);
-
-      expect(wrapper).toBeEmptyRender();
-    });
-  });
-
-  describe('when "includeHeadline" is not set', () => {
+  describe('when headline content from globalContent is present', () => {
     beforeEach(() => {
       jest.mock('fusion:context', () => ({
         useComponentContext: jest.fn(() => ({
@@ -82,11 +19,36 @@ describe('the headline feature for the default output type', () => {
       }));
     });
 
+    it('should render an h1', () => {
+      const { default: Headline } = require('./default');
+      const wrapper = shallow(<Headline />);
+
+      expect(wrapper.at(0).type()).toBe('h1');
+      expect(wrapper.at(0).hasClass('headline')).toBe(true);
+    });
+
+    it('should dangerously set the innerHTML to the headline content', () => {
+      const { default: Headline } = require('./default');
+      const wrapper = shallow(<Headline />);
+
+      expect(wrapper.at(0).prop('dangerouslySetInnerHTML')).toStrictEqual({ __html: 'headline for our story' });
+    });
+  });
+
+  describe('when headline content from globalContent is NOT present', () => {
+    beforeEach(() => {
+      jest.mock('fusion:context', () => ({
+        useComponentContext: jest.fn(() => ({
+          globalContent: {},
+        })),
+      }));
+    });
+
     it('should render nothing', () => {
       const { default: Headline } = require('./default');
-      const wrapper = shallow(<Headline customFields={{}} />);
+      const wrapper = shallow(<Headline />);
 
-      expect(wrapper).not.toBeEmptyRender();
+      expect(wrapper).toBeEmptyRender();
     });
   });
 });
--- blocks/subheadline-block/features/subheadline/default.test.jsx
+++ blocks/subheadline-block/features/subheadline/default.test.jsx
@@ -6,70 +6,7 @@ describe('the subheadline feature for the default output type', () => {
     jest.resetModules();
   });
 
-  describe('when "includeSubHeadline" is set to true', () => {
-    describe('when subheadline content from globalContent is present', () => {
-      beforeEach(() => {
-        jest.mock('fusion:context', () => ({
-          useComponentContext: jest.fn(() => ({
-            globalContent: {
-              subheadlines: {
-                basic: 'subheadline for our story',
-              },
-            },
-          })),
-        }));
-      });
-
-      it('should render an h1', () => {
-        const { default: SubHeadline } = require('./default');
-        const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
-
-        expect(wrapper.at(0).type()).toBe('h2');
-        expect(wrapper.at(0).hasClass('sub-headline')).toBe(true);
-      });
-
-      it('should dangerously set the innerHTML to the subheadline content', () => {
-        const { default: SubHeadline } = require('./default');
-        const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
-
-        expect(wrapper.at(0).prop('dangerouslySetInnerHTML')).toStrictEqual({ __html: 'subheadline for our story' });
-      });
-    });
-
-    describe('when subheadline content from globalContent is NOT present', () => {
-      beforeEach(() => {
-        jest.mock('fusion:context', () => ({
-          useComponentContext: jest.fn(() => ({
-            globalContent: {},
-          })),
-        }));
-      });
-
-      it('should render nothing', () => {
-        const { default: SubHeadline } = require('./default');
-        const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
-
-        expect(wrapper).toBeEmptyRender();
-      });
-    });
-  });
-
-  describe('when "includeSubHeadline" is set to false', () => {
-    beforeEach(() => {
-      jest.mock('fusion:context', () => ({
-        useComponentContext: jest.fn(() => ({})),
-      }));
-    });
-
-    it('should render nothing', () => {
-      const { default: SubHeadline } = require('./default');
-      const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: false }} />);
-
-      expect(wrapper).toBeEmptyRender();
-    });
-  });
-
-  describe('when "includeSubHeadline" is not set', () => {
+  describe('when subheadline content from globalContent is present', () => {
     beforeEach(() => {
       jest.mock('fusion:context', () => ({
         useComponentContext: jest.fn(() => ({
@@ -82,11 +19,36 @@ describe('the subheadline feature for the default output type', () => {
       }));
     });
 
+    it('should render an h1', () => {
+      const { default: SubHeadline } = require('./default');
+      const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
+
+      expect(wrapper.at(0).type()).toBe('h2');
+      expect(wrapper.at(0).hasClass('sub-headline')).toBe(true);
+    });
+
+    it('should dangerously set the innerHTML to the subheadline content', () => {
+      const { default: SubHeadline } = require('./default');
+      const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
+
+      expect(wrapper.at(0).prop('dangerouslySetInnerHTML')).toStrictEqual({ __html: 'subheadline for our story' });
+    });
+  });
+
+  describe('when subheadline content from globalContent is NOT present', () => {
+    beforeEach(() => {
+      jest.mock('fusion:context', () => ({
+        useComponentContext: jest.fn(() => ({
+          globalContent: {},
+        })),
+      }));
+    });
+
     it('should render nothing', () => {
       const { default: SubHeadline } = require('./default');
-      const wrapper = shallow(<SubHeadline customFields={{}} />);
+      const wrapper = shallow(<SubHeadline customFields={{ includeSubHeadline: true }} />);
 
-      expect(wrapper).not.toBeEmptyRender();
+      expect(wrapper).toBeEmptyRender();
     });
   });
 });

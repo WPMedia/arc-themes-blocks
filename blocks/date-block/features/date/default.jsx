b5ce1e781365358406de0367627796b749b2fd8f
SangHee <sangheekim22@gmail.com>
2019-10-15 11:48:56 -0500

    PEN-397 Let intl use default timezone if no variable is passed

--- blocks/date-block/features/date/default.jsx
+++ blocks/date-block/features/date/default.jsx
@@ -24,7 +24,7 @@ class ArticleDate extends Component {
         month: 'long',
         hour: 'numeric',
         minute: 'numeric',
-        timeZone: (TIMEZONE) || 'America/New_York', // Default to EST if no timezone is provided in env
+        timeZone: (TIMEZONE) || undefined, // If no timezone is defined, let it use the default value
         timeZoneName: 'short',
       }).format(new Date(dateString))
         .replace(/(,)(.*?)(,)/, '$1$2 at')

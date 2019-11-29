cf1e6567d8af8869259f0933199ebe55a983e76e
SangHee <sangheekim22@gmail.com>
2019-11-29 12:19:47 -0600

    Revert "Revert "Converted footer block to use styled components for theme styles""
    This reverts commit 865cff210b0627320c440e5268ef8caba6f61c56.

--- blocks/footer-block/features/footer/default.jsx
+++ blocks/footer-block/features/footer/default.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useContent } from 'fusion:content';
 import { useFusionContext } from 'fusion:context';
+import getThemeStyle from 'fusion:themes';
 
 import './footer.scss';
+
+const FooterSection = styled.ul`
+  font-family: ${props => props.primaryFont};
+`;
 
 const Footer = () => {
   const { arcSite } = useFusionContext();
@@ -13,22 +19,21 @@ const Footer = () => {
       hierarchy: 'footer',
     },
   });
-
   const footerColumns = (content && content.children) ? content.children : [];
+
   return (
     <footer>
       {/* The columns are 2D arrays of columns x column items. Iterate through both */}
       {footerColumns.map((column) => {
-        const columnItems = (column.children) ? column.children.map((item) => {
-          return (
-            <li className="footer-item" key={item._id}><a href={item.url}>{item.display_name}</a></li>
-          );
-        }) : [];
+        const columnItems = (column.children) ? column.children.map(item => (
+          <li className="footer-item" key={item._id}><a href={item.url}>{item.display_name}</a></li>
+        )) : [];
+
         return (
-          <ul className="footer-section" key={column._id}>
+          <FooterSection className="footer-section" key={column._id} primaryFont={getThemeStyle(arcSite)['primary-font-family']}>
             <section className="footer-header">{(column.name) ? column.name : ''}</section>
             {columnItems}
-          </ul>
+          </FooterSection>
         );
       })}
     </footer>

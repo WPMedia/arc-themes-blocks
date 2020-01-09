1f218de3ee726a8690065e59ce253f64ab97edd7
Brent Miller <brent.miller@washport.com>
2020-01-08 21:35:36 -0600

    Merge with latest

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,9 @@ module.exports = {
   coverageDirectory: '<rootDir>/coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
+      branches: 70,
+      functions: 70,
+      lines: 70,
     },
   },
   collectCoverageFrom: [

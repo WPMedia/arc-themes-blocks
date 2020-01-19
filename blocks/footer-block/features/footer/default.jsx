f60fd544c96366c0dc966d7a7a7de799212eeb57
Brent Miller <brent.miller@washport.com>
2020-01-18 20:52:39 -0600

    footer fixes

--- blocks/footer-block/features/footer/default.jsx
+++ blocks/footer-block/features/footer/default.jsx
@@ -73,61 +73,59 @@ const Footer = () => {
   );
 
   return (
-    <footer>
-      <div className="container">
-        <div className="section-separator">
-          <section className="footer-header">
-            <div className="footer-row">
-              <div className="social-column">
-                <div className="socialBtn-container">
-                  {socialButtons}
-                </div>
-              </div>
-              <div className="copyright-column">
-                {/* If large screen, show copyright over border */}
-                <p className="copyright" id="copyright-top" style={{ width: '100%' }}>
-                  {getProperties(arcSite).copyrightText}
-                </p>
+    <div className="container">
+      <div className="section-separator">
+        <section className="footer-header">
+          <div className="footer-row">
+            <div className="social-column">
+              <div className="socialBtn-container">
+                {socialButtons}
               </div>
             </div>
-          </section>
-        </div>
-        <div>
-          {/* If small screen, show copyright under border */}
-          <p className="copyright" id="copyright-bottom" style={{ width: '100%' }}>
-            {getProperties(arcSite).copyrightText}
-          </p>
-        </div>
-        <div className="row">
-          {/* The columns are 2D arrays of columns x column items. Iterate through both */}
-          {footerColumns.map((column) => {
-            const columnItems = (column.children) ? column.children.map(item => (
-              <li className="footer-item" key={item._id}>
-                <a href={item.url}>{item.display_name}</a>
-              </li>
-            )) : [];
-
-            return (
-              <FooterSection
-                className="footer-section col-sm-12 col-md-6 col-lg-xl-3"
-                key={column._id}
-                primaryFont={getThemeStyle(arcSite)['primary-font-family']}
-              >
-                <section className="footer-header">{(column.name) ? column.name : ''}</section>
-                {columnItems}
-              </FooterSection>
-            );
-          })}
-        </div>
-        <div className="primaryLogo">
-          <img
-            src={getProperties(arcSite).primaryLogo || placeholderLogo}
-            alt={getProperties(arcSite).primaryLogoAlt || 'Footer logo'}
-            className="footer-logo"
-          />
-        </div>
+            <div className="copyright-column">
+              {/* If large screen, show copyright over border */}
+              <p className="copyright" id="copyright-top" style={{ width: '100%' }}>
+                {getProperties(arcSite).copyrightText}
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
-    </footer>
+      <div>
+        {/* If small screen, show copyright under border */}
+        <p className="copyright" id="copyright-bottom" style={{ width: '100%' }}>
+          {getProperties(arcSite).copyrightText}
+        </p>
+      </div>
+      <div className="row legacy-footer-row">
+        {/* The columns are 2D arrays of columns x column items. Iterate through both */}
+        {footerColumns.map((column) => {
+          const columnItems = (column.children) ? column.children.map(item => (
+            <li className="footer-item" key={item._id}>
+              <a href={item.url}>{item.display_name}</a>
+            </li>
+          )) : [];
+
+          return (
+            <FooterSection
+              className="footer-section col-sm-12 col-md-6 col-lg-xl-3"
+              key={column._id}
+              primaryFont={getThemeStyle(arcSite)['primary-font-family']}
+            >
+              <section className="footer-header">{(column.name) ? column.name : ''}</section>
+              {columnItems}
+            </FooterSection>
+          );
+        })}
+      </div>
+      <div className="primaryLogo">
+        <img
+          src={getProperties(arcSite).primaryLogo || placeholderLogo}
+          alt={getProperties(arcSite).primaryLogoAlt || 'Footer logo'}
+          className="footer-logo"
+        />
+      </div>
+    </div>
   );
 };
 

3d54dc256bffbc414cb8e7a3d5cd378b641ca7d9
Brent Miller <brent.miller@washport.com>
2020-01-20 20:16:46 -0600

    More IE fixes: grid (flex property must have all 3 values)  and icons

--- blocks/lead-art-block/features/leadart/default.jsx
+++ blocks/lead-art-block/features/leadart/default.jsx
@@ -10,6 +10,7 @@ import {
 import './leadart.scss';
 import fullScreenLogo from './images/full-screen.svg';
 
+
 const LeadArtWrapperDiv = styled.div`
   figcaption {
     font-family: ${props => props.primaryFont};
@@ -46,7 +47,7 @@ class LeadArt extends Component {
 
   lightboxImgHandler() {
     const imgParentElm = this.imgRef.current;
-    const imgElm = imgParentElm.querySelector('img')
+    const imgElm = imgParentElm.querySelector('img');
     if (imgElm) {
       return imgElm.dataset.lightbox;
     }
@@ -133,13 +134,14 @@ class LeadArt extends Component {
               className="btn-full-screen"
               onClick={() => this.setState({ isOpen: true })}
             >
-              <img
-                className="ui-img"
-                src={fullScreenLogo}
-                title={buttonLabel}
-                alt={buttonLabel}
-                aria-label={buttonLabel}
-              />
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="2 2 20.5 20.5"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M21.602 15.955v4.65a.994.994 0 01-.996.997h-4.651c-.888 0-1.332-1.075-.706-1.702l1.503-1.503-4.451-4.452-4.452 4.456L9.353 19.9c.627.627.182 1.702-.706 1.702h-4.65A.994.994 0 013 20.606v-4.651c0-.889 1.075-1.333 1.702-.706l1.503 1.503 4.454-4.451-4.454-4.455-1.503 1.507C4.075 9.98 3 9.536 3 8.647v-4.65A.994.994 0 013.997 3h4.65c.888 0 1.333 1.075.706 1.702L7.85 6.206l4.451 4.45 4.452-4.455-1.504-1.499C14.623 4.075 15.067 3 15.955 3h4.65a.994.994 0 01.997.997v4.65c0 .889-1.075 1.333-1.702.706L18.397 7.85 13.943 12.3l4.454 4.455 1.503-1.503c.627-.63 1.702-.187 1.702.702z" fill="#6B6B6B" fillRule="nonzero" />
+              </svg>
               {buttonLabel}
             </button>
             <div ref={this.imgRef}>

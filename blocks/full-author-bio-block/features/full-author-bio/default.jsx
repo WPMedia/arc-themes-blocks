8aff0dbfa840bc7af044e59e77a920e30bf007b8
beltrancaliz <beltran@payble.net>
2020-01-24 11:46:49 -0600

    CSS spacers

--- blocks/full-author-bio-block/features/full-author-bio/default.jsx
+++ blocks/full-author-bio-block/features/full-author-bio/default.jsx
@@ -30,25 +30,69 @@ const logos = {
   rss: <Rss title="RSS feed" alt="rss" />,
 };
 
-const links = {
-  twitter: 'https://www.twitter.com',
-  facebook: 'https://www.facebook.com',
-};
-
-const AuthorPage = () => {
+const FullAuthorBio = () => {
   const { globalContent: content, arcSite } = useFusionContext();
-  const temp = true;
-  const icons = Object.keys(logos);
+  const mock = {
+    authors: [
+      {
+        _id: 'saracarothers',
+        firstName: 'Sara',
+        lastName: 'Carothers',
+        secondLastName: '',
+        byline: 'Sara Lynn Carothers',
+        role: 'Senior Product Manager',
+        image: 'https://s3.amazonaws.com/arc-authors/corecomponents/b80bd029-16d8-4a28-a874-78fc07ebc14a.jpg',
+        email: '',
+        affiliations: '',
+        education: [],
+        awards: [],
+        books: [],
+        podcasts: [],
+        twitter: 'https://twitter.com/sLcarothers',
+        bio_page: '/author/sara-carothers/',
+        bio: 'Sara Carothers is a senior product manager for Arc Publishing. This is a short bio. ',
+        longBio: 'Sara Carothers is a senior product manager for Arc Publishing. She works on Arc Themes and PageBuilder Fusion. This is a long bio. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n',
+        slug: 'sara-carothers',
+        instagram: 'https://www.instagram.com/scarothers/',
+        native_app_rendering: false,
+        fuzzy_match: false,
+        contributor: false,
+        status: true,
+        last_updated_date: '2019-11-22T23:15:45.348Z',
+        type: 'author',
+      },
+    ],
+    last: 'c2FyYWNhcm90aGVycw==',
+    more: false,
+    _id: 'aea0c7ea37263d5d663cbb6844a506d39dfb7e02a76ab932d6e740c4e2807906',
+  };
+
+  const socials = [];
+
+  Object.keys(mock.authors[0]).forEach((item) => {
+    if ((item === 'facebook'
+      || item === 'instagram'
+      || item === 'email'
+      || item === 'rss'
+    ) && item.length > 0) {
+      socials.push(item);
+    }
+  });
+
   return (
     !!(
-      temp
+      mock.authors[0]
+      && mock.authors[0].byline
+      && mock.authors[0].role
+      && (mock.authors[0].bio || mock.authors[0].longBio)
+      && mock.authors[0].image
     ) && (
       <>
         <div className="image-container">
           <img
-            src="https://via.placeholder.com/250"
+            src={mock.authors[0].image}
             className="author-image"
-            alt="Author image"
+            alt="Author photo"
           />
         </div>
         <div>
@@ -57,10 +101,10 @@ const AuthorPage = () => {
             primaryFont={getThemeStyle(arcSite)['primary-font-family']}
             primaryColor={getThemeStyle(arcSite)['primary-color']}
           >
-            <h1 className="author-name">Author Name</h1>
-            <h4 className="author-title">Author Title</h4>
+            <h1 className="author-name">{mock.authors[0].byline}</h1>
+            <h4 className="author-title">{mock.authors[0].role}</h4>
             <p className="author-bio">
-              {'Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.'}
+              {mock.authors[0].longBio || mock.authors[0].bio}
             </p>
           </StyledAuthorContent>
         </div>
@@ -69,12 +113,16 @@ const AuthorPage = () => {
           <div className="social-items">
             <p className="social-column"><b>Connect</b></p>
             {
-              icons.map(logo => (
+              socials.map(item => (
                 <a
                   className="social-column"
-                  href={links[logo]}
+                  href={
+                    (item === 'email')
+                      ? `mailto:${mock.authors[0][item]}`
+                      : mock.authors[0][item]
+                  }
                 >
-                  {logos[logo]}
+                  {logos[item]}
                 </a>
               ))
             }
@@ -85,4 +133,4 @@ const AuthorPage = () => {
   );
 };
 
-export default AuthorPage;
+export default FullAuthorBio;

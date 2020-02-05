e00a26879416b92e604675a0ef603232b74b5206
Sean Shannon <sean.shannon@washpost.com>
2020-02-05 15:54:55 -0500

    Running ESLint on test file

--- blocks/overline-block/features/overline/default.test.jsx
+++ blocks/overline-block/features/overline/default.test.jsx
@@ -1,9 +1,7 @@
-/* global jest, describe, it, expect, beforeEach */
-
-import React from 'react'
-import { shallow, mount } from 'enzyme'
-import { useFusionContext } from 'fusion:context'
-import Overline from './default'
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { useFusionContext } from 'fusion:context';
+import Overline from './default';
 
 const mockContextObj = {
   arcSite: 'site',
@@ -12,120 +10,118 @@ const mockContextObj = {
       site: {
         website_section: {
           _id: '/news',
-          name: 'News'
-        }
-      }
-    }
-  }
-}
+          name: 'News',
+        },
+      },
+    },
+  },
+};
 
-jest.mock('fusion:themes', () => (jest.fn(() => ({}))))
+jest.mock('fusion:themes', () => (jest.fn(() => ({}))));
 jest.mock('fusion:context', () => ({
-  useFusionContext: jest.fn(() => {
-    return mockContextObj
-  })
-}))
+  useFusionContext: jest.fn(() => mockContextObj),
+}));
 
 describe('overline feature for default output type', () => {
   describe('when website_section content from globalContent is present', () => {
     it('should render an a', () => {
-      const wrapper = mount(<Overline />)
+      const wrapper = mount(<Overline />);
 
-      expect(wrapper.find('a')).toHaveClassName('overline')
-    })
+      expect(wrapper.find('a')).toHaveClassName('overline');
+    });
 
     it('should dangerously set the inner HTML to the website_section content', () => {
-      const wrapper = shallow(<Overline />)
+      const wrapper = shallow(<Overline />);
 
-      expect(wrapper.text()).toMatch('News')
-    })
+      expect(wrapper.text()).toMatch('News');
+    });
 
     it('should set a styled component class on the rendered a', () => {
-      const wrapper = mount(<Overline />)
+      const wrapper = mount(<Overline />);
 
-      expect(wrapper.find('a').hasClass(/sc-/)).toBe(true)
-    })
+      expect(wrapper.find('a').hasClass(/sc-/)).toBe(true);
+    });
 
     it('should have the href of the website_section _id', () => {
-      const wrapper = shallow(<Overline />)
+      const wrapper = shallow(<Overline />);
 
-      expect(wrapper.at(0).prop('href')).toStrictEqual('/news')
-    })
-  })
+      expect(wrapper.at(0).prop('href')).toStrictEqual('/news');
+    });
+  });
 
   describe('when label content from globalContent is present', () => {
     describe('when label.basic.display is true', () => {
       beforeEach(() => {
         const labelObj = {
-          label: { basic: { display: true, text: 'EXCLUSIVE', url: '/exclusive' } }
-        }
+          label: { basic: { display: true, text: 'EXCLUSIVE', url: '/exclusive' } },
+        };
         const contextObjWithLabel = Object.assign(
           {},
           mockContextObj,
           {
             globalContent: {
               ...labelObj,
-              ...mockContextObj.globalContent
-            }
-          }
-        )
-        useFusionContext.mockImplementation(() => contextObjWithLabel)
-      })
+              ...mockContextObj.globalContent,
+            },
+          },
+        );
+        useFusionContext.mockImplementation(() => contextObjWithLabel);
+      });
 
       it('should display the label name instead of the website section name', () => {
-        const wrapper = shallow(<Overline />)
+        const wrapper = shallow(<Overline />);
 
-        expect(wrapper.text()).toMatch('EXCLUSIVE')
-      })
+        expect(wrapper.text()).toMatch('EXCLUSIVE');
+      });
 
       it('should render the href of the label instead of the website section', () => {
-        const wrapper = shallow(<Overline />)
+        const wrapper = shallow(<Overline />);
 
-        expect(wrapper.at(0).prop('href')).toStrictEqual('/exclusive')
-      })
-    })
+        expect(wrapper.at(0).prop('href')).toStrictEqual('/exclusive');
+      });
+    });
 
     describe('when label.basic.display is NOT true', () => {
       beforeEach(() => {
         const labelObj = {
-          label: { basic: { display: false, text: 'EXCLUSIVE', url: '/exclusive' } }
-        }
+          label: { basic: { display: false, text: 'EXCLUSIVE', url: '/exclusive' } },
+        };
         const contextObjWithLabel = Object.assign(
           {},
           mockContextObj,
           {
             globalContent: {
               ...labelObj,
-              ...mockContextObj.globalContent
-            }
-          }
-        )
-        useFusionContext.mockImplementation(() => contextObjWithLabel)
-      })
+              ...mockContextObj.globalContent,
+            },
+          },
+        );
+        useFusionContext.mockImplementation(() => contextObjWithLabel);
+      });
 
       it('should dangerously set the inner HTML to the website_section content', () => {
-        const wrapper = shallow(<Overline />)
+        const wrapper = shallow(<Overline />);
 
-        expect(wrapper.text()).toMatch('News')
-      })
+        expect(wrapper.text()).toMatch('News');
+      });
 
       it('should have the href of the website_section _id', () => {
-        const wrapper = shallow(<Overline />)
+        const wrapper = shallow(<Overline />);
 
-        expect(wrapper.at(0).prop('href')).toStrictEqual('/news')
-      })
-    })
-  })
+        expect(wrapper.at(0).prop('href')).toStrictEqual('/news');
+      });
+    });
+  });
 
   describe('when headline content from globalContent is NOT present', () => {
     beforeEach(() => {
-      useFusionContext.mockImplementation(() => ({}))
-    })
+      useFusionContext.mockImplementation(() => ({}));
+    });
 
     it('should not render anything', () => {
-      const wrapper = mount(<Overline />)
+      const wrapper = mount(<Overline />);
 
-      expect(wrapper).toBeEmptyRender()
-    })
-  })
-})
+      expect(wrapper).toBeEmptyRender();
+    });
+  });
+});

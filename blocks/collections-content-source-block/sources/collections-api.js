56a7e7e4cd60653052a22a049f75dd399010040b
beltrancaliz <beltran@payble.net>
2020-02-10 14:31:59 -0600

    Unit tests for collections api

--- blocks/collections-content-source-block/sources/collections-api.js
+++ blocks/collections-content-source-block/sources/collections-api.js
@@ -1,13 +1,19 @@
 const params = {
   _id: 'text',
+  content_alias: 'text',
   from: 'text',
   size: 'text',
 };
 
 const resolve = (key = {}) => {
   const site = key['arc-site'];
-  const { _id, from, size } = key;
-  return `/content/v4/collections?_id=${_id}${site ? `&website=${site}` : ''}${from ? `&from=${from}` : ''}${size ? `&from=${size}` : ''}`;
+  // const site = 'the-sun'
+  const {
+    _id, content_alias: contentAlias, from, size,
+  } = key;
+  console.log('GOT', site)
+  // return `/content/v4/collections?_id=${_id}${site ? `&website=${site}` : ''}`;
+  return `content/v4/collections?${_id ? `_id=${_id}` : `content_alias=${contentAlias}`}${site ? `&website=${site}` : ''}${from ? `&from=${from}` : ''}${size ? `&size=${size}` : ''}&published=true`;
 };
 
 export default {

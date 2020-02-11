dbacc046361d27200d6d9318eccb250823b01d22
beltrancaliz <beltran@payble.net>
2020-02-11 11:17:17 -0600

    Updated content source

--- blocks/collections-content-source-block/sources/collections-api.js
+++ blocks/collections-content-source-block/sources/collections-api.js
@@ -7,12 +7,9 @@ const params = {
 
 const resolve = (key = {}) => {
   const site = key['arc-site'];
-  // const site = 'the-sun'
   const {
     _id, content_alias: contentAlias, from, size,
   } = key;
-  console.log('GOT', site)
-  // return `/content/v4/collections?_id=${_id}${site ? `&website=${site}` : ''}`;
   return `content/v4/collections?${_id ? `_id=${_id}` : `content_alias=${contentAlias}`}${site ? `&website=${site}` : ''}${from ? `&from=${from}` : ''}${size ? `&size=${size}` : ''}&published=true`;
 };
 

30e83caa439e7c8d467c25bd6e69fc0d8a78f93e
jackhowa <jack.howard@washpost.com>
2020-02-21 15:27:10 -0600

    Refactor tests to ensure test coverage, best practices

--- blocks/double-chain-block/chains/double-chain/default.test.jsx
+++ blocks/double-chain-block/chains/double-chain/default.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import DoubleChain from './default';
 
 describe('the double chain block', () => {
@@ -11,7 +11,7 @@ describe('the double chain block', () => {
     const component = shallow(
       <DoubleChain />,
     );
-    expect(component.type()).toBeNull();
+    expect(component).toBeEmptyRender();
   });
   it('should put all features into the first column by default', () => {
     const customFields = {};
@@ -23,12 +23,13 @@ describe('the double chain block', () => {
         <Comp4 />
       </DoubleChain>,
     );
+    expect(component).not.toBeEmptyRender();
     expect(component.find('.column-1').children().length).toBe(4);
     expect(component.find('.column-2').children().length).toBe(0);
   });
   it('should be able to accept a number in the custom field, and that number of features within the chain should appear in the first column. ', () => {
     const customFields = { columnOne: 2 };
-    const component = shallow(
+    const component = mount(
       <DoubleChain customFields={customFields}>
         <Comp1 />
         <Comp2 />
@@ -38,12 +39,14 @@ describe('the double chain block', () => {
     );
 
     const column1 = component.find('.column-1');
+    const column2 = component.find('.column-2');
 
-    expect(column1.children().length).toBe(2);
+    expect(column1.text()).toEqual('12');
+    expect(column2.text()).toEqual('34');
   });
   it('should be able to accept a number in the custom field, any additional features in the chain should be placed in the second column. ', () => {
     const customFields = { columnOne: 1 };
-    const component = shallow(
+    const component = mount(
       <DoubleChain customFields={customFields}>
         <Comp1 />
         <Comp2 />
@@ -52,8 +55,25 @@ describe('the double chain block', () => {
       </DoubleChain>,
     );
 
+    const column1 = component.find('.column-1');
     const column2 = component.find('.column-2');
 
+    expect(column1.text()).toEqual('1');
+    expect(column2.text()).toEqual('234');
+
     expect(column2.children().length).toBe(3);
+  });
+  it('should render nothing if negative column 1 amount', () => {
+    const customFields = { columnOne: -10 };
+    const component = mount(
+      <DoubleChain customFields={customFields}>
+        <Comp1 />
+        <Comp2 />
+        <Comp3 />
+        <Comp4 />
+      </DoubleChain>,
+    );
+
+    expect(component).toBeEmptyRender();
   });
 });

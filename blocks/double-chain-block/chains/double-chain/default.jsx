d149f58eac4e1ed4ebeb0f08e6eb35c67ae5c14b
jackhowa <jack.howard@washpost.com>
2020-02-21 12:58:57 -0600

    Replace index check with slice, check min length issue

--- blocks/double-chain-block/chains/double-chain/default.jsx
+++ blocks/double-chain-block/chains/double-chain/default.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DoubleChain = ({ children, customFields }) => (children && children.length > 0 ? (
-  <div className="container-fluid double-chain">
-    <div className="row">
-      <div className="col-sm-12 col-md-xl-6 ie-flex-100-percent-sm column-1">
-        {children.map((feature, index) => (((index < customFields.columnOne) || typeof customFields.columnOne === 'undefined') ? feature : ''))}
-      </div>
-      <div className="col-sm-12 col-md-xl-6 ie-flex-100-percent-sm column-2">
-        {children.map((feature, index) => ((index >= customFields.columnOne) ? feature : ''))}
-      </div>
-    </div>
-  </div>
-) : null);
+const DoubleChain = ({ children, customFields }) => {
+  if (children && children.length && children.length > 0) {
+    // if no columnOne length set, then use the length of the children
+    // if no length set, then all children will be put in column one
+    const { columnOne: columnOneLength = children.length } = customFields;
 
+    // check column one length not negative
+    if (columnOneLength > 0) {
+      return (
+        <div className="container-fluid double-chain">
+          <div className="row">
+            <div className="col-sm-12 col-md-xl-6 ie-flex-100-percent-sm column-1">
+              {children.slice(0, columnOneLength)}
+            </div>
+            <div className="col-sm-12 col-md-xl-6 ie-flex-100-percent-sm column-2">
+              {children.slice(columnOneLength)}
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return null;
+};
 
 DoubleChain.label = 'Double Chain – Arc Block';
 

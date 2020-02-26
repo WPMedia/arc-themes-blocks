8eac9a42aa3437761e88066fd30b3cd4fff0cd52
jackhowa <jack.howard@washpost.com>
2020-02-26 10:13:42 -0600

    Add consturcted url to small item

--- blocks/top-table-list-block/features/top-table-list/_children/item-title-with-right-image.jsx
+++ blocks/top-table-list-block/features/top-table-list/_children/item-title-with-right-image.jsx
@@ -3,27 +3,29 @@ import Title from './title';
 
 const ItemTitleWithRightImage = (props) => {
   const {
-    itemTitle, imageURL, id, primaryFont,
+    itemTitle, imageURL, id, primaryFont, constructedURL,
   } = props;
-
   return (
     <article key={id} className="container-fluid small-promo">
       <div className="row sm-promo-padding-btm">
         {itemTitle !== '' ? (
           <div className="col-sm-xl-8">
-            <Title primaryFont={primaryFont} className="sm-promo-headline">
-              {itemTitle}
-            </Title>
+            <a href={constructedURL} title={itemTitle} className="sm-promo-headline">
+              <Title primaryFont={primaryFont} className="sm-promo-headline">
+                {itemTitle}
+              </Title>
+            </a>
           </div>
         ) : null}
         <div className="col-sm-xl-4">
           {imageURL !== '' ? (
-            <img src={imageURL} alt={itemTitle} />
+            <a href={constructedURL} title={itemTitle} className="list-anchor">
+              <img src={imageURL} alt={itemTitle} />
+            </a>
           ) : null}
         </div>
       </div>
     </article>
   );
 };
-
 export default ItemTitleWithRightImage;

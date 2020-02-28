f93cc8a4053612ec5bbcb3cb0a22cabb0ce18b92
jackhowa <jack.howard@washpost.com>
2020-02-28 11:54:08 -0600

    Fix linting, odd, on another file

--- blocks/unpublished-content-source-block/sources/content-api-unpublished.js
+++ blocks/unpublished-content-source-block/sources/content-api-unpublished.js
@@ -1,6 +1,6 @@
 export default {
   resolve(contentOptions) {
-    const { _id, 'arc-site': arcSite } = contentOptions
+    const { _id, 'arc-site': arcSite } = contentOptions;
     return `content/v4?_id=${_id}&website=${arcSite}&published=false`;
   },
   params: {

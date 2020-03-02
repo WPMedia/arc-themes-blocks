d01ff36be4df2db6054cc8df67b127e5a6d467c4
jackhowa <jack.howard@washpost.com>
2020-03-02 13:47:42 -0600

    Add xl aspect ratio for images

--- blocks/extra-large-manual-promo-block/features/extra-large-manual-promo/default.jsx
+++ blocks/extra-large-manual-promo-block/features/extra-large-manual-promo/default.jsx
@@ -78,11 +78,11 @@ const ExtraLargeManualPromo = ({ customFields, arcSite }) => (customFields.linkU
                   url={customFields.imageURL}
                   alt={customFields.headline}
                   smallWidth={400}
-                  smallHeight={0}
+                  smallHeight={300}
                   mediumWidth={600}
-                  mediumHeight={0}
+                  mediumHeight={450}
                   largeWidth={800}
-                  largeHeight={0}
+                  largeHeight={600}
                 />
               </a>
             )}

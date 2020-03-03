1963409879242eca211cee7f7382d3f464b29cc6
jackhowa <jack.howard@washpost.com>
2020-03-03 09:56:43 -0600

    Update redirect for footer

--- blocks/footer-block/features/footer/default.jsx
+++ blocks/footer-block/features/footer/default.jsx
@@ -52,7 +52,8 @@ const Footer = ({ customFields: { navigationConfig } }) => {
               title="Twitter feed"
               target="_blank"
               rel="noopener noreferrer"
-              href={getProperties(arcSite).twitterUsername}
+              // https://twitter.com/jack
+              href={`https://twitter.com/${getProperties(arcSite).twitterUsername}`}
             >
               <TwitterIcon fill="#2980B9" />
             </a>

60f6ad20aa6942d19c7c55ff48be21b05b49524f
jackhowa <jack.howard@washpost.com>
2020-03-10 17:07:29 -0500

    Update styled components for masthead dynamic

---
+++ blocks/masthead-block/features/masthead-block/_children/StyledLink.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  font-family: ${(props) => props.primaryFont};
+  text-decoration: none;
+  font-size: 14px;
+  line-height: 16px;
+  color: #191919;
+`;
+
+export default StyledLink;
--- blocks/masthead-block/features/masthead-block/default.jsx
+++ blocks/masthead-block/features/masthead-block/default.jsx
@@ -1,47 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import getThemeStyle from 'fusion:themes';
 
-
-const MastheadItemsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-
-  > div {
-    flex: 1;
-    display: flex;
-    align-items: flex-end;
-
-    > p {
-      font-size: 14px;
-      line-height: 16px;
-      font-family: ${(props) => props.primaryFont};
-      margin: 0;
-      color: #191919;
-    }
-  }
-
-  > div:first-child {
-    justify-content: flex-start;
-  }
-
-  > div:nth-child(2) {
-    justify-content: center;
-  }
-
-  > div:nth-child(3) {
-    justify-content: flex-end;
-  }
-`;
-
-const StyledLink = styled.a`
-  font-family: ${(props) => props.primaryFont};
-  text-decoration: none;
-  font-size: 14px;
-  line-height: 16px;
-  color: #191919;
-`;
+// styles
+import MastheadItemsContainer from './_children/MastheadItemsContainer';
+import HeightConstrainedImageContainer from './_children/HeightConstrainedImageContainer';
+import StyledLink from './_children/StyledLink';
+import GenericDivider from './_children/GenericDivider';
+import HeaderContainerHideMobile from './_children/HeaderContainerHideMobile';
 
 const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December',
@@ -59,10 +25,16 @@ const Masthead = (props) => {
   const primaryFont = getThemeStyle(arcSite)['primary-font-family'];
 
   return (
-    <header>
-      <div>
-        {logoURL && <img src={logoURL} alt="Masthead logo" />}
-      </div>
+    <HeaderContainerHideMobile>
+      {
+         logoURL && (
+         <HeightConstrainedImageContainer>
+           <picture>
+             <img src={logoURL} alt="Masthead logo" />
+           </picture>
+         </HeightConstrainedImageContainer>
+         )
+      }
       <MastheadItemsContainer primaryFont={primaryFont}>
         <div>
           {showDate && <p>{showLocalMonthDateYear(new Date())}</p>}
@@ -78,8 +50,8 @@ const Masthead = (props) => {
           )}
         </div>
       </MastheadItemsContainer>
-      <hr color="#000A12" size={2} />
-    </header>
+      <GenericDivider color="#000A12" size={2} />
+    </HeaderContainerHideMobile>
   );
 };
 

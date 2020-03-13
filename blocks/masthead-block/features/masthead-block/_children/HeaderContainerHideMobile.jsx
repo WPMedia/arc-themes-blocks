1a63b611682daa1dc03756d7ca9dc82cdb7ad49a
jackhowa <jack.howard@washpost.com>
2020-03-13 15:28:58 -0500

    Remove header tag so no header collisions with layout

--- blocks/masthead-block/features/masthead-block/_children/HeaderContainerHideMobile.jsx
+++ blocks/masthead-block/features/masthead-block/_children/HeaderContainerHideMobile.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const HeaderContainerHideMobile = styled.header`
+const HeaderContainerHideMobile = styled.div`
   @media (max-width: 768px) {
     display: none;
   }

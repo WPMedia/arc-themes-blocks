457e18a28fa9204bcf632a4743e15e551f3c5e32
jackhowa <jack.howard@washpost.com>
2020-03-23 16:53:02 -0500

    Return the data passed in on the frontend

--- blocks/story-feed-query-content-source-block/sources/story-feed-query.js
+++ blocks/story-feed-query-content-source-block/sources/story-feed-query.js
@@ -1,3 +1,5 @@
+import getResizedImageData from './utils/getResizedImageData';
+
 export default {
   resolve: (params) => `/content/v4/search/published?q=${params.query || '*'}&website=${params['arc-site']}&size=${params.size || 8}&from=${params.offset || 0}&sort=display_date:desc`,
   schemaName: 'ans-feed',
@@ -5,5 +7,11 @@ export default {
     query: 'text',
     size: 'number',
     offset: 'number',
+  },
+  transform: (data) => {
+    if (typeof window === 'undefined') {
+      return getResizedImageData(data);
+    }
+    return data;
   },
 };

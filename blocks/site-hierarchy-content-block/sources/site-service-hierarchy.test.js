f7da9f8952c7346fd1d764c5cd1be788fc982f61
beltrancaliz <beltran@payble.net>
2020-03-23 13:08:33 -0500

    Updated site service hierarchy unit tests

--- blocks/site-hierarchy-content-block/sources/site-service-hierarchy.test.js
+++ blocks/site-hierarchy-content-block/sources/site-service-hierarchy.test.js
@@ -8,13 +8,35 @@ describe('the site-service-hierarchy content source block', () => {
     });
   });
 
+  it('when a hierarchy and sectionId are provided it should build the correct url', () => {
+    const url = contentSource.resolve({
+      hierarchy: 'foooter',
+      sectionId: '/sports',
+      'arc-site': 'bbbbb-ccccc',
+    });
+
+    expect(url).toEqual('/site/v3/navigation/bbbbb-ccccc?hierarchy=foooter&_id=/sports');
+  });
+
   it('when a hierarchy is provided it should build the correct url', () => {
-    const url = contentSource.resolve({ hierarchy: 'foooter', 'arc-site': 'bbbbb-ccccc' });
+    const url = contentSource.resolve({
+      hierarchy: 'foooter',
+      'arc-site': 'bbbbb-ccccc',
+    });
 
     expect(url).toEqual('/site/v3/navigation/bbbbb-ccccc?hierarchy=foooter');
   });
 
-  it('when a hierarchy is NOT provided it should build a url without it', () => {
+  it('when a sectionId is provided it should build the correct url', () => {
+    const url = contentSource.resolve({
+      sectionId: '/sports',
+      'arc-site': 'bbbbb-ccccc',
+    });
+
+    expect(url).toEqual('/site/v3/navigation/bbbbb-ccccc?&_id=/sports');
+  });
+
+  it('when a hierarchy and sectionId are NOT provided it should build a url without them', () => {
     const url = contentSource.resolve({ 'arc-site': 'bbbbb-ccccc' });
 
     expect(url).toEqual('/site/v3/navigation/bbbbb-ccccc?');

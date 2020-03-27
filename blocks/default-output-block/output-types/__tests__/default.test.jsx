69ce9c86d444f4c8d98cbddd1e01d46bfa30fff0
beltrancaliz <beltran@payble.net>
2020-03-27 14:01:41 -0500

    Default output type basic test

--- blocks/default-output-block/output-types/__tests__/default.test.jsx
+++ blocks/default-output-block/output-types/__tests__/default.test.jsx
@@ -1,9 +1,34 @@
+/**
+ * this is for mocking node env
+ * will not have window attribute, testing ssr
+ * https://jestjs.io/docs/en/configuration.html#testenvironment-string
+ * @jest-environment node
+ */
 import React from 'react';
 import { shallow } from 'enzyme';
+import getProperties from 'fusion:properties';
 import DefaultOutputType from '../default';
 
-xit('should render', () => {
-  const instance = shallow(<DefaultOutputType deployment={jest.fn()} />);
+jest.mock('fusion:context', () => ({
+  useFusionContext: jest.fn(() => ({
+    globalContent: {},
+    arcSite: 'the-sun',
+  })),
+}));
 
-  expect(instance).toBeDefined();
+jest.mock('react-dom/server', () => ({
+  renderToString: jest.fn().mockReturnValue('<meta />'),
+}));
+
+getProperties.mockImplementation(() => ({
+  websiteName: 'The Sun',
+  twitterSite: 'https://www.twitter.com/the-sun',
+  dangerouslyInjectJS: [],
+}));
+
+describe('the default output type', () => {
+  const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn()} />);
+  it('should render', () => {
+    expect(wrapper).toBeDefined();
+  });
 });

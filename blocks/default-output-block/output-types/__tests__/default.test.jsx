f792341e1ca2df6a0534794d39f5b993ee6aee7a
beltrancaliz <beltran@payble.net>
2020-03-30 10:15:46 -0500

    Unit tests for default output block and article page

--- blocks/default-output-block/output-types/__tests__/default.test.jsx
+++ blocks/default-output-block/output-types/__tests__/default.test.jsx
@@ -27,8 +27,76 @@ getProperties.mockImplementation(() => ({
 }));
 
 describe('the default output type', () => {
-  const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn()} />);
   it('should render', () => {
+    const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn()} />);
     expect(wrapper).toBeDefined();
+  });
+  describe('renders a page', () => {
+    it('should have a title', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('title').length).toBe(1);
+    });
+
+    it('should have a head', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('head').length).toBe(1);
+    });
+
+    it('should have a body', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('body').length).toBe(1);
+    });
+
+    it('should have meta tags', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('meta').length).toBe(10);
+    });
+
+    it('should have script tags', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('script').length).toBe(7);
+    });
+
+    it('should have link tags', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('link').length).toBe(2);
+    });
+  });
+
+  describe('when the an article page type is provided', () => {
+    it('should have a title tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find('title').childAt(0).text()).toEqual('article – The Sun');
+    });
+
+    it('should have an article description meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[name='description']").props().content).toBe('article');
+    });
+
+    it('should have an article keywords meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[name='keywords']").props().content).toBe('article');
+    });
+
+    it('should have an article og:title meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[property='og:title']").props().content).toBe('article');
+    });
+
+    it('should have an article og:image meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[property='og:image']").props().content).toBe('undefined/unsafe/1200x630/article');
+    });
+
+    it('should have an article og:image:alt meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[property='og:image:alt']").props().content).toBe('article');
+    });
+
+    it('should have an robots meta tag', () => {
+      const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+      expect(wrapper.find("meta[name='robots']").props().content).toBe('noarchive');
+    });
   });
 });

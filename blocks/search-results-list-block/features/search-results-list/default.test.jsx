3fd45b5c66c94a6fa438a6644fc09f1370b27a7f
beltrancaliz <beltran@payble.net>
2020-04-06 12:06:31 -0500

    Tests for search results list container

---
+++ blocks/search-results-list-block/features/search-results-list/default.test.jsx
@@ -0,0 +1,52 @@
+// eslint-disable-next-line max-classes-per-file
+import React from 'react';
+import { shallow } from 'enzyme';
+
+jest.mock('./_children/global-content', () => class GlobalContentSearchResultsList {});
+jest.mock('./_children/custom-content', () => class CustomContentSearchResultsList {});
+jest.mock('prop-types', () => ({
+  bool: true,
+  shape: () => {},
+  contentConfig: () => {},
+}));
+
+describe('the search results list feature block', () => {
+  describe('when it is configured to inherit global content', () => {
+    it('should render the global content search results list', () => {
+      const { default: SearchResultsListContainer } = require('./default');
+      const wrapper = shallow(
+        <SearchResultsListContainer customFields={{ inheritGlobalContent: true }} />,
+      );
+      expect(wrapper.is('GlobalContentSearchResultsList')).toBeTruthy();
+    });
+  });
+
+  describe('when it is configured to NOT inherit global content', () => {
+    it('should render the global content search results list', () => {
+      const { default: SearchResultsListContainer } = require('./default');
+
+      const wrapper = shallow(
+        <SearchResultsListContainer
+          customFields={{ inheritGlobalContent: false, sectionContentConfig: {} }}
+        />,
+      );
+      expect(wrapper.is('CustomContentSearchResultsList')).toBeTruthy();
+    });
+  });
+
+  describe('when customFields is empty', () => {
+    it('should render the global content search results list', () => {
+      const { default: SearchResultsListContainer } = require('./default');
+      const wrapper = shallow(<SearchResultsListContainer customFields={{}} />);
+      expect(wrapper.is('GlobalContentSearchResultsList')).toBeTruthy();
+    });
+  });
+
+  describe('when customFields is missing', () => {
+    it('should render the global content search results list', () => {
+      const { default: SearchResultsListContainer } = require('./default');
+      const wrapper = shallow(<SearchResultsListContainer customFields={undefined} />);
+      expect(wrapper.is('GlobalContentSearchResultsList')).toBeTruthy();
+    });
+  });
+});

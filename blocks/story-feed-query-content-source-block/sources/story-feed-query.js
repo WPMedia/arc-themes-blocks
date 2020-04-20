e24399d8d1ecb5271bbbd600dac8b6af8bea9434
jackhowa <jack.howard@washpost.com>
2020-04-20 14:10:59 -0500

    Update function call to image update

--- blocks/story-feed-query-content-source-block/sources/story-feed-query.js
+++ blocks/story-feed-query-content-source-block/sources/story-feed-query.js
@@ -1,3 +1,5 @@
+import getResizedImageData from '@wpmedia/resizer-image-block';
+
 export default {
   resolve: (params) => `/content/v4/search/published?q=${params.query || '*'}&website=${params['arc-site']}&size=${params.size || 8}&from=${params.offset || 0}&sort=display_date:desc`,
   schemaName: 'ans-feed',
@@ -6,5 +8,5 @@ export default {
     size: 'number',
     offset: 'number',
   },
-  transform: (data) => data,
+  transform: (data) => getResizedImageData(data),
 };

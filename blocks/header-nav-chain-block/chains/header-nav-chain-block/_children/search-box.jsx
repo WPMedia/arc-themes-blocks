e155821f8e404b9b86d84009d82b581951d187dc
Sean Shannon <sean.shannon@washpost.com>
2020-04-27 00:10:06 -0400

    Navigation bar chain

---
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/search-box.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState } from 'react';
+import SearchIcon from '@wpmedia/engine-theme-sdk/dist/es/components/icons/SearchIcon';
+
+export default ({ alwaysOpen = false, iconSize = 16 }) => {
+  const [shouldSearchOpen, setShouldSearchOpen] = useState(false);
+  const searchInput = useRef(null);
+
+  useEffect(() => {
+    const el = searchInput.current;
+    if (shouldSearchOpen) {
+      el.focus();
+    } else {
+      el.blur();
+    }
+  }, [shouldSearchOpen]);
+
+  const handleSearchBtnMousedown = (event) => {
+    // if open, prevent blur event so we don't get a race condition on click vs blur
+    if (shouldSearchOpen) {
+      event.preventDefault();
+    } else {
+      setShouldSearchOpen(true);
+    }
+  };
+
+  const isSearchBarOpen = shouldSearchOpen || alwaysOpen;
+  const navClassNames = `nav-search${isSearchBarOpen ? ' open' : ''}`;
+  const btnClassNames = `nav-btn transparent${!isSearchBarOpen ? ' border' : ''}`;
+  const iconFill = isSearchBarOpen ? '#666666' : 'white';
+
+  return (
+    <div className={navClassNames}>
+      <input ref={searchInput} onBlur={() => { setShouldSearchOpen(false); }} type="text" placeholder="Search" />
+      <button className={btnClassNames} onMouseDown={handleSearchBtnMousedown} type="button">
+        <SearchIcon fill={iconFill} height={iconSize} width={iconSize} />
+      </button>
+    </div>
+  );
+};

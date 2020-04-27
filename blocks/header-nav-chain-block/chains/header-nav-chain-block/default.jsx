effccb77f2df97bc28d1d67715955a2eaa61b2fb
Sean Shannon <sean.shannon@washpost.com>
2020-04-27 00:10:06 -0400

    Navigation bar chain

---
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/default.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { useContent } from 'fusion:content';
+import { useAppContext } from 'fusion:context';
+import getProperties from 'fusion:properties';
+import getThemeStyle from 'fusion:themes';
+import HamburgerMenuIcon from '@wpmedia/engine-theme-sdk/dist/es/components/icons/HamburgerMenuIcon';
+import ArcLogo from '@wpmedia/engine-theme-sdk/dist/es/components/ArcLogo';
+import SectionNav from './_children/section-nav';
+import SearchBox from './_children/search-box';
+import './navigation.scss';
+
+/* Global Constants */
+// Since these values are used to coordinate multiple components, I thought I'd make them variables
+// so we could just change the vars instead of multiple CSS values
+const iconSize = 16;
+const navHeight = '56px';
+const navZIdx = 9;
+const sectionZIdx = navZIdx - 1;
+
+/* Styled Components */
+const StyledNav = styled.nav`
+background-color: #000;
+height: ${navHeight};
+z-index: ${navZIdx};
+  * {
+    font-family: ${(props) => props.font};
+  }
+`;
+const StyledSectionDrawer = styled.div`
+  font-family: ${(props) => props.font};
+  position: fixed;
+  top: ${navHeight};
+  z-index: ${sectionZIdx};
+`;
+
+/* Main Component */
+const Nav = (props) => {
+  const { arcSite } = useAppContext();
+
+  const { primaryLogo, primaryLogoAlt } = getProperties(arcSite);
+
+  const {
+    'primary-font-family': primaryFont,
+  } = getThemeStyle(arcSite);
+
+  const { children = [], customFields: { hierarchy, signInOrder } = {} } = props;
+
+  // signInOrder is 1-based instead of 0-based, so we subtract 1
+  const signInButton = (Number.isInteger(signInOrder) && children[signInOrder - 1])
+    ? children[signInOrder - 1]
+    : null;
+
+  const mainContent = useContent({
+    source: 'site-service-hierarchy',
+    query: {
+      site: arcSite,
+      hierarchy,
+    },
+  });
+
+  const sections = (mainContent && mainContent.children) ? mainContent.children : [];
+
+  const [isSectionDrawerOpen, setSectionDrawerOpen] = useState(false);
+
+  const handleEscKey = (event) => {
+    if (event.keyCode === 27) {
+      setSectionDrawerOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscKey, true);
+    return () => {
+      window.removeEventListener('keydown', handleEscKey);
+    };
+  });
+
+  return (
+    <>
+      <StyledNav id="main-nav" className="news-theme-navigation" font={primaryFont}>
+
+        <div className="nav-left">
+          <SearchBox iconSize={20} />
+          <button onClick={() => setSectionDrawerOpen(!isSectionDrawerOpen)} className="nav-btn nav-sections-btn border transparent" type="button">
+            <span>Sections</span>
+            <HamburgerMenuIcon fill="white" height={iconSize} width={iconSize} />
+          </button>
+        </div>
+
+        <div className="nav-logo">
+          <a href="/" title={primaryLogoAlt}>
+            {primaryLogo
+              ? <img src={primaryLogo} alt={primaryLogoAlt || 'Navigation bar logo'} />
+              : <ArcLogo />}
+          </a>
+        </div>
+
+        <div className="nav-right">
+          {signInButton}
+        </div>
+      </StyledNav>
+
+      <StyledSectionDrawer id="nav-sections" className={isSectionDrawerOpen ? 'open' : 'closed'} font={primaryFont}>
+        <SectionNav sections={sections}>
+          <SearchBox alwaysOpen />
+        </SectionNav>
+      </StyledSectionDrawer>
+
+      {isSectionDrawerOpen ? <div id="overlay" role="dialog" onKeyDown={handleEscKey} onClick={() => setSectionDrawerOpen(false)} /> : null}
+    </>
+  );
+};
+
+Nav.propTypes = {
+  customFields: PropTypes.shape({
+    hierarchy: PropTypes.string,
+    signInOrder: PropTypes.number,
+  }),
+};
+
+Nav.label = 'Header Nav Chain – Arc Block';
+
+export default Nav;
---
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/default.test.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import getProperties from 'fusion:properties';
+import Navigation from './default';
+import SearchBox from './_children/search-box';
+
+jest.mock('fusion:themes', () => (jest.fn(() => ({}))));
+jest.mock('fusion:properties', () => (jest.fn(() => ({}))));
+jest.mock('fusion:context', () => ({
+  useAppContext: jest.fn(() => ({})),
+}));
+jest.mock('fusion:content', () => ({
+  useContent: jest.fn(() => ({})),
+}));
+
+describe('the header navigation feature for the default output type', () => {
+  it('should be a nav element with class .news-theme-navigation', () => {
+    const wrapper = mount(<Navigation />);
+
+    expect(wrapper.find('nav').hasClass('news-theme-navigation')).toBe(true);
+  });
+
+  it('should render a SearchBox component in the top navbar', () => {
+    const wrapper = mount(<Navigation />);
+
+    expect(wrapper.find('.nav-left').find(SearchBox)).toHaveLength(1);
+  });
+
+  it('should render a SearchBox component in the side navbar', () => {
+    const wrapper = mount(<Navigation />);
+
+    expect(wrapper.find('#nav-sections').find(SearchBox)).toHaveLength(1);
+  });
+
+  it('should render nothing inside the .nav-right', () => {
+    const wrapper = mount(<Navigation />);
+
+    expect(wrapper.find('.nav-right').children()).toHaveLength(0);
+  });
+
+  describe('when the signInOrder customField is set', () => {
+    describe('when no child component exists at the signInOrder index', () => {
+      it('should render nothing inside the .nav-right', () => {
+        const wrapper = mount(
+          <Navigation customFields={{ signInOrder: 2 }}>
+            {[<button type="button">Sign In</button>]}
+          </Navigation>,
+        );
+
+        expect(wrapper.find('.nav-right').children()).toHaveLength(0);
+      });
+    });
+
+    describe('when a child component exists at the signInOrder index', () => {
+      it('should render the child component inside the .nav-right', () => {
+        const wrapper = mount(
+          <Navigation customFields={{ signInOrder: 1 }}>
+            {[<button type="button">Sign In</button>]}
+          </Navigation>,
+        );
+
+        expect(wrapper.find('.nav-right').children()).toHaveLength(1);
+        expect(wrapper.find('.nav-right > button')).toHaveText('Sign In');
+      });
+    });
+  });
+
+  describe('the navigation bar image/logo', () => {
+    describe('when the theme manifest provides a logo url', () => {
+      it('should make the src of the logo the provided image', () => {
+        getProperties.mockImplementation(() => ({ primaryLogo: 'my-nav-logo.svg' }));
+        const wrapper = mount(<Navigation />);
+
+        expect(wrapper.find('div.nav-logo > a > img')).toHaveProp('src', 'my-nav-logo.svg');
+      });
+
+      it('should make the alt text of the logo the default text', () => {
+        const wrapper = mount(<Navigation />);
+
+        expect(wrapper.find('div.nav-logo > a > img')).toHaveProp('alt', 'Navigation bar logo');
+      });
+
+      describe('when the theme manifest provides alt text for the logo', () => {
+        it('should make the alt text of the logo the provided text', () => {
+          getProperties.mockImplementation(() => ({ primaryLogo: 'my-nav-logo.svg', primaryLogoAlt: 'My Nav Logo' }));
+          const wrapper = mount(<Navigation />);
+
+          expect(wrapper.find('div.nav-logo > a > img')).toHaveProp('alt', 'My Nav Logo');
+        });
+      });
+    });
+
+    describe('when the theme does not provide a logo url', () => {
+      it('should render a default SVG', () => {
+        getProperties.mockImplementation(() => ({}));
+        const wrapper = mount(<Navigation />);
+
+        expect(wrapper.find('div.nav-logo svg > title')).toIncludeText('Arc Publishing logo');
+      });
+    });
+  });
+});

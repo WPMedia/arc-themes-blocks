400eba02e26554ee8d194788ae0b5124b5694b4e
jackhowa <jack.howard@washpost.com>
2020-05-06 08:38:14 -0500

    Remove unnec comments, refactor import

--- blocks/resizer-image-content-source-block/sources/resize-image-api.js
+++ blocks/resizer-image-content-source-block/sources/resize-image-api.js
@@ -1,33 +1,28 @@
-import { getResizerParams } from '@wpmedia/resizer-image-block';
+import resizerImageBlock from '@wpmedia/resizer-image-block';
+
+// named import doesn't seem to be working as expected
+const { getResizerParams } = resizerImageBlock;
 
 const params = {
+  // has to be an external image
   raw_image_url: 'text',
 };
 
 // input: raw image url
-
-/*
-don't need the whole data object
-currently a lot of unnec data is being passed into the resizer
-  return resizer.getResizerParams(image.url);
-
-
-*/
-
 // output: object with dimensions and image keys
-
 const fetch = (query) => {
   const { raw_image_url: rawImageUrl } = query;
 
   return getResizerParams(rawImageUrl);
 };
 
+/*
+not exposing api
+http: false
+If false, available to components during server-side rendering. Default is true
+*/
 export default {
   params,
-  /*
-  not exposing api
-  If false, available to components during server-side rendering. Default is true
-  */
   http: false,
   fetch,
 };

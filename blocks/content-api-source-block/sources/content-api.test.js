4a118dd15023181cadfcc940b1b56f61924fa65a
beltrancaliz <beltran.caliz@washpost.com>
2020-06-01 19:34:46 -0500

    Added _id param to content-api

--- blocks/content-api-source-block/sources/content-api.test.js
+++ blocks/content-api-source-block/sources/content-api.test.js
@@ -4,12 +4,16 @@ describe('the content api source block', () => {
   it('should use the proper param types', () => {
     expect(contentApi.params).toEqual({
       website_url: 'text',
+      _id: 'text'
     });
   });
 
   describe('when a site is provided', () => {
     it('should include the "website" query param with the value', () => {
-      const url = contentApi.resolve({ website_url: '/aaaa/bccccd/', 'arc-site': 'wapo' });
+      const url = contentApi.resolve({ 
+        website_url: '/aaaa/bccccd/', 
+        'arc-site': 'wapo' 
+      });
 
       expect(url).toEqual('/content/v4/?website_url=/aaaa/bccccd/&website=wapo');
     });
@@ -31,7 +35,26 @@ describe('the content api source block', () => {
     });
   });
 
-  describe('when a website url is NOT provided', () => {
+  describe('when an _id is provided', () => {
+    it('should set the _id query param', () => {
+      const url = contentApi.resolve({ _id: 'myid' });
+
+      expect(url).toEqual('/content/v4/?_id=myid');
+    });
+  });
+
+  describe('when an _id and website_url are both provided', () => {
+    it('should set the _id query param', () => {
+      const url = contentApi.resolve({ 
+        website_url: '/aaaa/eeeeee/', 
+        _id: 'myid' 
+      });
+
+      expect(url).toEqual('/content/v4/?_id=myid');
+    });
+  });
+
+  describe('when a website url is and an id are NOT provided', () => {
     it('should have an undefined website_url', () => {
       const url = contentApi.resolve({});
 

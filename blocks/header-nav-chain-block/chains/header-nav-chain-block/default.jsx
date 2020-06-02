f4b7c9dab5fa4451016c24e1cd1436a79efa1a02
Joe Grosspietsch <joe.grosspietsch@washpost.com>
2020-06-02 10:26:40 -0500

    Typescript would've caught this🤑 (2)

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/default.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/default.jsx
@@ -107,7 +107,7 @@ const Nav = (props) => {
       <StyledNav id="main-nav" className={`${navColor === 'light' ? 'light' : 'dark'}`} font={primaryFont} navBarColor={navColor}>
         <div className="news-theme-navigation-container news-theme-navigation-bar">
           <div className="nav-left">
-            <SearchBox iconSize={20} navBarColor={navColor} placeholderTqext={phrases.t('header-nav-block.search-text')} />
+            <SearchBox iconSize={20} navBarColor={navColor} placeholderText={phrases.t('header-nav-block.search-text')} />
             <button onClick={hamburgerClick} className={`nav-btn nav-sections-btn border transparent ${navColor === 'light' ? 'nav-btn-light' : 'nav-btn-dark'}`} type="button">
               <span>{phrases.t('header-nav-block.sections-button')}</span>
               <HamburgerMenuIcon fill={null} height={iconSize} width={iconSize} />

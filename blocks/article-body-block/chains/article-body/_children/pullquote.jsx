4156dc819d8e93092b6f9a703d3193dc5cf0cf4b
Matthew Roach <matthew.roach@washpost.com>
2020-06-10 15:27:04 +0100

    PEN-1058 - Switch HTML article body content type to be static
    Also fix all the react unique key warnings for article body

--- blocks/article-body-block/chains/article-body/_children/pullquote.jsx
+++ blocks/article-body-block/chains/article-body/_children/pullquote.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 export default ({ element }) => {
-  const { content_elements: contentElements = [], citation = {} } = element;
+  const { content_elements: contentElements = [], citation = {}, _id: elementId } = element;
 
   // Add to the block quote component if the paragraph is a text and has content
   const pullQuote = [];
   contentElements.forEach((paragraph) => {
     if (paragraph.type === 'text'
       && Object.prototype.hasOwnProperty.call(paragraph, 'content')) {
-      pullQuote.push(<p key={paragraph.id}>{ paragraph.content }</p>);
+      pullQuote.push(<p key={paragraph._id}>{ paragraph.content }</p>);
     }
   });
   if (citation.type === 'text') {
     pullQuote.push(
-      <span className="citation-text">
+      <span key={`text-${elementId}`} className="citation-text">
         &mdash;
         &nbsp;
         {citation.content}
@@ -22,11 +22,14 @@ export default ({ element }) => {
   }
 
   return (
-    <blockquote
-      className="pullquote"
-      cite={element.citation && element.citation.content}
-    >
-      {pullQuote}
-    </blockquote>
+    pullQuote.length === 0 ? null
+      : (
+        <blockquote
+          className="pullquote"
+          cite={element.citation && element.citation.content}
+        >
+          {pullQuote}
+        </blockquote>
+      )
   );
 };

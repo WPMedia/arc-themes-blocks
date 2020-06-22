5b2dc9fc87131afbf3982c7924a7620a865bab11
jackhowa <jack.howard@washpost.com>
2020-06-22 10:53:42 -0500

    Update theme and alc top nav

--- .storybook/arcTheme.js
+++ .storybook/arcTheme.js
@@ -1,63 +1,47 @@
 import { create } from "@storybook/theming/create";
 
 /*
-reference arcpublishing.com
---green: #6bc1ae;
---blue: #46a1dc;
---orange: #f87966;
---turquoise-blue: #2b83c1;
---gray: #a7a9ac;
---binhex: #4ba2da;
---ink-dark: #333;
---ink-light: #fff;
---dark-gray-primary: #3e5364;
---dark-gray-secondary: #869098;
---light-gray-tertiary: #979797;
---light-gray-primary: #dfe3e6;
---light-gray-secondary: #f4f7fb;
---light-blue: #d8ecf8;
+
+ Pickled Bluewood, off-navy #2D3E4A
+ plastic blue #45a1dc
+
 */
-
-// #333 - ink dark (primary)
-// #4ba2da - arc flat blue (secondary)
-// #6bc1ae - sea foam green arc (highlight)
-// white background rgba(244, 247, 251, 0.92)
 export default create({
-	// see documentation for themes
-	// https://storybook.js.org/docs/configurations/theming/
-	base: 'light',
+  // see documentation for themes 
+  // https://storybook.js.org/docs/configurations/theming/
+  base: 'light',
 
-	colorPrimary: '#333',
-	colorSecondary: '#4ba2da',
+  colorPrimary: '#2D3E4A',
+  colorSecondary: '#45a1dc',
 
-	// UI
-	appBg: 'white',
-	appBorderColor: '#6bc1ae',
+  // UI
+  appBg: '#fff',
+  // appContentBg: 'rgba(244, 247, 251, 0.92)',
+  appBorderColor: '#fff',
+  // rounded corners
+  appBorderRadius: 4,
 
-	// rounded corners
-	appBorderRadius: 4,
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
 
-	// Typography
-	fontBase: '"Open Sans", sans-serif',
-	fontCode: 'monospace',
+  // Text colors
+  textColor: 'black',
+  textInverseColor: 'white',
 
-	// Text colors
-	textColor: 'black',
-	textInverseColor: 'white',
+  // Toolbar default and active colors
+  barTextColor: 'white',
+  barSelectedColor: '#45a1dc',
+  barBg: '#2D3E4A',
 
-	// Toolbar default and active colors
-	barTextColor: 'white',
-	barSelectedColor: '#4ba2da',
-	barBg: '#333',
+  // Form colors
+  inputBg: '#fff',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
 
-	// Form colors
-	inputBg: 'white',
-	inputBorder: 'silver',
-	inputTextColor: 'black',
-	inputBorderRadius: 4,
-
-	brandTitle: 'News Theme Blocks',
-	brandUrl: 'https://github.com/WPMedia/fusion-news-theme-blocks',
-	// todo: change this to a legitimate image url
-	brandImage: 'https://arcdesignsystem.com/img/arc-logo-black.svg',
+  brandTitle: 'Theme Blocks',
+  brandUrl: 'https://github.com/WPMedia/fusion-news-theme-blocks',
+  // todo: change this to a legitimate image url
+  // brandImage: 'https://arcdesignsystem.com/img/arc-logo-black.svg',
 });

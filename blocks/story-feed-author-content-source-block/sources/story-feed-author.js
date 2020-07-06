3c3025095f3b11eed52af4d3df77be50e22b1327
Matthew Roach <matthew.roach@washpost.com>
2020-07-06 14:07:27 +0100

    PEN-1152 - remove core components dependency and update content source to have defaults and authorSlug to use quotes in url

--- blocks/story-feed-author-content-source-block/sources/story-feed-author.js
+++ blocks/story-feed-author-content-source-block/sources/story-feed-author.js
@@ -1,10 +1,15 @@
-import source from '@arc-core-components/content-source_story-feed_author-v4';
 import getResizedImageData from '@wpmedia/resizer-image-block';
 
 export default {
-  resolve: source.resolve,
-  schemaName: source.schemaName,
-  params: source.params,
+  resolve: ({
+    authorSlug, feedSize = 8, feedOffset = 0, 'arc-site': website,
+  }) => `/content/v4/search/published?q=credits.by.slug:"${authorSlug}"&size=${feedSize}&from=${feedOffset}&sort=display_date:desc&website=${website}`,
+  schemaName: 'ans-feed',
+  params: {
+    authorSlug: 'text',
+    feedSize: 'number',
+    feedOffset: 'number',
+  },
   // other options null use default functionality, such as filter quality
   transform: (data, query) => getResizedImageData(
     data,

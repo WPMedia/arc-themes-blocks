641ddf49c1c6dc4d89397a99f3fcab2f652ebd30
Nelson Fernandez <nelson@netflux.com.ar>
2020-07-08 15:26:51 -0300

    move code inserted between head and body tags

--- blocks/default-output-block/output-types/__tests__/default.text.jsx
+++ blocks/default-output-block/output-types/__tests__/default.text.jsx
@@ -43,11 +43,23 @@ describe('the default output type', () => {
     });
 
     it('should have script tags', () => {
-      expect(wrapper.find('script').length).toBe(4);
+      expect(wrapper.find('script').length).toBe(3);
     });
 
     it('should have link tags', () => {
       expect(wrapper.find('link').length).toBe(2);
+    });
+  });
+
+  describe('root html layout', () => {
+    const wrapper = shallow(<DefaultOutputType deployment={jest.fn()} metaValue={jest.fn().mockReturnValue('article')} />);
+
+    it('html must have only have head and body tags', () => {
+      const html = wrapper.find('html');
+      expect(html.length).toBe(1);
+      expect(html.children().length).toBe(2);
+      expect(html.children('head').length).toBe(1);
+      expect(html.children('body').length).toBe(1);
     });
   });
 });

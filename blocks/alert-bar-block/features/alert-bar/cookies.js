a8c3409c99fadb63768b06b2d22a030830059881
Nelson Fernandez <nelson@netflux.com.ar>
2020-08-31 17:16:04 -0300

    prevent alert bar from showing again when dismissed

---
+++ blocks/alert-bar-block/features/alert-bar/cookies.js
@@ -0,0 +1,26 @@
+import cookie from 'cookie';
+
+const COOKIE_NAME = 'arcblock_alert-bar';
+
+function twoDaysInFuture() {
+  const future = new Date();
+  future.setDate(future.getDate() + 2);
+  return future;
+}
+
+export function readCookie() {
+  const cookies = cookie.parse(document.cookie);
+  return cookies[COOKIE_NAME];
+}
+
+export function saveCookie(value) {
+  const options = {
+    expires: twoDaysInFuture(),
+    httpOnly: false,
+    maxAge: 60 * 60 * 24 * 2, // 2 days
+    path: '/',
+    sameSite: 'Lax',
+    secure: window.location.protocol === 'https:',
+  };
+  document.cookie = cookie.serialize(COOKIE_NAME, value, options);
+}

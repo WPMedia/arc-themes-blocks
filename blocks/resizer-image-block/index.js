aba4bb8b285ec30a6893d3484f430e33e8433de2
Brent Miller <brent.miller@washpost.com>
2020-09-28 15:35:35 -0500

    compressing resizer params

--- blocks/resizer-image-block/index.js
+++ blocks/resizer-image-block/index.js
@@ -71,7 +71,6 @@ const getResizerParam = (
       thumborParam = thumborParam.filter(focalPointFilter(focalPoint));
     }
     thumborParam = thumborParam.resize(width, height).buildUrl();
-
     const urlSuffix = originalUrl.replace('https://', '');
     const breakpointName = `${width}x${height}`;
     return thumborParam
@@ -138,6 +137,15 @@ export const getResizerParams = (
   // todo: use webp for next gen images spike
   // getParamsByFormat('webp', output);
   getParamsByFormat('jpg', output);
+  // Going to clean the data by stripping off leading slash
+  // and compressing format and quality notation
+  // Since this code is only supporting jpg and filterQuality of 70,
+  // we will just assume as much for now.
+  Object.keys(output).forEach((key) => {
+    if (output[key]) {
+      output[key] = output[key].replace(/\//, '').replace('format(jpg)', 'f=jpg').replace('quality(70)', 'q=70');
+    }
+  });
   return output;
 };
 

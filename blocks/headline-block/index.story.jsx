5002472c79e6258a2aaf2aa120509b5fe57be792
jackhowa <jack.howard@washpost.com>
2020-10-02 15:24:23 -0500

    docs: Simplify headline block to just use knobs

--- blocks/headline-block/index.story.jsx
+++ blocks/headline-block/index.story.jsx
@@ -7,14 +7,6 @@ export default {
   decorators: [withKnobs],
 };
 
-export const shortHeadline = () => (
-  <Headline headlineString="Man Bites Dog" />
-);
-
-export const longHeadline = () => (
-  <Headline headlineString="At home with your kids? Here’s the ultimate parents’ guide to summer activity resources." />
-);
-
 export const customHeadline = () => {
   const headlineString = text('headlineString', 'Man Bites Dog');
 

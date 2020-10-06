47ebb970b9a443707476c888cfd946cb7f296a78
Nelson Fernandez <nelson@netflux.com.ar>
2020-10-05 21:36:35 -0300

    update tests

--- blocks/default-output-block/output-types/_children/chart_beat_code.test.js
+++ blocks/default-output-block/output-types/_children/chart_beat_code.test.js
@@ -1,6 +1,14 @@
 import chartBeatCode from './chart_beat_code';
 
 describe('ChartBeat code', () => {
+  it('must not render if both parameters are missing', () => {
+    expect(chartBeatCode()).toBeFalsy();
+    expect(chartBeatCode(null, null)).toBeFalsy();
+    expect(chartBeatCode(undefined, undefined)).toBeFalsy();
+    expect(chartBeatCode(null, undefined)).toBeFalsy();
+    expect(chartBeatCode(undefined, null)).toBeFalsy();
+  });
+
   it('must not render if accountId missing', () => {
     expect(chartBeatCode(null, 'a')).toBeFalsy();
     expect(chartBeatCode(undefined, 'a')).toBeFalsy();

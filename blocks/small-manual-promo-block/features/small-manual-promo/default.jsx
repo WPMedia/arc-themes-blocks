b6bbb6b2876fd8630cff6da9f104c8a752f0d5b8
amyerz <amy.erz@washpost.com>
2020-10-23 19:26:42 -0700

    PEN-802 small manual promo refactoring

--- blocks/small-manual-promo-block/features/small-manual-promo/default.jsx
+++ blocks/small-manual-promo-block/features/small-manual-promo/default.jsx
@@ -1,81 +1,34 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import getThemeStyle from 'fusion:themes';
-import getProperties from 'fusion:properties';
-import { useFusionContext } from 'fusion:context';
-import { Image } from '@wpmedia/engine-theme-sdk';
-import { useContent } from 'fusion:content';
+
+import getPromoContainer from './_children/promo_container';
+import getPromoStyle from './_children/promo_style';
+import PromoManualHeadline from './_children/promo_manual_headline';
+import PromoManualImage from './_children/promo_manual_image';
 
 import '@wpmedia/shared-styles/scss/_small-promo.scss';
 
-const HeadlineText = styled.h2`
-  font-family: ${(props) => props.primaryFont};
-`;
-
 const SmallManualPromo = ({ customFields }) => {
-  const { arcSite } = useFusionContext();
+  const imagePosition = customFields?.imagePosition || 'right';
 
-  const resizedImageOptions = useContent({
-    source: 'resize-image-api',
-    query: { raw_image_url: customFields.imageURL, 'arc-site': arcSite },
-  });
-  const headlineClass = customFields.showImage ? 'col-sm-xl-8' : 'col-sm-xl-12 no-image-padding';
+  const headline = customFields.showHeadline && customFields.headline
+    && (<PromoManualHeadline customFields={customFields} />);
 
-  const renderWithLink = useCallback((element, props) => (
-    <a
-      href={customFields.linkURL || '#'}
-      className={(props && props.className) || ''}
-      title={customFields.headline}
-      target={customFields.newTab ? '_blank' : '_self'}
-      rel={customFields.newTab ? 'noreferrer noopener' : ''}
-      onClick={!customFields.linkURL ? (evt) => {
-        evt.preventDefault();
-      } : undefined}
-    >
-      {element}
-    </a>
-  ), [customFields.linkURL, customFields.headline, customFields.newTab]);
+  const image = customFields.showImage && customFields.imageURL
+          && (<PromoManualImage customFields={customFields} />);
+
+  const promoContainersStyles = {
+    containerClass: getPromoStyle(imagePosition, 'container'),
+    headlineClass: customFields.showImage
+      ? 'col-sm-xl-8'
+      : 'col-sm-xl-12 no-image-padding',
+    imageClass: 'col-sm-xl-4',
+  };
 
   return (
     <>
       <article className="container-fluid small-promo">
-        <div className="row sm-promo-padding-btm">
-          {(customFields.showHeadline && customFields.headline)
-          && (
-            <div className={headlineClass}>
-              { renderWithLink((
-                <HeadlineText
-                  primaryFont={getThemeStyle(getProperties(arcSite))['primary-font-family']}
-                  className="sm-promo-headline"
-                >
-                  {customFields.headline}
-                </HeadlineText>
-              ), { className: 'sm-promo-headline' }) }
-            </div>
-          )}
-          {(customFields.showImage && customFields.imageURL)
-          && (
-            <div className="col-sm-xl-4 right-aligned-container">
-              { renderWithLink(
-                <Image
-                  url={customFields.imageURL}
-                  alt={customFields.headline}
-                  // small should be 3:2 aspect ratio
-                  smallWidth={105}
-                  smallHeight={70}
-                  mediumWidth={105}
-                  mediumHeight={70}
-                  largeWidth={105}
-                  largeHeight={70}
-                  breakpoints={getProperties(arcSite)?.breakpoints}
-                  resizerURL={getProperties(arcSite)?.resizerURL}
-                  resizedImageOptions={resizedImageOptions}
-                />,
-              )}
-            </div>
-          )}
-        </div>
+        {getPromoContainer(imagePosition, headline, image, promoContainersStyles)}
       </article>
       <hr />
     </>
@@ -110,6 +63,19 @@ SmallManualPromo.propTypes = {
       label: 'Show image',
       defaultValue: true,
       group: 'Show promo elements',
+    }),
+    imagePosition: PropTypes.oneOf([
+      'right', 'left', 'above', 'below',
+    ]).tag({
+      defaultValue: 'right',
+      label: 'Image Position',
+      group: 'Image',
+      labels: {
+        right: 'Image Right',
+        left: 'Image Left',
+        above: 'Image Above',
+        below: 'Image Below',
+      },
     }),
   }),
 };

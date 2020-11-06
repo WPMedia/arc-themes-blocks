c3ac3f036e109f04c0a2f7b34203c3a724d0bac0
jackhowa <jack.howard@washpost.com>
2020-11-06 16:16:43 -0600

    chore: Add tests for blocks not included in testing
    Closes #426

---
+++ blocks/lead-art-block/features/leadart/default.test.jsx
@@ -0,0 +1,5 @@
+// adding test so it's included in test coverage
+// https://github.com/WPMedia/fusion-news-theme-blocks/issues/426
+describe('lead art', () => {
+  xit('has a test', () => {});
+});

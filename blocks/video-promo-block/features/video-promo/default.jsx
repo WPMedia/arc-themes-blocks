ce0eac1d105f250cffdb6b8d9bb7a9f21cd13d6c
amyerz <amy.erz@washpost.com>
2020-11-25 11:06:45 -0800

    PEN-1459 Alert Badge Styling Updates

--- blocks/video-promo-block/features/video-promo/default.jsx
+++ blocks/video-promo-block/features/video-promo/default.jsx
@@ -21,10 +21,12 @@ const DescriptionText = styled.p`
 
 const AlertBadge = styled.span`
   background-color: #db0a07;
-  border-radius: 5px;
+  border-radius: 24px;
   color: #fff;
   display: inline-block;
   padding: 0.3rem 0.8rem;
+  font-size: 0.75rem;
+  line-height: 1.33;
 `;
 
 const VideoPromo = ({ customFields }) => {
@@ -61,7 +63,9 @@ const VideoPromo = ({ customFields }) => {
     <div className="container-fluid video-promo">
       <div className="row">
         <div className="col-sm-xl-12">
-          {alertBadge && <AlertBadge>{alertBadge}</AlertBadge>}
+          <div className="padding-sm-bottom">
+            {alertBadge && <AlertBadge>{alertBadge}</AlertBadge>}
+          </div>
           {title
             && (
             <TitleText

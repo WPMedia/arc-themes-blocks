78d2aeb0b46bb230ae061f335807c91e9893efcd
jackhowa <jack.howard@washpost.com>
2020-11-27 20:30:45 -0600

    fix: Add storybook for offending search box use effect issue
    Include particular child component with duplicated styles. Seemed like if there was a goal of use effect for a side effect it was having issues with should vs is open. possibily due to the initialization and closure

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/search-box.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/search-box.jsx
@@ -1,47 +1,44 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
+// todo: use search import export without es module full path
 import SearchIcon from '@wpmedia/engine-theme-sdk/dist/es/components/icons/SearchIcon';
 
-export default ({
-  alwaysOpen = false, iconSize = 16, placeholderText, navBarColor = 'dark', customSearchAction = null,
-}) => {
-  const [shouldSearchOpen, setShouldSearchOpen] = useState(false);
+function SearchBox(props) {
+  const {
+    alwaysOpen = false,
+    iconSize = 16,
+    placeholderText,
+    navBarColor = 'dark',
+    customSearchAction = null,
+  } = props;
+
+  // initialize as always open true if so
+  const [isSearchBarOpen, setSearchBarOpen] = useState(alwaysOpen);
   const searchInput = useRef(null);
-  let disabledBtn = true;
 
-  useEffect(() => {
-    const el = searchInput.current;
-    if (shouldSearchOpen) {
-      el.focus();
-      // Wait for open searchbar animation to finish
-      setTimeout(() => {
-        disabledBtn = false;
-      }, 250);
-    } else {
-      el.blur();
-    }
-  }, [shouldSearchOpen]);
+  function handleClick(event) {
+    // handling submit manually
+    event.preventDefault();
 
-  const handleSearchBtnMousedown = (event) => {
-    // if open, prevent blur event so we don't get a race condition on click vs blur
-    if (shouldSearchOpen) {
-      event.preventDefault();
-    } else {
-      setShouldSearchOpen(true);
-    }
-  };
-
-  const handleClick = (event) => {
-    if (!disabledBtn) {
-      event.preventDefault();
+    if (isSearchBarOpen) {
+      // if search bar open, then actually search its contents
+      // of unmanaged state input
       if (customSearchAction) {
         customSearchAction(searchInput.current.value);
       } else {
         window.location.href = `/search/${searchInput.current.value}`;
       }
+    } else {
+      // if the search bar is not already open,
+      // then open it and focus the input on click
+      // set focus on open
+      // in some sense this is a side effect that could be a custom useState hook
+      searchInput.current.focus();
+      setSearchBarOpen(true);
     }
-  };
+  }
 
   const handleKey = (event) => {
+    // want to allow typing keys like letters
     if (event.key === 'Enter') {
       event.preventDefault();
       if (customSearchAction) {
@@ -52,17 +49,31 @@ export default ({
     }
   };
 
-  const isSearchBarOpen = shouldSearchOpen || alwaysOpen;
+  // looks like slightly differently nav classes to
+  // header nav. not sure if that's deliberate
+  // if not, then this could be duplicated
   const navClassNames = `nav-search ${isSearchBarOpen ? 'open' : ''} ${navBarColor === 'light' ? 'light' : 'dark'}`;
   const btnClassNames = `nav-btn ${navBarColor === 'light' ? 'nav-btn-light' : 'nav-btn-dark'} transparent${!isSearchBarOpen ? ' border' : ''}`;
   const iconFill = isSearchBarOpen ? '#666666' : null;
 
   return (
     <div className={navClassNames}>
-      <input ref={searchInput} onBlur={() => { setShouldSearchOpen(false); }} onKeyDown={handleKey} type="text" placeholder={placeholderText} />
-      <button className={btnClassNames} onClick={handleClick} onMouseDown={handleSearchBtnMousedown} type="button">
+      <input
+        ref={searchInput}
+        onKeyDown={handleKey}
+        type="text"
+        placeholder={placeholderText}
+      />
+      <button
+        className={btnClassNames}
+        onClick={handleClick}
+        type="button"
+        aria-label={isSearchBarOpen ? "Search the site's content" : 'Open the search input to search the site'}
+      >
         <SearchIcon fill={iconFill} height={iconSize} width={iconSize} />
       </button>
     </div>
   );
-};
+}
+
+export default SearchBox;

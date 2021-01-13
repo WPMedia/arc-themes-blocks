05c0f006c9f5a25f33bd7f5b66877f90569ee0f8
Artem Bogomaz <moldis@bk.ru>
2021-01-13 13:43:28 +0700

    fixup! lint

--- blocks/section-title-block/features/section-title/_children/section-title.test.jsx
+++ blocks/section-title-block/features/section-title/_children/section-title.test.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import SectionTitle from './section-title';
 import {
-  mockOneSection, mockTwoSection, mockNoChildren, mockTwoSectionWithUrl 
+  mockOneSection, mockTwoSection, mockNoChildren, mockTwoSectionWithUrl,
 } from './mock-data';
 
 jest.mock('fusion:themes', () => (jest.fn(() => ({}))));

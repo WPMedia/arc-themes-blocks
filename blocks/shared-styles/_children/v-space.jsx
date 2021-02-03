356ac4ed004fe4dff9513dadea924be38f4a5faf
Matthew Roach <matthewroach@users.noreply.github.com>
2021-02-03 21:44:41 +0000

    Add a VSpace helper component for setting spacing between child elements

---
+++ blocks/shared-styles/_children/v-space.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { framework } from '@wpmedia/news-theme-css/js/framework';
+
+const VSpaceStyles = styled.div.attrs((props) => ({
+  space: props.space || 'lg',
+  breakpoint: props.breakpoint || 'md',
+  breakpointSpace: props.breakpointSpace || 'md',
+}))`
+  ${({ space }) => space && `
+    > * {
+      margin-bottom: ${framework.spacers[space]};
+    }
+  `}
+
+  @media screen and (min-width: ${({ breakpoint }) => framework.gridBreakpoints[breakpoint]}) {
+    > * {
+      margin-bottom: ${({ breakpointSpace }) => framework.spacers[breakpointSpace]};
+    }
+  }
+`;
+
+const VSpace = (props) => {
+  console.log(props);
+  return <VSpaceStyles {...props} />
+}
+
+export default VSpace;

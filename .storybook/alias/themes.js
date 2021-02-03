66624276c11c99487252fe186028fcb55bb290e1
Matthew Roach <matthewroach@users.noreply.github.com>
2021-02-03 21:42:21 +0000

    Add mock theme settings

--- .storybook/alias/themes.js
+++ .storybook/alias/themes.js
@@ -1,5 +1,7 @@
 const themes = () => ({
-	primaryFont: ''
+	'primary-font-family': 'Arial',
+	'primary-color': 'rgb(255, 105, 180)',
+	'secondary-color': 'rgb(0, 0, 0)'
 })
 
 export default themes;

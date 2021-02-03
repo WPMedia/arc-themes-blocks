8f0180bc6da4c1cd7ec2840b74c7b27657c6c310
jackhowa <jack.howard@washpost.com>
2021-02-03 17:10:50 -0600

    Factor out compose hooks, simplify headline string

--- blocks/headline-block/features/headline/_children/headline.jsx
+++ blocks/headline-block/features/headline/_children/headline.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { PrimaryFont } from '@wpmedia/shared-styles';
-import './headline.scss';
+import '../headline.scss';
 
 // presentational component handles only visual
 const Headline = ({
-  useFusionContext: {
-    globalContent: {
-      headlines: {
-        basic: headlineString = '',
-      },
-    },
-  },
+  headlineString = '',
 }) => (
   /*
     if string is default empty from presentational container,
@@ -20,18 +14,6 @@ const Headline = ({
   <>
     <PrimaryFont
       as="h1"
-      className="headline"
-      fontColor="primary-color"
-      // dangerouslySetInnerHTML seems to be a pattern for blocks
-      dangerouslySetInnerHTML={{ __html: headlineString }}
-    />
-    <PrimaryFont
-      as="h2"
-    >
-      {headlineString}
-    </PrimaryFont>
-    <PrimaryFont
-      as="h3"
       className="headline"
       fontColor="primary-color"
       // dangerouslySetInnerHTML seems to be a pattern for blocks

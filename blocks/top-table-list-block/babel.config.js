1d45441fe304d635fbb529bc6830e5c275839dfb
babadofar <christoffer.vig@gmail.com>
2021-03-02 10:01:18 +0100

    chore: more adding blocks in package.json2

---
+++ blocks/top-table-list-block/babel.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  ignore: [
+    "./dist",
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'commonjs',
+      },
+    ],
+    '@babel/preset-react'],
+  env: {
+    test: {
+      plugins: [ 
+        'transform-react-remove-prop-types',
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          },
+        ],
+        '@babel/plugin-proposal-class-properties',
+        ['module-resolver', {
+          alias: {
+            'fusion:themes': './jest/mocks/themes.js',
+            'fusion:content': './jest/mocks/content.js',
+            'fusion:context': './jest/mocks/context.js',
+            'fusion:consumer': './jest/mocks/consumer.js',
+            'fusion:environment': './jest/mocks/environment.js',
+            'fusion:properties': './jest/mocks/properties.js',
+            'fusion:static': './jest/mocks/static.js',
+            'fusion:intl': './jest/mocks/intl.js',
+          },
+        }],
+      ],
+    },
+  },
+};

ca076a353f20c39f418f98067c1c535405de8287
babadofar <christoffer.vig@gmail.com>
2021-03-02 09:16:38 +0100

    chore: more adding blocks in package.json

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  ignore: [
+    "./dist",
+  ],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],

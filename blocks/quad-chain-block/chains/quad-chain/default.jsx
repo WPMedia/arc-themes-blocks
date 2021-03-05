fd3dc123e3933fc90914ad026c5580943864204d
Matthew Roach <matthewroach@users.noreply.github.com>
2021-03-05 14:40:50 +0000

    Remove *

--- blocks/quad-chain-block/chains/quad-chain/default.jsx
+++ blocks/quad-chain-block/chains/quad-chain/default.jsx
@@ -72,7 +72,7 @@ QuadChain.propTypes = {
     }),
     ...spacerCustomField('spacing', 'Default Vertical block spacing', 'Vertical spacing between blocks', 'lg', 'Select the spacing token used vertically between blocks in this chain. The default in Themes is Large, except for when placing small promos within a chain, when Medium should be selected.'),
     ...breakpointCustomField('spacingBreakpoint', 'Vertical block spacing breakpoint', 'Vertical spacing between blocks', 'md', 'Select the horizontal breakpoint that will be used as the point the above breakpoint spacing token will be used'),
-    ...spacerCustomField('spacingAboveBreakpoint', 'Vertical block spacing above breakpoint', 'Vertical spacing between blocks', 'md', 'Spacing token that is used above the specified breakpoint. The default in Themes is Medium, except for when placing small promos within a chain*, when Small should be selected'),
+    ...spacerCustomField('spacingAboveBreakpoint', 'Vertical block spacing above breakpoint', 'Vertical spacing between blocks', 'md', 'Spacing token that is used above the specified breakpoint. The default in Themes is Medium, except for when placing small promos within a chain, when Small should be selected'),
   }),
 };
 

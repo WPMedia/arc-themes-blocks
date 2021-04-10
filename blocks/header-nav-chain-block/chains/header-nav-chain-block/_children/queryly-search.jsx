537dee54b1281bcd2f0be5e63ac728df2be6e331
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-10 08:59:11 +0100

    Add click handler with comment to make queryly button accessible

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.jsx
@@ -1,10 +1,30 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import SearchIcon from '@wpmedia/engine-theme-sdk/dist/es/components/icons/SearchIcon';
+import { SearchIcon } from '@wpmedia/engine-theme-sdk';
+
+/*
+  This querylySearchClick event isn't the ideal solution -
+
+  Queryly uses a hidden checkbox in the DOM which they have an
+  onChange event listener attached to. As we are using a label
+  with the htmlFor attribute that is linked to the ID of their
+  checkbox the change event is fired using the mouse.
+
+  The issue is label's are not a focusable element. But a
+  button is. But events do not bubble downwards, and also
+  due to their use of the checkbox we have to manaully
+  check the checkbox and they dispatch a change event so their
+  code picks it up and loads the queryly search UI.
+*/
+const querylySearchClick = () => {
+  const event = new Event('change');
+  document.getElementById('queryly_toggle').checked = true;
+  document.getElementById('queryly_toggle').dispatchEvent(event);
+};
 
 const QuerylySearch = ({ theme = 'dark', iconSize = 16 }) => (
   <div className={`nav-search ${theme} queryly`}>
-    <button className={`nav-btn nav-btn-${theme} transparent border`} type="button">
+    <button className={`nav-btn nav-btn-${theme} transparent border`} type="button" onClick={querylySearchClick}>
       <label htmlFor="queryly_toggle">
         <SearchIcon height={iconSize} width={iconSize} />
       </label>

4dfb2e6764c9e459333520a5ff646e8e76cc426b
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-14 15:11:34 +0100

    Allow background-color to be set for primary and secondary font components

--- blocks/shared-styles/_children/secondary-font.jsx
+++ blocks/shared-styles/_children/secondary-font.jsx
@@ -6,11 +6,16 @@ import { useFusionContext } from 'fusion:context';
 const SecondaryFontStyles = styled.div.attrs((props) => ({
   arcSite: props.arcSite,
   fontColor: props.fontColor || null,
+  backgroundColor: props.backgroundColor || null,
 }))`
   font-family: ${({ arcSite }) => getThemeStyle(arcSite)['secondary-font-family']};
 
   ${({ arcSite, fontColor }) => fontColor && `
     color: ${getThemeStyle(arcSite)[fontColor]};
+  `}
+
+  ${({ arcSite, backgroundColor }) => backgroundColor && `
+    background-color: ${getThemeStyle(arcSite)[backgroundColor]};
   `}
 `;
 

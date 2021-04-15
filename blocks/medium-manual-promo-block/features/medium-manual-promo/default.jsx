f8814930336ecdf4bd93b640e018a7f8426086be
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-15 16:03:48 +0100

    Remove un used import

--- blocks/medium-manual-promo-block/features/medium-manual-promo/default.jsx
+++ blocks/medium-manual-promo-block/features/medium-manual-promo/default.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import getThemeStyle from 'fusion:themes';

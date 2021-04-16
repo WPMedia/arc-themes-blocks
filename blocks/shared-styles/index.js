7cdd5e94e1727c03a378a78b6d23ddda6e470a05
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-16 10:13:52 +0100

    Extract Overline component to shared styles

--- blocks/shared-styles/index.js
+++ blocks/shared-styles/index.js
@@ -1,3 +1,4 @@
+import Overline from './_children/overline';
 import PromoHeadline from './_children/promo-headline';
 import PromoLabel from './_children/promo-label';
 import PromoImage from './_children/promo-image';
@@ -7,6 +8,7 @@ import SmallPromoContainer from './_children/promo-helpers/small/container';
 import SmallPromoStyles from './_children/promo-helpers/small/styles';
 
 export {
+  Overline,
   PromoHeadline,
   PromoLabel,
   PromoImage,

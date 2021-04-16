02a880d15a54d1f9319e2bd8ecc259a4c0dd685d
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-16 11:16:05 +0100

    Add PromoData component to shared styles

--- blocks/shared-styles/index.js
+++ blocks/shared-styles/index.js
@@ -1,4 +1,5 @@
 import Overline from './_children/overline';
+import PromoDate from './_children/promo-date';
 import PromoDescription from './_children/promo-description';
 import PromoHeadline from './_children/promo-headline';
 import PromoLabel from './_children/promo-label';
@@ -10,6 +11,7 @@ import SmallPromoStyles from './_children/promo-helpers/small/styles';
 
 export {
   Overline,
+  PromoDate,
   PromoDescription,
   PromoHeadline,
   PromoLabel,

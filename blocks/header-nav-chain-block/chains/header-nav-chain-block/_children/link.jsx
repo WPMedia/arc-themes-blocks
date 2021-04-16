a753ed4f42cfab6830b48a9b7bda4e719dfba562
JackHowa <jack.howard@washpost.com>
2021-04-16 15:40:42 -0500

    feat: Factor out format url and rename import to engine theme sdk

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/link.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/link.jsx
@@ -1,34 +1,5 @@
 import React from 'react';
-
-function getLocation(uri) {
-  let url;
-  if (typeof window === 'undefined') {
-    url = new URL(uri, 'http://example.com');
-  } else {
-    url = document.createElement('a');
-    // IE doesn't populate all link properties when setting .href with a relative URL,
-    // however .href will return an absolute URL which then can be used on itself
-    // to populate these additional fields.
-    url.href = uri;
-    if (url.host === '') {
-      url.href = `${url.href}`;
-    }
-  }
-  return url;
-}
-
-function fixTrailingSlash(item) {
-  const url = getLocation(item);
-
-  if (url.hash || url.search || url.pathname.match(/\./)) {
-    return item;
-  }
-
-  if (item[item.length - 1] !== '/') {
-    return `${item}/`;
-  }
-  return item;
-}
+import { formatURL } from '@wpmedia/engine-theme-sdk';
 
 const Link = ({
   href, name, child, isHidden = false,
@@ -37,13 +8,13 @@ const Link = ({
   const linkAttributes = isHidden ? { tabIndex: -1 } : {};
   return (
     externalUrl ? (
-      <a href={fixTrailingSlash(href)} target="_blank" rel="noopener noreferrer" {...linkAttributes}>
+      <a href={formatURL(href)} target="_blank" rel="noopener noreferrer" {...linkAttributes}>
         {name}
         <span className="sr-only">(Opens in new window)</span>
         {child}
       </a>
     ) : (
-      <a href={fixTrailingSlash(href)} {...linkAttributes}>
+      <a href={formatURL(href)} {...linkAttributes}>
         {name}
         {child}
       </a>

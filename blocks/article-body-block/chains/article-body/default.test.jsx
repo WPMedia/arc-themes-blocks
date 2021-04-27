0ff44850623eb9a038b45c1d07ce2f0ad158d50f
Nick Schubach <nicholas.schubach@washpost.com>
2021-04-27 09:20:32 -0400

    Add negative test for no copyright

--- blocks/article-body-block/chains/article-body/default.test.jsx
+++ blocks/article-body-block/chains/article-body/default.test.jsx
@@ -1943,5 +1943,38 @@ describe('article-body chain', () => {
       );
       expect(wrapper.find('article.article-body-wrapper').find('p.body-copyright').last().text()).toEqual('Copyright 2021 - Copyright Holder');
     });
+    it('should not render copyright after content if it does not exist', () => {
+      jest.mock('fusion:context', () => ({
+        useFusionContext: jest.fn(() => ({
+          globalContent: {
+            _id: 'NGGXZJ4HAJH5DI3SS65EVBMEMQ',
+            type: 'story',
+            version: '0.10.6',
+            content_elements: [
+              {
+                _id: 'TLF25CWTCBBOHOVFPK4C2RR5JA',
+                type: 'text',
+                additional_properties: {
+                  comments: [],
+                  inline_comments: [],
+                },
+                content: 'Paragraph with Copyright Following',
+              },
+            ],
+          },
+          arcSite: 'the-sun',
+          customFields: { },
+        })),
+      }));
+      const { default: ArticleBodyChain } = require('./default');
+      const wrapper = mount(
+        <ArticleBodyChain>
+          <div>1</div>
+          <div>2</div>
+          <span>3</span>
+        </ArticleBodyChain>,
+      );
+      expect(wrapper.find('article.article-body-wrapper').find('p.body-copyright').length).toEqual(0);
+    });
   });
 });

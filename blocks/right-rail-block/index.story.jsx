f6ba69fa87507dbc8ce17df40c8dbd40a3b9d3d7
Matthew Roach <matthewroach@users.noreply.github.com>
2021-04-27 11:58:02 +0100

    Update right rail story to not rely on other blocks

--- blocks/right-rail-block/index.story.jsx
+++ blocks/right-rail-block/index.story.jsx
@@ -1,38 +1,41 @@
 /* eslint-disable react/no-children-prop */
 import React from 'react';
 import RightRailBlock from './layouts/right-rail/default';
-import Navigation from '../header-nav-block/features/navigation/default';
-
-import Masthead from '../masthead-block/features/masthead-block/default';
-// import ExtraLargeManualPromo from
-//  '../extra-large-manual-promo-block/features/extra-large-manual-promo/default';
 
 export default { title: 'Blocks/Right Rail Block' };
 
-const navigationComponents = () => (
-  <Navigation />
-);
+const layoutItemStyles = {
+  navigation: {
+    backgroundColor: 'rgb(236 155 223)',
+    height: '100px',
+  },
+  footer: {
+    height: '100px',
+  },
+  main: {
+    height: '800px',
+  },
+  rightRail: {
+    height: '600px',
+  },
+  default: {
+    width: '100%',
+    backgroundColor: 'rgb(230 230 230)',
+    textAlign: 'center',
+  },
+};
+const getStyles = (name) => {
+  const defaults = layoutItemStyles.default;
 
-// https://arc-anglerfish-staging-staging.s3.amazonaws.com/public/MBMXUMZQZZCWBMD3Z4BY6FYFF4.png
-//   const [navigation, fullWidth1, main, rightRail, fullWidth2, footer] = children;
-
-const mastheadCustomFields = {
-  tagLine: 'Customization Thrives in the Light',
-  promoLinkURL: 'https://duckduckgo.com/?va=z&t=hc',
-  promoLinkText: 'Promo',
-  logoURL: 'https://arc-anglerfish-staging-staging.s3.amazonaws.com/public/MBMXUMZQZZCWBMD3Z4BY6FYFF4.png',
-  showDate: true,
+  return { ...defaults, ...layoutItemStyles[name] } || defaults;
 };
 
-const fullWidth1Components = () => (
-  <Masthead customFields={mastheadCustomFields} />
-);
-// mainComponents()
-// const mainComponents = () => (
-//   <ExtraLargeManualPromo />
-// );
+const layoutItem = (name) => <div style={getStyles(name)}>{name}</div>;
+
+const layoutAreas = ['navigation', 'fullWidth1', 'main', 'rightRail', 'fullWidth2', 'footer'];
+
 export const basic = () => (
   <RightRailBlock
-    children={[navigationComponents(), fullWidth1Components()]}
+    children={layoutAreas.map((name) => layoutItem(name))}
   />
 );

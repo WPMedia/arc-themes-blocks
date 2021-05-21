2e141ede4d2ae734e6dc016bcda96d03180b18bb
farleyawapo <andre.farley@washpost.com>
2021-05-21 11:55:20 -0500

    overline imports removed

--- blocks/overline-block/features/overline/default.jsx
+++ blocks/overline-block/features/overline/default.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { useFusionContext } from 'fusion:context';
 import { useEditableContent } from 'fusion:content';
 import getThemeStyle from 'fusion:themes';

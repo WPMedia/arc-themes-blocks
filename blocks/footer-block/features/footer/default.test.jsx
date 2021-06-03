ec4c39aacbab2f849d2a60e310dc33a3eed062db
farleyawapo <andre.farley@washpost.com>
2021-06-03 10:53:39 -0500

    footer tests updated

--- blocks/footer-block/features/footer/default.test.jsx
+++ blocks/footer-block/features/footer/default.test.jsx
@@ -145,8 +145,8 @@ describe('the footer feature for the default output type', () => {
 
   it('should have 5 column headers', () => {
     const wrapper = mount(<Footer customFields={{ navigationConfig: { contentService: 'footer-service', contentConfiguration: {} } }} />);
-
-    expect(wrapper.find('.footer-section > h4')).toHaveLength(5);
+    
+    expect(wrapper.find('h4.footer-header')).toHaveLength(5);
   });
 
   it('should have 12 column items', () => {

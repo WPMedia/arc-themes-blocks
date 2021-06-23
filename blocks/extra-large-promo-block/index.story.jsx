8e16cd0878fb76d2131a3dbc03d6541cc693971e
Jack Howard <jack.howard@washpost.com>
2021-06-23 10:00:13 -0500

    docs: Storybook start for promo

---
+++ blocks/extra-large-promo-block/index.story.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Promo from './features/extra-large-promo/default';
+
+export default {
+  title: 'Blocks/Extra Large Promo',
+};
+
+export const noRenderPromo = () => (
+  <Promo customFields={null} />
+);

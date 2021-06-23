4d0cd5909ddc6e6e8ec51f2c74af8f40900c09e3
Nick Schubach <nicholas.schubach@washpost.com>
2021-06-23 10:44:41 -0400

    Fix for text output type compilation issue JSX v JS found in testing

--- blocks/text-output-block/output-types/text.js
+++ blocks/text-output-block/output-types/text.js
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types';
 
-const TextOutputType = (props) => props.children;
+const TextOutputType = ({ children }) => {
+  const generateText = (child) => {
+    if (Array.isArray(child)) return child.map(generateText).join('\n');
+    return child;
+  }
+  return generateText(children);
+};
 
 TextOutputType.contentType = 'text/plain';
 TextOutputType.fallback = false;

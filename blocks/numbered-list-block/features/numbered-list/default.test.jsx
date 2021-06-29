aaf8cae067d937d66feb0f787fedf5e54a58fc97
Matthew Roach <matthewroach@users.noreply.github.com>
2021-06-29 13:40:19 +0100

    Remove unused code

--- blocks/numbered-list-block/features/numbered-list/default.test.jsx
+++ blocks/numbered-list-block/features/numbered-list/default.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { useFusionContext } from 'fusion:context';
 
 jest.mock('fusion:properties', () => (jest.fn(() => ({
   fallbackImage: 'placeholder.jpg',

6d8854a50dd53d215b1132923659488455d3360f
Matthew Roach <matthewroach@users.noreply.github.com>
2021-07-19 19:43:18 +0100

    Export VSpace component via shared styles index.js

--- blocks/shared-styles/index.js
+++ blocks/shared-styles/index.js
@@ -11,6 +11,7 @@ import PromoLabel from './_children/promo-label';
 import SecondaryFont from './_children/secondary-font';
 import SmallPromoContainer from './_children/promo-helpers/small/container';
 import SmallPromoStyles from './_children/promo-helpers/small/styles';
+import VSpace from './_children/vspace';
 
 export {
   Byline,
@@ -26,4 +27,5 @@ export {
   SecondaryFont,
   SmallPromoContainer,
   SmallPromoStyles,
+  VSpace,
 };

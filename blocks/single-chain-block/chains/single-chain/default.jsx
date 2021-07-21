708f8bd63393d3a3b016db72391feab817a72c4b
Matthew Roach <matthewroach@users.noreply.github.com>
2021-07-21 16:39:50 +0100

    Update single chain to use VSpace component for layout spacing and exposing separator custom field

--- blocks/single-chain-block/chains/single-chain/default.jsx
+++ blocks/single-chain-block/chains/single-chain/default.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import PropTypes from '@arc-fusion/prop-types';
-import { Heading, HeadingSection } from '@wpmedia/shared-styles';
+import { Heading, HeadingSection, VSpace } from '@wpmedia/shared-styles';
 
 import '@wpmedia/shared-styles/scss/_chains.scss';
 
 const SingleChain = ({ children, customFields = {} }) => {
-  const { heading = null } = customFields;
+  const { heading = null, childrenSeparator = false } = customFields;
+
+  const Wrapper = children && children.length ? VSpace : React.Fragment;
+  const wrapperProps = children && children.length ? {
+    space: 'md',
+    breakpointSpace: 'sm',
+    childrenSeparator,
+  } : null;
+  const ChildrenOutput = <Wrapper {...wrapperProps}>{children}</Wrapper>;
 
   if (!heading) {
-    return <>{children}</>;
+    return ChildrenOutput;
   }
 
   return (
     <HeadingSection>
       <Heading className="chain-heading">{heading}</Heading>
-      {children}
+      {ChildrenOutput}
     </HeadingSection>
   );
 };
@@ -26,6 +34,10 @@ SingleChain.propTypes = {
   customFields: PropTypes.shape({
     heading: PropTypes.string.tag({
       label: 'Heading',
+    }),
+    childrenSeparator: PropTypes.bool.tag({
+      label: 'Add a separator between features',
+      defaultValue: false,
     }),
   }),
 };

ecf9c1a87f7367dee5af706065445cde7b367944
Jack Howard <jack.howard@washpost.com>
2021-07-26 09:25:15 -0500

    perf: Use story-feed tag inline rather than import via package

--- blocks/story-feed-tag-content-source-block/sources/story-feed-tag.js
+++ blocks/story-feed-tag-content-source-block/sources/story-feed-tag.js
@@ -1,6 +1,39 @@
-/* eslint-disable import/no-unresolved */
-import source from '@arc-core-components/content-source_story-feed_tag-v4';
 import getResizedImageData from '@wpmedia/resizer-image-block';
+
+// via https://github.com/wapopartners/core-components/blob/dev/packages/content-source_story-feed_tag-v4/src/index.js#L19
+// Default values for the Core Component
+const schemaName = 'ans-feed';
+
+const params = {
+  tagSlug: 'text',
+  feedSize: 'number',
+  feedOffset: 'number',
+};
+
+/**
+* @func pattern
+* @param {Object} key
+* @return {String} slugified content api search url
+*/
+const pattern = (key = {}) => {
+  const website = key['arc-site'] || 'Arc Site is not defined';
+  const { tagSlug, feedOffset, feedSize } = key;
+
+  return `/content/v4/search/published?q=taxonomy.tags.slug:${tagSlug}&size=${feedSize}&from=${feedOffset}&sort=display_date:desc&website=${website}`;
+};
+
+/**
+* @func resolve
+* @param {Object} key - the object key to return a slugified pattern for
+* @return {String} slugified content api search url
+*/
+const resolve = (key) => pattern(key);
+
+const source = {
+  resolve,
+  schemaName,
+  params,
+};
 
 export default {
   resolve: source.resolve,

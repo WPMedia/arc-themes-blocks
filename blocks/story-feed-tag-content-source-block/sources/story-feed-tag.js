0130297e094e082ac53a8b4784e5001aa8a05f80
Jack Howard <jack.howard@washpost.com>
2021-07-26 09:36:13 -0500

    feat: Throw error if tag slug not present

--- blocks/story-feed-tag-content-source-block/sources/story-feed-tag.js
+++ blocks/story-feed-tag-content-source-block/sources/story-feed-tag.js
@@ -1,9 +1,5 @@
 import getResizedImageData from '@wpmedia/resizer-image-block';
 
-// via https://github.com/wapopartners/core-components/blob/dev/packages/content-source_story-feed_tag-v4/src/index.js#L19
-// Default values for the Core Component
-const schemaName = 'ans-feed';
-
 const params = {
   tagSlug: 'text',
   feedSize: 'number',
@@ -19,6 +15,10 @@ const pattern = (key = {}) => {
   const website = key['arc-site'] || 'Arc Site is not defined';
   const { tagSlug, feedOffset, feedSize } = key;
 
+  if (!tagSlug) {
+    throw new Error('tagSlug parameter is required');
+  }
+
   return `/content/v4/search/published?q=taxonomy.tags.slug:${tagSlug}&size=${feedSize}&from=${feedOffset}&sort=display_date:desc&website=${website}`;
 };
 
@@ -29,16 +29,11 @@ const pattern = (key = {}) => {
 */
 const resolve = (key) => pattern(key);
 
-const source = {
-  resolve,
-  schemaName,
-  params,
-};
-
 export default {
-  resolve: source.resolve,
-  schemaName: source.schemaName,
-  params: source.params,
+  resolve,
+  schemaName: 'ans-feed',
+  params,
+
   // other options null use default functionality, such as filter quality
   transform: (data, query) => (
     getResizedImageData(

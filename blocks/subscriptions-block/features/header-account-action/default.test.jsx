c742618df8a8392b156c28a0b609e92555751890
Matthew Roach <matthewroach@users.noreply.github.com>
2021-07-29 18:45:02 +0100

    spelling mistake

--- blocks/subscriptions-block/features/header-account-action/default.test.jsx
+++ blocks/subscriptions-block/features/header-account-action/default.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import HeaderAccountAction from './default';
 
-describe('Subscripitons HeaderAccountAction', () => {
+describe('Subscriptions HeaderAccountAction', () => {
   it('renders', () => {
     const wrapper = mount(<HeaderAccountAction />);
     expect(wrapper.html()).not.toBe(null);

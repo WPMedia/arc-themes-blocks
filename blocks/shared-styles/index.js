9c0a1e845f68553f3f3e97b953d18904e5e1ef17
farleyawapo <andre.farley@washpost.com>
2021-08-02 14:56:41 -0500

    extra-large-promo-block

--- blocks/shared-styles/index.js
+++ blocks/shared-styles/index.js
@@ -12,9 +12,11 @@ import SecondaryFont from './_children/secondary-font';
 import SmallPromoContainer from './_children/promo-helpers/small/container';
 import SmallPromoPresentation from './_children/promos/small';
 import SmallPromoStyles from './_children/promo-helpers/small/styles';
+import ExtraLargePromoPresentation from './_children/promos/extra-large';
 
 export {
   Byline,
+  ExtraLargePromoPresentation,
   Heading,
   HeadingSection,
   Overline,

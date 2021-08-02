30badf041ab9b2ac6003768a2940d67f3ceb4768
Jack Howard <jack.howard@washpost.com>
2021-08-02 13:13:59 -0500

    docs: Add not found social media and really long bios

--- blocks/author-bio-block/index.story.jsx
+++ blocks/author-bio-block/index.story.jsx
@@ -197,3 +197,48 @@ export const threeAuthors = () => {
   };
   return (<AuthorBioItems content={data} />);
 };
+
+export const reallyLongAuthorName = () => {
+  const reallyLongAuthorNameObject = {
+    ...authorObject,
+    additional_properties: {
+      original: {
+        ...authorObject.additional_properties.original,
+        byline: 'Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas Uncas Victor William Xerxes Yancy Zeus Wolfeschlegel­steinhausen­bergerdorff­welche­vor­altern­waren­gewissenhaft­schafers­wessen­schafe­waren­wohl­gepflege­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­ihr­raubgierig­feinde­welche­vor­altern­zwolfhundert­tausend­jahres­voran­die­erscheinen­von­der­erste­erdemensch­der­raumschiff­genacht­mit­tungstein­und­sieben­iridium­elektrisch­motors­gebrauch­licht­als­sein­ursprung­von­kraft­gestart­sein­lange­fahrt­hinzwischen­sternartig­raum­auf­der­suchen­nachbarschaft­der­stern­welche­gehabt­bewohnbar­planeten­kreise­drehen­sich­und­wohin­der­neue­rasse­von­verstandig­menschlichkeit­konnte­fortpflanzen­und­sich­erfreuen­an­lebenslanglich­freude­und­ruhe­mit­nicht­ein­furcht­vor­angreifen­vor­anderer­intelligent­geschopfs­von­hinzwischen­sternartig­raum Sr',
+      },
+    },
+  };
+
+  const data = {
+    credits: { by: [reallyLongAuthorNameObject] },
+  };
+  return (<AuthorBioItems content={data} />);
+};
+
+export const reallyLongBioString = () => {
+  const reallyLongAuthorNameObject = {
+    ...authorObject,
+    description: `
+    Hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor. Hodor hodor hodor hodor hodor hodor hodor hodor hodor.
+    `,
+  };
+
+  const data = {
+    credits: { by: [reallyLongAuthorNameObject] },
+  };
+  return (<AuthorBioItems content={data} />);
+};
+
+export const notFoundSocialMedia = () => {
+  const notFoundSocialMediaObject = {
+    ...authorObject,
+    social_links: [
+      { site: 'newnew', url: 'f' },
+    ],
+  };
+
+  const data = {
+    credits: { by: [notFoundSocialMediaObject] },
+  };
+  return (<AuthorBioItems content={data} />);
+};

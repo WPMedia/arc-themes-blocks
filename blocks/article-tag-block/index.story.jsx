d3c0b98dd3bedb641751bf23efeef3c8e29e131f
Jack Howard <jack.howard@washpost.com>
2021-08-03 10:14:03 -0500

    docs: Update and refactor the setup for links bar for storybook

--- blocks/article-tag-block/index.story.jsx
+++ blocks/article-tag-block/index.story.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
-// import ArticleTagBlock from './features/tag/default';
+import { ArticleTagItems } from './features/tag/default';
 
 export default {
   title: 'Blocks/Tags Bar',
@@ -11,4 +11,95 @@ export default {
   },
 };
 
-export const showSomething = () => <p>ya</p>;
+const data = {
+  taxonomy: {
+    tags: [{
+      description: 'dogs',
+      slug: 'dogs slug',
+      text: 'dogs text',
+    }, {
+      description: 'cats',
+      slug: 'cats slug',
+      text: 'cats text',
+    }],
+  },
+};
+
+export const showAllFields = () => {
+  const allData = data;
+  return (
+    <ArticleTagItems content={allData} arcSite="story-book" />
+  );
+};
+
+export const noTags = () => {
+  const noData = {
+    taxonomy: {
+      tags: [],
+    },
+  };
+  return (
+    <ArticleTagItems content={noData} arcSite="story-book" />
+  );
+};
+
+export const noDescription = () => {
+  const noDescriptionFieldData = {
+    taxonomy: {
+      tags: [{
+        slug: 'cats slug',
+        text: 'cats text',
+      }],
+    },
+  };
+  return (
+    <ArticleTagItems content={noDescriptionFieldData} arcSite="story-book" />
+  );
+};
+
+export const noSlug = () => {
+  const noSlugFieldData = {
+    taxonomy: {
+      tags: [{
+        description: 'cats',
+        text: 'cats text',
+      }],
+    },
+  };
+  return (
+    <ArticleTagItems content={noSlugFieldData} arcSite="story-book" />
+  );
+};
+
+export const noText = () => {
+  const noTextFieldData = {
+    taxonomy: {
+      tags: [{
+        description: 'cats',
+        slug: 'cats slug',
+      }],
+    },
+  };
+
+  return (
+    <ArticleTagItems content={noTextFieldData} arcSite="story-book" />
+  );
+};
+
+export const manyTags = () => {
+  const tenTags = Array(10).fill(0).map((x, i) => ({
+    description: `tag ${i}`,
+    slug: `tag-${i}`,
+    text: `tag ${i} text`,
+  }));
+
+  const tenData = {
+    taxonomy: {
+      tags: tenTags,
+    },
+  };
+
+  return (
+    <ArticleTagItems content={tenData} arcSite="story-book" />
+  );
+};

8bf564aa370e3dc7dbdfec1e3ed0abb76a1999cb
farleyawapo <andre.farley@washpost.com>
2021-08-06 15:46:14 -0500

    header storybook

--- blocks/header-block/features/header/default.jsx
+++ blocks/header-block/features/header/default.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from '@arc-fusion/prop-types';
 import Consumer from 'fusion:consumer';
 import './header.scss';
 import { PrimaryFont } from '@wpmedia/shared-styles';
---
+++ blocks/header-block/index.story.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Header from './features/header/default';
+
+export default {
+  title: 'Blocks/Header',
+};
+
+export const extralarge = () => {
+  const customFields = {
+    text: 'Baby panda born at the zoo',
+    size: 'Extra Large',
+  };
+
+  return (
+    <Header customFields={customFields} />
+  );
+};
+
+export const large = () => {
+  const customFields = {
+    text: 'Baby panda born at the zoo',
+    size: 'Large',
+  };
+
+  return (
+    <Header customFields={customFields} />
+  );
+};
+
+export const medium = () => {
+  const customFields = {
+    text: 'Baby panda born at the zoo',
+    size: 'Medium',
+  };
+
+  return (
+    <Header customFields={customFields} />
+  );
+};
+
+export const small = () => {
+  const customFields = {
+    text: 'Baby panda born at the zoo',
+    size: 'Small',
+  };
+
+  return (
+    <Header customFields={customFields} />
+  );
+};

81b1b1e0652b75658f21b0aa84e062163d0a79c0
Matthew Roach <matthewroach@users.noreply.github.com>
2021-08-18 14:30:41 +0100

    Add Video stories

--- blocks/video-player-block/index.story.jsx
+++ blocks/video-player-block/index.story.jsx
@@ -8,6 +8,34 @@ export default {
   },
 };
 
-export const base = () => (
+const embedMarkup = '<div class="powa" id="powa-e924e51b-db94-492e-8346-02283a126943" data-org="corecomponents" data-env="prod" data-uuid="e924e51b-db94-492e-8346-02283a126943" data-aspect-ratio="0.562" data-api="prod"><script src="//d2w3jw6424abwq.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>';
+
+const customFields = {
+  alertBadge: 'Alert Badge',
+  title: 'Video Title',
+  description: 'Video description would be rendered here',
+};
+
+export const noProps = () => (
   <VideoPlayer />
+);
+
+export const videoOnly = () => (
+  <VideoPlayer embedMarkup={embedMarkup} />
+);
+
+export const alertTitleAndDescription = () => (
+  <VideoPlayer embedMarkup={embedMarkup} customFields={customFields} />
+);
+
+export const alertBadge = () => (
+  <VideoPlayer embedMarkup={embedMarkup} customFields={{ alertBadge: customFields.alertBadge }} />
+);
+
+export const withTitle = () => (
+  <VideoPlayer embedMarkup={embedMarkup} customFields={{ title: customFields.title }} />
+);
+
+export const withDescription = () => (
+  <VideoPlayer embedMarkup={embedMarkup} customFields={{ description: customFields.description }} />
 );

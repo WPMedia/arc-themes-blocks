550e6722b604a45e146ffa1b704a4dad59db4f5c
Matthew Roach <matthewroach@users.noreply.github.com>
2021-08-19 15:28:52 +0100

    Sort stories to be alphabetical

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,8 @@ export const parameters = {
     options: {},
     manual: false,
   },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 };

01efb29cf440fd6f3f4a16223eafe8808c5df9d9
Nick Schubach <nschubach@gmail.com>
2021-08-31 15:55:19 -0400

    TMEDIA-469: Switch Error Icon to use the SDK Icon

--- blocks/identity-block/components/HeadlinedSubmitForm/index.jsx
+++ blocks/identity-block/components/HeadlinedSubmitForm/index.jsx
@@ -2,6 +2,8 @@ import React, { useRef } from 'react';
 import PropTypes from '@arc-fusion/prop-types';
 
 import { PrimaryFont } from '@wpmedia/shared-styles';
+import { ErrorIcon } from '@wpmedia/engine-theme-sdk';
+
 import Button, { BUTTON_STYLES, BUTTON_SIZES } from '../Button';
 
 import './styles.scss';
@@ -53,14 +55,7 @@ const HeadlinedSubmitForm = ({
         {formErrorText ? (
           <section className="xpmedia-form-error">
             <PrimaryFont as="p">
-              <svg fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.49.463a8.137 8.137 0 0 0-5.674 2.435A7.866 7.866 0 0 0 .501 8.6a7.852 7.852 0 0 0 7.866 7.861h.143a8.073 8.073 0 0 0 7.99-8.138A7.843 7.843 0 0 0 8.49.463zM7.5 11.49a.984.984 0 0 1 .966-1.02h.018c.547.001.995.434 1.016.98a.983.983 0 0 1-.966 1.02h-.018a1.02 1.02 0 0 1-1.016-.98zm.334-6.694v4a.667.667 0 1 0 1.333 0v-4a.667.667 0 1 0-1.333 0z"
-                  fill="currentColor"
-                />
-              </svg>
+              <ErrorIcon />
               {formErrorText}
             </PrimaryFont>
           </section>

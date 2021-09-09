80558af5caf33d9f94471e3610c57160beda5101
Matthew Kim <matthew.kim@washpost.com>
2021-09-08 22:13:55 -0400

    fix tests

--- blocks/identity-block/features/login/default.test.jsx
+++ blocks/identity-block/features/login/default.test.jsx
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import Login from './default';
 
 jest.mock('fusion:properties', () => (jest.fn(() => ({}))));
+jest.mock('react-google-recaptcha');
 
 it('renders nothing if identity not initialized', () => {
   const wrapper = shallow(<Login customFields={{ redirectURL: '' }} />);

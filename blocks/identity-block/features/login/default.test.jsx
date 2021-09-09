de120112d4d20d9e8edfc2ba142c93e5f610f626
Matthew Kim <matthew.kim@washpost.com>
2021-09-09 18:00:01 -0400

    add tests

--- blocks/identity-block/features/login/default.test.jsx
+++ blocks/identity-block/features/login/default.test.jsx
@@ -1,41 +1,66 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { useFusionContext } from 'fusion:context';
+import { act } from 'react-dom/test-utils';
 import Login from './default';
+
+import useIdentity from '../../components/Identity';
+jest.mock('../../components/Identity')
+
+const defaultCustomFields = {
+  redirectURL: '',
+  redirectToPreviousPage: true,
+  forgotPasswordUrl: '',
+  signupURL: ''
+};
 
 jest.mock('fusion:properties', () => (jest.fn(() => ({}))));
 jest.mock('react-google-recaptcha');
-jest.mock('../../components/Identity', () => ({
-  __esModule: true,
-  default: jest.fn(() => ({ isInitialized: true, isLoggedIn: () => true })),
-}));
-
-
 describe('Subscriptions Login Feature', () => {
+  let wrapper;
+  beforeEach(() => {
+    useIdentity.mockImplementation(() => ({
+      isInitialized: true,
+      isLoggedIn: () => true,
+      Identity: {
+        isLoggedIn: jest.fn(async () => await act(async () => false)),
+        getConfig: jest.fn(async () => await act(async () => ({})))
+      }
+    }))
+    act(async () => {
+      wrapper = await mount(
+        <Login
+          customFields={defaultCustomFields}
+        />
+      );
+    });
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   it('renders nothing if identity not initialized', () => {
+    useIdentity.mockImplementation(() => ({
+      isInitialized: false,
+      isLoggedIn: () => true,
+      Identity: {
+        isLoggedIn: jest.fn(async () => false),
+        getConfig: jest.fn(async () => ({}))
+      }
+    }));
     const wrapper = shallow(
       <Login
-        customFields={{
-          redirectURL: '',
-          redirectToPreviousPage: true,
-          forgotPasswordUrl: '',
-          signupURL: ''
-        }}
+        customFields={defaultCustomFields}
       />
     );
     expect(wrapper.html()).toBe(null);
   });
   it('renders', () => {
-    const wrapper = mount(
-      <Login
-        customFields={{
-          redirectURL: '',
-          redirectToPreviousPage: true,
-          forgotPasswordUrl: '',
-          signupURL: ''
-        }}
-      />
-    );
     expect(wrapper.html()).not.toBe(null);
   });
+  it('shows login form', () => {
+    expect(wrapper.find('form.xpmedia-form-submittable')).toHaveLength(1);
+    expect(wrapper.find('input.xpmedia-form-field-input')).toHaveLength(2);
+    expect(wrapper.find('form.xpmedia-form-submittable button')).toHaveLength(1);
+  });
+  it('shows forgot password & register links', () => {});
 });

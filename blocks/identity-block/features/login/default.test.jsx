7433b979ff6640cc405f9c45bc06bb479820cce3
Matthew Kim <matthew.kim@washpost.com>
2021-09-09 16:52:51 -0400

    wip tests

--- blocks/identity-block/features/login/default.test.jsx
+++ blocks/identity-block/features/login/default.test.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { useFusionContext } from 'fusion:context';
 import Login from './default';
 
 jest.mock('fusion:properties', () => (jest.fn(() => ({}))));
 jest.mock('react-google-recaptcha');
+jest.mock('../../components/Identity', () => ({
+  __esModule: true,
+  default: jest.fn(() => ({ isInitialized: true, isLoggedIn: () => true })),
+}));
 
-it('renders nothing if identity not initialized', () => {
-  const wrapper = shallow(<Login customFields={{ redirectURL: '' }} />);
-  expect(wrapper.html()).toBe(null);
+
+describe('Subscriptions Login Feature', () => {
+  it('renders nothing if identity not initialized', () => {
+    const wrapper = shallow(
+      <Login
+        customFields={{
+          redirectURL: '',
+          redirectToPreviousPage: true,
+          forgotPasswordUrl: '',
+          signupURL: ''
+        }}
+      />
+    );
+    expect(wrapper.html()).toBe(null);
+  });
+  it('renders', () => {
+    const wrapper = mount(
+      <Login
+        customFields={{
+          redirectURL: '',
+          redirectToPreviousPage: true,
+          forgotPasswordUrl: '',
+          signupURL: ''
+        }}
+      />
+    );
+    expect(wrapper.html()).not.toBe(null);
+  });
 });

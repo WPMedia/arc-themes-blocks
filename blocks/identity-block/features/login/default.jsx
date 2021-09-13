3a4db8d024f90f0a5254e453dc6f96120e1691ac
Jack Howard <jack.howard@washpost.com>
2021-09-13 15:39:06 -0500

    fix: Remove recaptcha implementation, remove login password validation, fix formatting, remove remember me, remove untranslated section

--- blocks/identity-block/features/login/default.jsx
+++ blocks/identity-block/features/login/default.jsx
@@ -4,7 +4,6 @@ import { isServerSide } from '@wpmedia/engine-theme-sdk';
 import { useFusionContext } from 'fusion:context';
 import getProperties from 'fusion:properties';
 import getTranslatedPhrases from 'fusion:intl';
-import ReCaptcha from 'react-google-recaptcha';
 
 import FormInputField, { FIELD_TYPES } from '../../components/FormInputField';
 import HeadlinedSubmitForm from '../../components/HeadlinedSubmitForm';
@@ -12,26 +11,18 @@ import useIdentity from '../../components/Identity';
 
 import './styles.scss';
 
-const Login = ({ customFields, arcSite }) => {
+const Login = ({ customFields }) => {
   let { redirectURL } = customFields;
   const {
     redirectToPreviousPage,
     loggedInPageLocation,
-    forgotPasswordURL,
-    signupURL,
   } = customFields;
 
-  const { isAdmin } = useFusionContext();
-  const { locale = 'en' } = getProperties(arcSite);
+  const { isAdmin, arcSite } = useFusionContext();
+  const { locale } = getProperties(arcSite);
   const phrases = getTranslatedPhrases(locale);
 
   const { Identity, isInitialized } = useIdentity();
-
-  const [recaptchaToken, setRecaptchaToken] = useState();
-  const [recaptchaConfig, setRecaptchaConfig] = useState({
-    signinRecaptcha: false,
-    status: 'initial',
-  });
 
   const [error, setError] = useState();
 
@@ -41,20 +32,7 @@ const Login = ({ customFields, arcSite }) => {
 
   useEffect(() => {
     const getConfig = async () => {
-      await Identity.getConfig()
-        .then((response) => {
-          const {
-            signinRecaptcha,
-            recaptchaSiteKey,
-          } = response;
-
-          setRecaptchaConfig({
-            signinRecaptcha,
-            recaptchaSiteKey,
-            status: 'success',
-          });
-        })
-        .catch(() => setRecaptchaConfig({ status: 'error' }));
+      await Identity.getConfig();
     };
 
     if (Identity) {
@@ -80,52 +58,31 @@ const Login = ({ customFields, arcSite }) => {
   }
 
   return (
-    <>
-      <HeadlinedSubmitForm
-        headline={phrases.t('identity-block.log-in')}
-        buttonLabel={phrases.t('identity-block.log-in')}
-        onSubmit={({ email, password }) => Identity
-          .login(email, password, { rememberMe: true, recaptchaToken })
-          .then(() => { window.location = redirectURL; })
-          .catch(() => setError(phrases.t('identity-block.login-form-error')))}
-        formErrorText={error}
-      >
-        <FormInputField
-          label={phrases.t('identity-block.email')}
-          name="email"
-          required
-          showDefaultError={false}
-          type={FIELD_TYPES.EMAIL}
-          validationErrorMessage={phrases.t('identity-block.email-requirements')}
-        />
-        <FormInputField
-          label={phrases.t('identity-block.password')}
-          name="password"
-          required
-          showDefaultError={false}
-          type={FIELD_TYPES.PASSWORD}
-          validationErrorMessage={phrases.t('identity-block.password-requirements')}
-        />
-        {
-          recaptchaConfig.signinRecaptcha ? (
-            <section className="xpmedia-subs-login-recaptcha">
-              <ReCaptcha
-                sitekey={recaptchaConfig.recaptchaSiteKey}
-                onChange={setRecaptchaToken}
-              />
-            </section>
-          ) : null
-        }
-      </HeadlinedSubmitForm>
-      {forgotPasswordURL || signupURL
-        ? (
-          <section className="xpmedia-subs-login-footer">
-            {forgotPasswordURL ? <a href={forgotPasswordURL}>{phrases.t('identity-block.forgot-password')}</a> : null}
-            {signupURL ? <a href={signupURL}>{phrases.t('identity-block.signup-cta')}</a> : null}
-          </section>
-        )
-        : null}
-    </>
+    <HeadlinedSubmitForm
+      headline={phrases.t('identity-block.log-in')}
+      buttonLabel={phrases.t('identity-block.log-in')}
+      onSubmit={({ email, password }) => Identity
+        .login(email, password)
+        .then(() => { window.location = redirectURL; })
+        .catch(() => setError(phrases.t('identity-block.login-form-error')))}
+      formErrorText={error}
+    >
+      <FormInputField
+        label={phrases.t('identity-block.email')}
+        name="email"
+        required
+        showDefaultError={false}
+        type={FIELD_TYPES.EMAIL}
+        validationErrorMessage={phrases.t('identity-block.email-requirements')}
+      />
+      <FormInputField
+        label={phrases.t('identity-block.password')}
+        name="password"
+        required
+        showDefaultError={false}
+        type={FIELD_TYPES.PASSWORD}
+      />
+    </HeadlinedSubmitForm>
   );
 };
 
@@ -146,14 +103,6 @@ Login.propTypes = {
       name: 'Logged In URL',
       defaultValue: '/account/',
       description: 'The URL to which a user would be redirected to if logged in an vist a page with the login form on',
-    }),
-    forgotPasswordURL: PropTypes.string.tag({
-      name: 'Forgot password URL',
-      defaultValue: '/account/forgot-password/',
-    }),
-    signupURL: PropTypes.string.tag({
-      name: 'Sign Up URL',
-      defaultValue: '/account/sign-up/',
     }),
   }),
 };

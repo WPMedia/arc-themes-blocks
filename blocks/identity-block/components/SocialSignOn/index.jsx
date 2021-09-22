e4907a001806becd27e1a0194d5d902cfa4c51d6
Brent Miller <brent.miller@washport.com>
2021-09-22 09:23:32 -0500

    Adding Facebook social sign on

--- blocks/identity-block/components/SocialSignOn/index.jsx
+++ blocks/identity-block/components/SocialSignOn/index.jsx
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
 import useIdentity from '../Identity';
+import './styles.scss';
 
 const SocialSignOn = () => {
   const { Identity } = useIdentity();
   const config = Identity?.configOptions ?? {};
 
   const [isGoogleInitialized, setIsGoogleInitialized] = useState(false);
-  // const [ isFacebookInitialized, setIsFacebookInitialized ] = useState(false);
+  const [isFacebookInitialized, setIsFacebookInitialized] = useState(false);
+
+  if (!window.onFacebookSignOn) {
+    window.onFacebookSignOn = async () => {
+      try {
+        await Identity.facebookSignOn();
+        // history.push('/profile');
+        // window.location = redirectURL;
+      } catch (e) {
+        // setErr(e);
+        // TODO: Deal with error messaging
+      }
+    };
+  }
 
   useEffect(() => {
     const fetchConfig = async () => {
@@ -19,19 +33,30 @@ const SocialSignOn = () => {
   }, [Identity]);
 
   useEffect(() => {
+    const initializeFacebook = async () => {
+      await Identity.initFacebookLogin(null);
+    };
     const initializeGoogle = async () => {
       await Identity.initGoogleLogin(null, {
-        width: 500,
-        height: 32,
-        onSuccess: console.log,
-        onFailure: console.log,
+        width: 300,
+        height: 48,
+        onSuccess: console.log, // Need to redirect
+        onFailure: console.log, // TODO: Deal with error messaging
       });
     };
     if (config.googleClientId && !isGoogleInitialized) {
       initializeGoogle();
       setIsGoogleInitialized(true);
     }
-  }, [Identity, config.googleClientId, isGoogleInitialized]);
+    if (config.facebookAppId && !isFacebookInitialized) {
+      initializeFacebook();
+      setIsFacebookInitialized(true);
+    }
+  }, [Identity,
+    config.facebookAppId,
+    config.googleClientId,
+    isFacebookInitialized,
+    isGoogleInitialized]);
 
   if (!Identity) {
     return null;
@@ -42,12 +67,23 @@ const SocialSignOn = () => {
   }
 
   return (
-    <section>
+    <section className="xpmedia-social-signin-wrapper">
       {
         config.googleClientId ? <div id="google-sign-in-button" /> : null
       }
       {
-        config.facebookAppId ? (<div>facebook button </div>) : null
+        config.facebookAppId ? (
+          <div
+            className="fb-login-button"
+            data-width="512"
+            data-size="large"
+            data-button-type="login_with"
+            data-scope="public_profile,email"
+            data-auto-logout-link="false"
+            data-use-continue-as="true"
+            data-onlogin="window.onFacebookSignOn()"
+          />
+        ) : null
       }
     </section>
   );

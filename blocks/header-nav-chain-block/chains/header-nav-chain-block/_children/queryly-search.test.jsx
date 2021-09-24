b4266ff5ecbc63e72a2da23813fef419efe784cd
Jack Howard <jack.howard@washpost.com>
2021-09-24 17:05:35 -0500

    test: Add on click search handler test for querly

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.test.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.test.jsx
@@ -24,4 +24,31 @@ describe('<QuerylySearch/>', () => {
 
     expect(wrapper.find('Button').length).toBe(1);
   });
+
+  it('affects the querly toggle on click', () => {
+    const MockQuerlyToggle = () => (
+      <input
+        type="checkbox"
+        id="queryly_toggle"
+        checked={false}
+        onChange={() => {}}
+      />
+    );
+
+    // via https://stackoverflow.com/a/43734834/7491536
+    // in order to use document.body queries, have to attach to body
+    // not ideal using getElementById within a react component
+    const wrapper = mount(
+      <>
+        <MockQuerlyToggle />
+        <QuerylySearch />
+      </>, { attachTo: document.body },
+    );
+    const inputMock = wrapper.find('input#queryly_toggle');
+    expect(inputMock.props().checked).toBe(false);
+    const button = wrapper.find('Button');
+
+    button.simulate('click');
+    expect(inputMock.props().checked).toBe(false);
+  });
 });

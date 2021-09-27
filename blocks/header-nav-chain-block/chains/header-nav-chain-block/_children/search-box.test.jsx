65639314f44e8852eb86fbfbf854a896fd721c69
Jack Howard <jack.howard@washpost.com>
2021-09-27 14:42:49 -0500

    test: Remove unused tests with nav color custom

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/search-box.test.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/search-box.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import getProperties from 'fusion:properties';
 import SearchBox from './search-box';
 
 const fakeEvent = { preventDefault: () => {} };
@@ -83,33 +82,6 @@ describe('the SearchBox component', () => {
       const wrapper = mount(<SearchBox alwaysOpen />);
 
       expect(wrapper.find('.nav-search')).toHaveClassName('open');
-    });
-  });
-
-  describe('when the navBarColor is set to "dark"', () => {
-    it('should set the "dark" class on the component', () => {
-      const wrapper = mount(<SearchBox />);
-
-      expect(wrapper.find('.nav-search')).toHaveClassName('dark');
-    });
-
-    it('should set the buttons to dark mode', () => {
-      const wrapper = mount(<SearchBox />);
-
-      expect(wrapper.find('.nav-btn').every('.nav-btn-dark')).toEqual(true);
-    });
-  });
-
-  describe('when the navBarColor is set to "light"', () => {
-    it('should set the "light" class on the component', () => {
-      getProperties.mockImplementation(() => ({
-        locale: 'en',
-        navBarBackground: '',
-        navColor: 'light',
-      }));
-      const wrapper = mount(<SearchBox />);
-
-      expect(wrapper.find('.nav-search')).toHaveClassName('light');
     });
   });
 });

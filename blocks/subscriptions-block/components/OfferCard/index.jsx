f571028af25215c69b14bd760dfc470d234db0eb
Matthew Roach <matthewroach@users.noreply.github.com>
2021-10-12 15:49:10 +0100

    Add OfferCard Component
    Including tests, and stories
    Updated Button props to include fullWidth proptype

---
+++ blocks/subscriptions-block/components/OfferCard/index.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from '@arc-fusion/prop-types';
+import { CheckIcon } from '@wpmedia/engine-theme-sdk';
+
+import {
+  Button,
+  BUTTON_SIZES,
+  BUTTON_STYLES,
+  PrimaryFont,
+} from '@wpmedia/shared-styles';
+
+import './styles.scss';
+
+const OfferCard = ({
+  headline,
+  subHeadline,
+  actionText,
+  actionEvent,
+  features = [],
+}) => (
+  <PrimaryFont as="div" className="xpmedia-subscription-offer-card">
+    {headline
+      ? <h2 className="xpmedia-subscription-offer-card--headline">{headline}</h2>
+      : null}
+
+    {subHeadline
+      ? <h3 className="xpmedia-subscription-offer-card--subheadline">{subHeadline}</h3>
+      : null}
+
+    {actionText && actionEvent
+      ? (
+        <Button
+          buttonSize={BUTTON_SIZES.LARGE}
+          buttonStyle={BUTTON_STYLES.PRIMARY}
+          fullWidth
+          text={actionText}
+          onClick={actionEvent}
+        />
+      )
+      : null}
+
+    {features.length
+      ? (
+        <ul className="xpmedia-subscription-offer-card--features">
+          {features.map((feat) => (
+            <li className="xpmedia-subscription-offer-card--feature-item" key={`feat-${feat.featureText}`}>
+              <span className="xpmedia-subscription-offer-card--feature-item-icon">
+                <CheckIcon width="16" height="16" />
+              </span>
+              {feat.featureText}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+  </PrimaryFont>
+);
+
+OfferCard.propTypes = {
+  headline: PropTypes.string,
+  subHeadline: PropTypes.string,
+  actionText: PropTypes.string,
+  actionEvent: PropTypes.func,
+  features: PropTypes.arrayOf(PropTypes.shape({
+    featureText: PropTypes.string,
+  })),
+};
+
+export default OfferCard;

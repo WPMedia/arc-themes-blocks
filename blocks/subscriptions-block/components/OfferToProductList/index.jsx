7100b898a04f105d5f2bfbce2ac1988b89cc5f81
Brent Miller <brent.miller@washport.com>
2021-10-28 13:08:53 -0500

    refacted product processing to a component

---
+++ blocks/subscriptions-block/components/OfferToProductList/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from '@arc-fusion/prop-types';
+import { useFusionContext } from 'fusion:context';
+import getProperties from 'fusion:properties';
+import Sales from '@arc-publishing/sdk-sales';
+import Identity from '@arc-publishing/sdk-identity';
+import GridList from '../GridList';
+import OfferCard from '../OfferCard';
+
+const OfferToProductList = ({
+  offer, isLoggedIn, checkoutURL, loginURL,
+}) => {
+  const { arcSite } = useFusionContext();
+  const { api } = getProperties(arcSite);
+
+  Sales.options({
+    apiOrigin: api?.retail?.origin || '',
+    Identity,
+  });
+
+  const buildOffers = () => {
+    const offers = [];
+    let productIdx;
+    let strategiesIdx;
+    if (offer.products) {
+      const { products } = offer;
+      for (productIdx = 0; productIdx < products.length; productIdx += 1) {
+        const { pricingStrategies: strategies } = products[productIdx];
+        for (strategiesIdx = 0; strategiesIdx < strategies.length; strategiesIdx += 1) {
+          const features = (typeof products[productIdx].attributes !== 'undefined' && products[productIdx].attributes.length !== 0)
+            ? products[productIdx].attributes.map((feature) => ({
+              featureText: feature.value,
+            }))
+            : [];
+          const { sku } = products[productIdx];
+          const { priceCode } = strategies[strategiesIdx];
+          offers.push(
+            <OfferCard
+              key={strategies[strategiesIdx].pricingStrategyId}
+              subHeadline={strategies[strategiesIdx].description}
+              headline={strategies[strategiesIdx].name}
+              actionText={strategies[strategiesIdx].summary}
+              actionEvent={() => {
+                Sales.clearCart()
+                  .then(() => Sales.addItemToCart([{
+                    sku,
+                    priceCode,
+                    quantity: 1,
+                  }]))
+                  .then(() => {
+                    if (isLoggedIn) {
+                      window.location.href = checkoutURL;
+                      return;
+                    }
+                    window.location.href = `${loginURL}?redirect=${checkoutURL}`;
+                  });
+              }}
+              features={features}
+            />,
+          );
+        }
+      }
+    }
+    return offers;
+  };
+
+  return (
+    <GridList>
+      {buildOffers()}
+    </GridList>
+  );
+};
+
+OfferToProductList.propTypes = {
+  offer: PropTypes.any,
+  loginURL: PropTypes.string,
+  checkoutURL: PropTypes.string,
+};
+
+export default OfferToProductList;
--- blocks/subscriptions-block/features/offer/default.jsx
+++ blocks/subscriptions-block/features/offer/default.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { isServerSide } from '@wpmedia/engine-theme-sdk';
 import PropTypes from '@arc-fusion/prop-types';
-import { useFusionContext } from 'fusion:context';
-import getProperties from 'fusion:properties';
 import Identity from '@arc-publishing/sdk-identity';
-import Sales from '@arc-publishing/sdk-sales';
 import useOffer from '../../components/useOffer';
-import GridList from '../../components/GridList';
-import OfferCard from '../../components/OfferCard';
+import OfferToProductList from '../../components/OfferToProductList';
 import './styles.scss';
 
 const Offer = ({
@@ -19,14 +15,6 @@ const Offer = ({
     checkoutURL,
   } = customFields;
 
-  const { arcSite } = useFusionContext();
-  const { api } = getProperties(arcSite);
-
-  Sales.options({
-    apiOrigin: api?.retail?.origin || '',
-    Identity,
-  });
-
   let selectedCampaignCode = campaignCode || 'default';
 
   if (!isServerSide()) {
@@ -34,7 +22,7 @@ const Offer = ({
     selectedCampaignCode = searchParams.has('campaign') ? searchParams.get('campaign') : campaignCode;
   }
 
-  const { offer } = useOffer({
+  const { offer, isFetching } = useOffer({
     campaignCode: selectedCampaignCode,
   });
 
@@ -49,68 +37,29 @@ const Offer = ({
     getIsLoggedIn();
   }, []);
 
-  if (isServerSide() || !offer) {
+  if (isServerSide()) {
     return null;
   }
 
-  const buildOffers = () => {
-    const offers = [];
-    let productIdx;
-    let strategiesIdx;
-    if (offer.products) {
-      const { products } = offer;
-      for (productIdx = 0; productIdx < products.length; productIdx += 1) {
-        const { pricingStrategies: strategies } = products[productIdx];
-        for (strategiesIdx = 0; strategiesIdx < strategies.length; strategiesIdx += 1) {
-          const features = (typeof products[productIdx].attributes !== 'undefined' && products[productIdx].attributes.length !== 0)
-            ? products[productIdx].attributes.map((feature) => ({
-              featureText: feature.value,
-            }))
-            : [];
-          const { sku } = products[productIdx];
-          const { priceCode } = strategies[strategiesIdx];
-          offers.push(
-            <OfferCard
-              key={strategies[strategiesIdx].pricingStrategyId}
-              subHeadline={strategies[strategiesIdx].description}
-              headline={strategies[strategiesIdx].name}
-              actionText={strategies[strategiesIdx].summary}
-              actionEvent={() => {
-                Sales.clearCart()
-                  .then(() => Sales.addItemToCart([{
-                    sku,
-                    priceCode,
-                    quantity: 1,
-                  }]))
-                  .then(() => {
-                    if (isLoggedIn) {
-                      window.location.href = checkoutURL;
-                      return;
-                    }
-                    window.location.href = `${loginURL}?redirect=${checkoutURL}`;
-                  });
-              }}
-              features={features}
-            />,
-          );
-        }
-      }
-    }
-    return offers;
-  };
-
   return (
     <div className="xpmedia-subscription-offer-wrapper layout-section wrap-bottom">
-      <div className="xpmedia-subscription-offer-headings">
-        <h1 dangerouslySetInnerHTML={{ __html: offer.pageTitle }} />
-        <h2 dangerouslySetInnerHTML={{ __html: offer.pageSubTitle }} />
-      </div>
+      {!isFetching && offer && (
+        <>
+          <div className="xpmedia-subscription-offer-headings">
+            <h1 dangerouslySetInnerHTML={{ __html: offer.pageTitle }} />
+            <h2 dangerouslySetInnerHTML={{ __html: offer.pageSubTitle }} />
+          </div>
 
-      <div className="margin-md-top">
-        <GridList>
-          {buildOffers()}
-        </GridList>
-      </div>
+          <div className="margin-md-top">
+            <OfferToProductList
+              offer={offer}
+              isLoggedIn={isLoggedIn}
+              checkoutURL={checkoutURL}
+              loginURL={loginURL}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };

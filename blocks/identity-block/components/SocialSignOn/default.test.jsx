788e1baa1d16b9e74c59f2aeeb0def1b56b737df
Jack Howard <jack.howard@washpost.com>
2021-12-08 15:19:48 -0600

    test: Add back removed test without weird async

--- blocks/identity-block/components/SocialSignOn/default.test.jsx
+++ blocks/identity-block/components/SocialSignOn/default.test.jsx
@@ -105,4 +105,33 @@ describe('Subscriptions Social Login Component', () => {
     );
     expect(wrapper.html()).not.toBe(null);
   });
+
+  it('renders placeholders for Google and Facebook sign-in buttons', () => {
+    useIdentity.mockImplementation(() => ({
+      isInitialized: true,
+      isLoggedIn: () => true,
+      Identity: {
+        isLoggedIn: jest.fn(async () => false),
+        getConfig: jest.fn(async () => Promise.resolve({
+          signinRecaptcha: false,
+          recaptchaSiteKey: '6LdXKVQcAAAAAO2tv3GdUbSK-1vcgujX6cP0IgF_',
+        })),
+        initGoogleLogin: jest.fn(async () => Promise.resolve({
+          init: true,
+        })),
+        initializeFacebook: jest.fn(async () => Promise.resolve({
+          init: true,
+        })),
+        configOptions: {
+          googleClientId: true,
+          facebookAppId: true,
+        },
+      },
+    }));
+    const wrapper = mount(
+      <SocialSignOn onError={() => null} redirectURL="#" />,
+    );
+    expect(wrapper.find('#google-sign-in-button')).toHaveLength(1);
+    expect(wrapper.find('.fb-login-button')).toHaveLength(1);
+  });
 });

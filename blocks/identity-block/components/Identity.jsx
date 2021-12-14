dbab0a5a38697169bb9a9df7e51ab427fa38affd
Brent Miller <brent.miller@washport.com>
2021-12-14 08:26:10 -0600

    Fixed recursive calls on Identity hook

--- blocks/identity-block/components/Identity.jsx
+++ blocks/identity-block/components/Identity.jsx
@@ -3,6 +3,15 @@ import Identity from '@arc-publishing/sdk-identity';
 import { useFusionContext } from 'fusion:context';
 import getProperties from 'fusion:properties';
 
+/**
+ * getSignedInIdentity: Helper function to determine what account user is logged in with.
+ * @param user
+ * @returns entry in the identities array that has the most recent login date
+ */
+const getSignedInIdentity = (user) => user?.identities?.reduce(
+  (prev, current) => ((prev.lastLoginDate > current.lastLoginDate) ? prev : current),
+) || null;
+
 const useIdentity = () => {
   const { arcSite } = useFusionContext();
   const { api } = getProperties(arcSite);
@@ -12,15 +21,6 @@ const useIdentity = () => {
     Identity.options({ apiOrigin: api.identity.origin });
     setIsInit(true);
   }
-
-  /**
-   * getSignedInIdentity: Helper function to determine what account user is logged in with.
-   * @param user
-   * @returns entry in the identities array that has the most recent login date
-   */
-  const getSignedInIdentity = (user) => user?.identities?.reduce(
-    (prev, current) => ((prev.lastLoginDate > current.lastLoginDate) ? prev : current),
-  ) || null;
 
   return {
     Identity,

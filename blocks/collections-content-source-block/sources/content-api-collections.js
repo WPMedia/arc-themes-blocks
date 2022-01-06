fd006506023f9ae45f1b5b501fc8c8869848a714
Matthew Roach <matthewroach@users.noreply.github.com>
2022-01-06 18:19:27 +0000

    Add missing trailing slash

--- blocks/collections-content-source-block/sources/content-api-collections.js
+++ blocks/collections-content-source-block/sources/content-api-collections.js
@@ -40,7 +40,7 @@ const fetch = (key = {}) => {
     const existingData = content;
 
     return axios({
-      url: `${CONTENT_BASE}content/v4/collections?${_id ? `_id=${_id}` : `content_alias=${contentAlias}`}${site ? `&website=${site}` : ''}&from=${updatedSize}${size ? `&size=${updatedSize}` : ''}&published=true`,
+      url: `${CONTENT_BASE}/content/v4/collections?${_id ? `_id=${_id}` : `content_alias=${contentAlias}`}${site ? `&website=${site}` : ''}&from=${updatedSize}${size ? `&size=${updatedSize}` : ''}&published=true`,
       headers: {
         'content-type': 'application/json',
         Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,

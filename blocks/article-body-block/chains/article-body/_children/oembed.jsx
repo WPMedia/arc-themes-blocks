4a6811173083479c061b8fcae3a07b4708aa11a5
Jack Howard <jack.howard@washpost.com>
2022-01-27 14:18:58 -0600

    perf: Remove oembed container as it's unnecessary bloat and not used for its purpose

--- blocks/article-body-block/chains/article-body/_children/oembed.jsx
+++ blocks/article-body-block/chains/article-body/_children/oembed.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from '@arc-fusion/prop-types';
-import EmbedContainer from 'react-oembed-container';
 
 const Oembed = ({
   element,
 }) => (
   <div className="block-margin-bottom">
-    <EmbedContainer markup={element.raw_oembed.html}>
-      <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
-    </EmbedContainer>
+    <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
   </div>
 );
 

aa7e1c2f24fcd8a5b0a75b4e8db484010b34fbc5
Jack Howard <jack.howard@washpost.com>
2022-01-27 14:57:47 -0600

    Revert "perf: Remove oembed container as it's unnecessary bloat and not used for its purpose"
    This reverts commit 4a6811173083479c061b8fcae3a07b4708aa11a5.

--- blocks/article-body-block/chains/article-body/_children/oembed.jsx
+++ blocks/article-body-block/chains/article-body/_children/oembed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from '@arc-fusion/prop-types';
+import EmbedContainer from 'react-oembed-container';
 
 const Oembed = ({
   element,
@@ -13,7 +14,9 @@ const Oembed = ({
 
   return (
     <div className={cssClasses}>
-      <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
+      <EmbedContainer markup={element.raw_oembed.html}>
+        <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
+      </EmbedContainer>
     </div>
   );
 };

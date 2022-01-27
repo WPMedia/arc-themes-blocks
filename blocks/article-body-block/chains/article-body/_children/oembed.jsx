c3b93bfcad19abb6ba1af28f6506283e6a86a085
Jack Howard <jack.howard@washpost.com>
2022-01-27 14:33:09 -0600

    fix: Add back classnames that are needed and used from news-theme-css

--- blocks/article-body-block/chains/article-body/_children/oembed.jsx
+++ blocks/article-body-block/chains/article-body/_children/oembed.jsx
@@ -3,11 +3,20 @@ import PropTypes from '@arc-fusion/prop-types';
 
 const Oembed = ({
   element,
-}) => (
-  <div className="block-margin-bottom">
-    <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
-  </div>
-);
+}) => {
+  // If element is a subtype of youtube or vimeo,
+  // add responsive video classes
+  // embed-responsive-16by9, etc are custom classes in news-theme-css
+  const responsiveClass = (['youtube', 'vimeo'].indexOf(element.subtype) !== -1) ? 'embed-responsive embed-responsive-16by9' : '';
+
+  const cssClasses = `${responsiveClass} block-margin-bottom`;
+
+  return (
+    <div className={cssClasses}>
+      <div dangerouslySetInnerHTML={{ __html: element.raw_oembed.html }} />
+    </div>
+  );
+};
 
 Oembed.propTypes = {
   element: PropTypes.object,

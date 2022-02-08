c609c1dd1d88e1005fb4725974974e8ad7993e5f
Matthew Roach <matthewroach@users.noreply.github.com>
2022-02-08 16:43:19 +0000

    Add npmignore, and update prompt text

--- _templates/content-source/new/prompt.js
+++ _templates/content-source/new/prompt.js
@@ -2,6 +2,6 @@ module.exports = [
   {
     type: 'input',
     name: 'block_name',
-    message: 'What is the content source block called? Type `collection` for Collection Source Block.',
+    message: 'What is the content source block called? Type `collection` for Collection Content Source Block.',
   },
 ];

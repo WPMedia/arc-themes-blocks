182ecc592283f177ef6603966eb1d42585102d5d
Matthew Roach <matthewroach@users.noreply.github.com>
2022-02-09 15:43:11 +0000

    Fix ArcAds export

--- blocks/ads-block/index.js
+++ blocks/ads-block/index.js
@@ -1,1 +1,7 @@
-module.exports = {};
+import ArcAd from './features/ads/default';
+
+export default ArcAd;
+
+// Once blocks that are using this export have been refactored
+// remove the above export and uncomment the below export
+// module.exports = {};

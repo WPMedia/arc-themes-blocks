008f36fce620783eb01d614dfab4901c3f73a85c
Brent Miller <brent.miller@washport.com>
2022-02-09 15:48:52 -0600

    Added storybook test

--- .storybook/mock-content/linksBar.js
+++ .storybook/mock-content/linksBar.js
@@ -37,6 +37,72 @@ export const linksBarMock = (query) => {
           display_name: 'Link Text',
         }
       ],
+    },
+    tenLinks: {
+      children: [
+        {
+          _id: 'id_1',
+          name: 'Test Link 1',
+        },
+        {
+          _id: 'id_2',
+          name: 'Test Link 2',
+        },
+        {
+          _id: 'id_3',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 3',
+        },
+        {
+          _id: 'id_4',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 4',
+        }
+        ,
+        {
+          _id: 'id_5',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 5',
+        }
+        ,
+        {
+          _id: 'id_6',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 6',
+        }
+        ,
+        {
+          _id: 'id_7',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 7',
+        }
+        ,
+        {
+          _id: 'id_8',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 8',
+        }
+        ,
+        {
+          _id: 'id_9',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 9',
+        }
+        ,
+        {
+          _id: 'id_10',
+          node_type: 'link',
+          url: '/',
+          display_name: 'Link Text 10',
+        }
+      ],
     }
   }
 

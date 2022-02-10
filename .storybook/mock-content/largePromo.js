f32df9326bbfee8cdde717808f8302e1f4303f80
Jack Howard <jackhowa@fastmail.com>
2022-02-10 15:51:48 -0600

    style: Update eslint to not ignore and fix or ignore eslint issues

--- .storybook/mock-content/largePromo.js
+++ .storybook/mock-content/largePromo.js
@@ -1,81 +1,82 @@
+// eslint-disable-next-line import/prefer-default-export
 export const largePromoMock = {
-  _id: "GHRBLTO6MNGV5G65F5D5XFX4SU",
-  "credits": {
-  "by": [
-    {
-    "_id": "saracarothers",
-    "additional_properties": {
-      "original": {
-        "byline": "Sara Lynn Carothers"
-      }
+  _id: 'GHRBLTO6MNGV5G65F5D5XFX4SU',
+  credits: {
+    by: [
+      {
+        _id: 'saracarothers',
+        additional_properties: {
+          original: {
+            byline: 'Sara Lynn Carothers',
           },
-          "name": "Sara Carothers",
-          "type": "author",
-          "url": "/author/sara-carothers/"
         },
-        {
-          "_id": "5IWXTLUXWNAZTN45YUTJBZM3JM",
-          "additional_properties": {
-            "original": {
-              "byline": "Taylor Doe"
-            }
+        name: 'Sara Carothers',
+        type: 'author',
+        url: '/author/sara-carothers/',
+      },
+      {
+        _id: '5IWXTLUXWNAZTN45YUTJBZM3JM',
+        additional_properties: {
+          original: {
+            byline: 'Taylor Doe',
           },
-          "name": "Taylor Doe",
-          "type": "author",
-          "url": "/author/taylor-doe/"
         },
-        {
-          "_id": "john-doe",
-          "additional_properties": {
-            "original": {
-              "byline": "John M Doe"
-            }
+        name: 'Taylor Doe',
+        type: 'author',
+        url: '/author/taylor-doe/',
+      },
+      {
+        _id: 'john-doe',
+        additional_properties: {
+          original: {
+            byline: 'John M Doe',
           },
-          "name": "John Doe",
-          "type": "author",
-          "url": "/author/john-m-doe/"
-        }
-      ]
-  },
-  "description": {
-    "basic": "This is a test story that can be used for breaking news situations like if a new baby panda was born."
-  },
-  "display_date": "2019-12-02T18:58:11.638Z",
-  "headlines": {
-    "basic": "Baby panda born at the zoo"
-  },
-  "label": {
-    "basic": {
-      "display": true,
-      "text": "Exclusive"
-    }
-  },
-  "owner": {
-    "sponsored": false
-  },
-  "promo_items": {
-    "basic": {
-      "resized_params": {
-        "274x154": "EfMJE6Iiv4GxwSe15iCDxBnN2ag=filters:format(jpg):quality(70)/",
-        "274x183": "a0vSW5PVOKTguW9d5fVcZO-61L4=filters:format(jpg):quality(70)/",
-        "274x206": "Uq6qFBw-w6VLN6hhAt0sjSt5UXs=filters:format(jpg):quality(70)/",
-        "377x212": "6hCESxDFY823IfORYxfAE__fhPw=filters:format(jpg):quality(70)/",
-        "377x251": "oKOQKByIKv7SDRQ3XezELPSAs_o=filters:format(jpg):quality(70)/",
-        "377x283": "kJ48hfQYVtqL6H_kUy06_Od9GIU=filters:format(jpg):quality(70)/"
+        },
+        name: 'John Doe',
+        type: 'author',
+        url: '/author/john-m-doe/',
       },
-      "type": "image",
-      "url": "https://arc-anglerfish-arc2-prod-corecomponents.s3.amazonaws.com/public/CLPUNWMKOZHWPLFYKRZXW6XTNU.jpg"
-    }
+    ],
   },
-  "type": "story",
-  "website_url": "/2019/12/02/baby-panda-born-at-the-zoo/",
-  "websites": {
-    "the-sun": {
-      "website_section": {
-        "_id": "/health",
-        "name": "Health"
+  description: {
+    basic: 'This is a test story that can be used for breaking news situations like if a new baby panda was born.',
+  },
+  display_date: '2019-12-02T18:58:11.638Z',
+  headlines: {
+    basic: 'Baby panda born at the zoo',
+  },
+  label: {
+    basic: {
+      display: true,
+      text: 'Exclusive',
+    },
+  },
+  owner: {
+    sponsored: false,
+  },
+  promo_items: {
+    basic: {
+      resized_params: {
+        '274x154': 'EfMJE6Iiv4GxwSe15iCDxBnN2ag=filters:format(jpg):quality(70)/',
+        '274x183': 'a0vSW5PVOKTguW9d5fVcZO-61L4=filters:format(jpg):quality(70)/',
+        '274x206': 'Uq6qFBw-w6VLN6hhAt0sjSt5UXs=filters:format(jpg):quality(70)/',
+        '377x212': '6hCESxDFY823IfORYxfAE__fhPw=filters:format(jpg):quality(70)/',
+        '377x251': 'oKOQKByIKv7SDRQ3XezELPSAs_o=filters:format(jpg):quality(70)/',
+        '377x283': 'kJ48hfQYVtqL6H_kUy06_Od9GIU=filters:format(jpg):quality(70)/',
       },
-      "website_url": "/2019/12/02/baby-panda-born-at-the-zoo/"
-    }
-  }
+      type: 'image',
+      url: 'https://arc-anglerfish-arc2-prod-corecomponents.s3.amazonaws.com/public/CLPUNWMKOZHWPLFYKRZXW6XTNU.jpg',
+    },
+  },
+  type: 'story',
+  website_url: '/2019/12/02/baby-panda-born-at-the-zoo/',
+  websites: {
+    'the-sun': {
+      website_section: {
+        _id: '/health',
+        name: 'Health',
+      },
+      website_url: '/2019/12/02/baby-panda-born-at-the-zoo/',
+    },
+  },
 };
--- .storybook/mock-content/linksBar.js
+++ .storybook/mock-content/linksBar.js
@@ -1,12 +1,12 @@
+// eslint-disable-next-line import/prefer-default-export
 export const linksBarMock = (query) => {
-
   const mocks = {
     noData: { children: [] },
     oneLink: {
       children: [{
         _id: 'id_1',
         name: 'test link 1',
-      }]
+      }],
     },
     twoLinks: {
       children: [
@@ -18,7 +18,7 @@ export const linksBarMock = (query) => {
           _id: 'id_2',
           name: 'test link 2',
         },
-      ]
+      ],
     },
     threeLinks: {
       children: [
@@ -35,7 +35,7 @@ export const linksBarMock = (query) => {
           node_type: 'link',
           url: '/',
           display_name: 'Link Text',
-        }
+        },
       ],
     },
     tenLinks: {
@@ -59,53 +59,46 @@ export const linksBarMock = (query) => {
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 4',
-        }
-        ,
+        },
         {
           _id: 'id_5',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 5',
-        }
-        ,
+        },
         {
           _id: 'id_6',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 6',
-        }
-        ,
+        },
         {
           _id: 'id_7',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 7',
-        }
-        ,
+        },
         {
           _id: 'id_8',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 8',
-        }
-        ,
+        },
         {
           _id: 'id_9',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 9',
-        }
-        ,
+        },
         {
           _id: 'id_10',
           node_type: 'link',
           url: '/',
           display_name: 'Link Text 10',
-        }
+        },
       ],
-    }
-  }
+    },
+  };
 
   return mocks[query.hierarchy] || mocks.noData;
-
-}
+};

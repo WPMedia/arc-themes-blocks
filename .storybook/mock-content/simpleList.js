90b5e9bcf8198e34ed39a08910f56d9185ab7003
Brent Miller <brent.miller@washport.com>
2022-02-10 16:27:21 -0600

    Added story and used fit-content in height

--- .storybook/mock-content/simpleList.js
+++ .storybook/mock-content/simpleList.js
@@ -1,92 +1,106 @@
 export const simpleListMock = {
-	content_elements: [
-		{
-			_id: '1',
-			headlines: {
-				basic: 'Castle church tower'
-			},
-			promo_items: {
-				basic: {
-					resized_params: {
-						'105x70': "toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/",
-						'274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
-						'377x283': "fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/",
-					},
-					type: 'image',
-					url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg'
-				}
-			},
-			websites: {
-				'story-book': {
-					website_url: '/video/2019/12/12/castle-church-tower/'
-				}
-			}
-		},
-		{
-			_id: '2',
-			headlines: {
-				basic: 'Castle church tower Castle church tower Castle church tower Castle church tower Castle church tower Castle church tower Castle church tower'
-			},
-			promo_items: {
-				basic: {
-					resized_params: {
-						'105x70': "toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/",
-						'274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
-						'377x283': "fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/",
-					},
-					type: 'image',
-					url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg'
-				}
-			},
-			websites: {
-				'story-book': {
-					website_url: '/video/2019/12/12/castle-church-tower/'
-				}
-			}
-		},
-		{
-			_id: '3',
-			headlines: {
-				basic: '7 questions about traveling to Australia during catastrophic fires, answered'
-			},
-			promo_items: {
-				basic: {
-					resized_params: {
-						'105x70': "toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/",
-						'274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
-						'377x283': "fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/",
-					},
-					type: 'image',
-					url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg'
-				}
-			},
-			websites: {
-				'story-book': {
-					website_url: '/video/2019/12/12/castle-church-tower/'
-				}
-			}
-		},
-		{
-			_id: '4',
-			headlines: {
-				basic: 'Castle church tower'
-			},
-			promo_items: {
-				basic: {
-					resized_params: {
-						'105x70': "toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/",
-						'274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
-						'377x283': "fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/",
-					},
-					type: 'image',
-					url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg'
-				}
-			},
-			websites: {
-				'story-book': {
-					website_url: '/video/2019/12/12/castle-church-tower/'
-				}
-			}
-		}
-	]
-}
+  content_elements: [
+    {
+      _id: '1',
+      headlines: {
+        basic: 'Castle church tower',
+      },
+      promo_items: {
+        basic: {
+          resized_params: {
+            '105x70': 'toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/',
+            '274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
+            '377x283': 'fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/',
+          },
+          type: 'image',
+          url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg',
+        },
+      },
+      websites: {
+        'story-book': {
+          website_url: '/video/2019/12/12/castle-church-tower/',
+        },
+      },
+    },
+    {
+      _id: '2',
+      headlines: {
+        basic: `Maces congue nibh purus, vitae sollicitudin nulla lacinia in. Nulla sodales,
+eros ut gravida elementum, lectus turpis porttitor felis, id rutrum ligula tellus eu ante.
+Morbi eleifend ex auctor, vestibulum elit in, dictum dolor.
+Mauris pharetra accumsan neque sed vehicula.
+Sed vitae elit ac ex aliquam facilisis.
+Ut molestie consectetur dui quis facilisis.
+Donec egestas bibendum massa vel lacinia.
+Etiam tincidunt non felis at gravida. Ut interdum, nisi sed consequat eleifend,
+sapien ipsum dictum velit, vitae rhoncus sem augue ac sapien. Vestibulum rutrum
+molestie viverra. Integer mollis eros eu tincidunt cursus. Sed ac nibh sed justo
+vestibulum malesuada a eu odio. Etiam et nibh aliquet, posuere ligula eget,
+tincidunt diam. In odio risus, rutrum sed sem nec, vestibulum varius purus.
+Maecenas vehicula, sem a maximus varius, nulla ipsum imperdiet erat,
+sit amet varius erat nunc a enim. Curabitur nulla lacus, posuere et enim sit amet,
+commodo consequat lectus.`,
+      },
+      promo_items: {
+        basic: {
+          resized_params: {
+            '105x70': 'toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/',
+            '274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
+            '377x283': 'fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/',
+          },
+          type: 'image',
+          url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg',
+        },
+      },
+      websites: {
+        'story-book': {
+          website_url: '/video/2019/12/12/castle-church-tower/',
+        },
+      },
+    },
+    {
+      _id: '3',
+      headlines: {
+        basic: '7 questions about traveling to Australia during catastrophic fires, answered',
+      },
+      promo_items: {
+        basic: {
+          resized_params: {
+            '105x70': 'toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/',
+            '274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
+            '377x283': 'fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/',
+          },
+          type: 'image',
+          url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg',
+        },
+      },
+      websites: {
+        'story-book': {
+          website_url: '/video/2019/12/12/castle-church-tower/',
+        },
+      },
+    },
+    {
+      _id: '4',
+      headlines: {
+        basic: 'Castle church tower',
+      },
+      promo_items: {
+        basic: {
+          resized_params: {
+            '105x70': 'toq6Dkqxl1xDvAHTWaO9c5qw4_o=filters:format(jpg):quality(70)/',
+            '274x183': 'Y9lCrjFoGrJZukqRLRMi5ntzkak=filters:format(jpg):quality(70)/',
+            '377x283': 'fpRHXIzIpdkN2NZ-2GLSF8sl4Pk=filters:format(jpg):quality(70)/',
+          },
+          type: 'image',
+          url: 'https://d22ff27hdsy159.cloudfront.net/12-12-2019/t_42fd2e985d3f4b868b927ca58620a2ca_name_file_1280x720_2000_v3_1_.jpg',
+        },
+      },
+      websites: {
+        'story-book': {
+          website_url: '/video/2019/12/12/castle-church-tower/',
+        },
+      },
+    },
+  ],
+};

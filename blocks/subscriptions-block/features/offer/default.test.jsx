7de020de1ee829b580b11f4c7fa1ee243a85ac8c
Jack Howard <jackhowa@fastmail.com>
2022-03-22 09:59:08 -0500

    test: Remove test change

--- blocks/subscriptions-block/features/offer/default.test.jsx
+++ blocks/subscriptions-block/features/offer/default.test.jsx
@@ -25,7 +25,7 @@ const sampleOffer = {
 			canRenew: true,
 			canRestart: true,
 			canStart: true,
-			name: "allaccesfffs",
+			name: "allaccess",
 			validFrom: 1632146400000,
 			validUntil: null,
 		},

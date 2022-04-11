7242a61899e2a6354eae152059fc14242a47424d
Jack Howard <jackhowa@fastmail.com>
2022-04-11 14:53:48 -0500

    test: Add basic mock and test to algolia assortment block

--- blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
+++ blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
@@ -1,6 +1,27 @@
 import contentSource from "./algolia-assortment";
 
+const HITS_MOCK = [{ name: "product 1" }];
+
+jest.mock("algoliasearch/lite", () => ({
+	__esModule: true,
+	default: jest.fn(() => ({
+		initIndex: jest.fn(() => ({
+			search: jest.fn(() => ({
+				hits: HITS_MOCK,
+			})),
+		})),
+	})),
+}));
+
 describe("Algolia Assortment content source", () => {
+	it("should use the proper param types", () => {
+		expect(contentSource.params).toEqual({
+			filters: "text",
+			index: "text",
+			query: "text",
+			ruleContexts: "text",
+		});
+	});
 	it("should transform data", () => {
 		const key = {
 			"arc-site": "arc-site",
@@ -9,5 +30,20 @@ describe("Algolia Assortment content source", () => {
 		const dataTransform = contentSource.transform(key);
 
 		expect(dataTransform).toEqual(key);
+	});
+	it("passes in filter, index, query, ruleContexts to algolia search", async () => {
+		const filters = "test-filters";
+		const index = "test-index";
+		const query = "test-query";
+		const ruleContexts = "test-ruleContexts";
+		const key = {
+			filters,
+			index,
+			query,
+			ruleContexts,
+		};
+		const fetchData = await contentSource.fetch(key);
+
+		expect(fetchData).toEqual(HITS_MOCK);
 	});
 });

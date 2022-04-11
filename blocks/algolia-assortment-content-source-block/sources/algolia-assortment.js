04d00b27d07a6a01612311e407f0272693ac4ccc
Jack Howard <jackhowa@fastmail.com>
2022-04-11 11:38:34 -0500

    feat: Install algolia search lib. Add basic environment secret mocking.

--- blocks/algolia-assortment-content-source-block/sources/algolia-assortment.js
+++ blocks/algolia-assortment-content-source-block/sources/algolia-assortment.js
@@ -1,15 +1,29 @@
+import algoliasearch from "algoliasearch/lite";
+import { algoliaSearchKey, algoliaAppId } from "fusion:environment";
+
 const params = {
 	input: "text",
 };
 
-const resolve = (key) => {
-	const { input, "arc-site": arcSite } = key;
-
-	return `${arcSite}-${input}`;
+const fetch = async (key = {}) => {
+	const client = algoliasearch(algoliaAppId, algoliaSearchKey);
+	const index = client.initIndex("instant_search");
+	try {
+		const { hits } = await index.search(key.query, {
+			ruleContext: key.ruleContext,
+			filters: key.filter,
+		});
+		return hits;
+	} catch (err) {
+		const error = new Error(err);
+		error.statusCode = 500;
+		throw error;
+	}
 };
-
 export default {
-	resolve,
+	fetch,
 	params,
 	transform: (data) => data,
+	// Time to live cache set to the Fusion minimum of 2 minutes (120 seconds)
+	ttl: 120,
 };
--- blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
+++ blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
@@ -1,16 +1,6 @@
 import contentSource from "./algolia-assortment";
 
 describe("Algolia Assortment content source", () => {
-	it("should build the correct url", () => {
-		const key = {
-			"arc-site": "arc-site",
-			input: "test-input",
-		};
-		const url = contentSource.resolve(key);
-
-		expect(url).toEqual(`${key["arc-site"]}-${key.input}`);
-	});
-
 	it("should transform data", () => {
 		const key = {
 			"arc-site": "arc-site",

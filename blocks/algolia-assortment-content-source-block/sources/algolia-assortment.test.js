7e291c474b4338d62d098c61e275193d0ff7b562
Jack Howard <jackhowa@fastmail.com>
2022-04-12 10:26:01 -0500

    test: Add mock and error-handling tests for algolia lib

--- blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
+++ blocks/algolia-assortment-content-source-block/sources/algolia-assortment.test.js
@@ -1,26 +1,23 @@
+import algoliasearch from "algoliasearch/lite";
 import contentSource from "./algolia-assortment";
 
 const HITS_MOCK = [{ name: "product 1" }];
-
-jest.mock("algoliasearch/lite", () => ({
-	__esModule: true,
-	default: jest.fn(() => ({
-		initIndex: jest.fn(() => ({
-			search: jest.fn(() => ({
-				hits: HITS_MOCK,
-			})),
-		})),
-	})),
-}));
+jest.mock("algoliasearch/lite");
 
 describe("Algolia Assortment content source", () => {
 	it("should use the proper param types", () => {
-		expect(contentSource.params).toEqual({
-			filters: "text",
-			index: "text",
-			query: "text",
-			ruleContexts: "text",
-		});
+		expect(contentSource.params).toMatchInlineSnapshot(
+			`
+		Object {
+		  "filters": "text",
+		  "hitsPerPage": "number",
+		  "index": "text",
+		  "page": "number",
+		  "query": "text",
+		  "ruleContexts": "text",
+		}
+	`
+		);
 	});
 	it("should transform data", () => {
 		const key = {
@@ -32,6 +29,13 @@ describe("Algolia Assortment content source", () => {
 		expect(dataTransform).toEqual(key);
 	});
 	it("passes in filter, index, query, ruleContexts to algolia search", async () => {
+		algoliasearch.mockImplementation(() => ({
+			initIndex: jest.fn(() => ({
+				search: jest.fn(() => ({
+					hits: HITS_MOCK,
+				})),
+			})),
+		}));
 		const filters = "test-filters";
 		const index = "test-index";
 		const query = "test-query";
@@ -45,5 +49,29 @@ describe("Algolia Assortment content source", () => {
 		const fetchData = await contentSource.fetch(key);
 
 		expect(fetchData).toEqual(HITS_MOCK);
+	});
+	it("should throw error", async () => {
+		algoliasearch.mockImplementation(() => ({
+			initIndex: jest.fn(() => ({
+				search: jest.fn(() => {
+					throw new Error();
+				}),
+			})),
+		}));
+
+		const filters = "test-filters";
+		const index = "test-index";
+		const query = "test-query";
+		const ruleContexts = "test-ruleContexts";
+		const key = {
+			filters,
+			index,
+			query,
+			ruleContexts,
+		};
+
+		expect(async () => {
+			await contentSource.fetch(key);
+		}).rejects.toThrow();
 	});
 });

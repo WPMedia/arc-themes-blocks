0664c11a239a95c937c87d2c1ddaf4b22982634f
Jack Howard <jackhowa@fastmail.com>
2022-05-10 12:08:00 -0500

    test: Update snapshots based on new classes. Check for case if there's no navigation

--- blocks/right-rail-block/layouts/right-rail/default.test.jsx
+++ blocks/right-rail-block/layouts/right-rail/default.test.jsx
@@ -89,11 +89,34 @@ describe("the right rail layout for the default output type", () => {
 			</RightRailLayout>
 		);
 
-		expect(wrapper.find("header")).toHaveHTML(
-			'<header class="page-header"><div id="navigation"></div></header>'
-		);
+		expect(wrapper.find(".b-right-rail__navigation")).toMatchInlineSnapshot(`
+		<Stack
+		  alignment="unset"
+		  className="b-right-rail__navigation"
+		  direction="vertical"
+		  divider={false}
+		  inline={false}
+		  justification="start"
+		  role="banner"
+		  wrap="nowrap"
+		>
+		  <div
+		    id="navigation"
+		    key=".0"
+		  />
+		</Stack>
+	`);
 		expect(wrapper.find("#fullWidth2").exists()).toBe(true);
-		expect(wrapper.find("footer")).toHaveHTML('<footer><div id="footer"></div></footer>');
+		expect(wrapper.find("footer")).toMatchInlineSnapshot(`
+		<footer
+		  className="b-right-rail__footer"
+		>
+		  <div
+		    id="footer"
+		    key=".5"
+		  />
+		</footer>
+	`);
 	});
 
 	it("should not show fullWidth2 section", () => {
@@ -109,10 +132,39 @@ describe("the right rail layout for the default output type", () => {
 			</RightRailLayout>
 		);
 
-		expect(wrapper.find("header")).toHaveHTML(
-			'<header class="page-header"><div id="navigation"></div></header>'
-		);
+		expect(wrapper.find(".b-right-rail__navigation")).toMatchInlineSnapshot(`
+		<Stack
+		  alignment="unset"
+		  className="b-right-rail__navigation"
+		  direction="vertical"
+		  divider={false}
+		  inline={false}
+		  justification="start"
+		  role="banner"
+		  wrap="nowrap"
+		>
+		  <div
+		    id="navigation"
+		    key=".0"
+		  />
+		</Stack>
+	`);
 		expect(wrapper.find("#fullWidth2").exists()).toBe(false);
-		expect(wrapper.find("footer")).toHaveHTML('<footer><div id="footer"></div></footer>');
+		expect(wrapper.find("footer")).toMatchInlineSnapshot(`
+		<footer
+		  className="b-right-rail__footer"
+		>
+		  <div
+		    id="footer"
+		    key=".5"
+		  />
+		</footer>
+	`);
+	});
+
+	it("should not render a navigation component if none exists", () => {
+		useAppContext.mockReturnValue({ renderables: allRenderables });
+		const wrapper = shallow(<RightRailLayout />);
+		expect(wrapper.find("#navigation").exists()).toBe(false);
 	});
 });

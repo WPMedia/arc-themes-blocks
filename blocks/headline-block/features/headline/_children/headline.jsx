ce1dea491027b4ad95bbaf9214475795ebecde98
Jack Howard <jackhowa@fastmail.com>
2022-05-11 14:19:23 -0500

    test: Remove unnecessary fallback logic as it's untestable

--- blocks/headline-block/features/headline/_children/headline.jsx
+++ blocks/headline-block/features/headline/_children/headline.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Heading } from "@wpmedia/arc-themes-components";
 
 // presentational component handles only visual
-const Headline = ({ headlineString = "" }) =>
+const Headline = ({ headlineString }) =>
 	/*
     if string is default empty from presentational container,
     then render null

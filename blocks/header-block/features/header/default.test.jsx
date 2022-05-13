7b12500d0f7284285b1508dfb70a49e2ece3012a
Jack Howard <jackhowa@fastmail.com>
2022-05-13 15:42:24 -0500

    style: Remove unused variable

--- blocks/header-block/features/header/default.test.jsx
+++ blocks/header-block/features/header/default.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import { useFusionContext } from "fusion:context";
 import Header from "./default";
 

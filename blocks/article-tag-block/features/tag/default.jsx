4df05d63dd4494c38a0cf7c90dad7e833f5c3ef4
Jack Howard <jackhowa@fastmail.com>
2022-05-20 09:58:52 -0500

    fix: Remove align center as not necessary

--- blocks/article-tag-block/features/tag/default.jsx
+++ blocks/article-tag-block/features/tag/default.jsx
@@ -10,13 +10,7 @@ export const ArticleTagItems = ({ content }) => {
 	const { taxonomy: { tags = [] } = {} } = content;
 
 	return tags.length ? (
-		<Stack
-			className={BLOCK_CLASS_NAME}
-			direction="horizontal"
-			wrap="wrap"
-			alignment="center"
-			justification="center"
-		>
+		<Stack className={BLOCK_CLASS_NAME} direction="horizontal" wrap="wrap" justification="center">
 			{tags.map((tag) => {
 				// fallback to "" because some tags don't have a slug
 				const { slug = "" } = tag;

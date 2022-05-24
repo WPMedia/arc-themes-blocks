0b050c445e6942550a29aab39522d76d2b4d2fb0
Nick Schubach <nschubach@gmail.com>
2022-05-24 15:02:16 -0400

    TMEDIA-732: Byline

---
+++ blocks/byline-block/features/byline/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { useFusionContext } from "fusion:context";
+import getProperties from "fusion:properties";
+import getTranslatedPhrases from "fusion:intl";
+
+import { Attribution, formatAuthors } from "@wpmedia/arc-themes-components";
+
+const BLOCK_CLASS_NAME = "b-byline";
+
+const Byline = () => {
+	const { arcSite, globalContent } = useFusionContext();
+	const phrases = getTranslatedPhrases(getProperties(arcSite).locale || "en");
+	const bylineNodes = formatAuthors(globalContent?.credits?.by, phrases.t("byline-block.and-text"));
+
+	return bylineNodes ? (
+		<Attribution className={`${BLOCK_CLASS_NAME}`}>
+			<span className={`${BLOCK_CLASS_NAME}__by`}>{phrases.t("byline-block.by-text")}</span>{" "}
+			<span className={`${BLOCK_CLASS_NAME}__names`}>{bylineNodes}</span>
+		</Attribution>
+	) : null;
+};
+
+Byline.label = "Byline – Arc Block";
+
+Byline.icon = "user-question";
+
+export default Byline;

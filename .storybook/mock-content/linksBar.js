5ac146aec811e918ea0303f76242aaf2443cb720
Malavika Koppula <mkreddy1110@gmail.com>
2022-06-01 09:58:16 -0400

    fixed styles according to mocks

--- .storybook/mock-content/linksBar.js
+++ .storybook/mock-content/linksBar.js
@@ -6,7 +6,7 @@ export const linksBarMock = (query) => {
 			children: [
 				{
 					_id: "id_1",
-					name: "test link 1",
+					name: "Link 1",
 				},
 			],
 		},
@@ -14,11 +14,11 @@ export const linksBarMock = (query) => {
 			children: [
 				{
 					_id: "id_1",
-					name: "test link 1",
+					name: "Link 1",
 				},
 				{
 					_id: "id_2",
-					name: "test link 2",
+					name: "Link 2",
 				},
 			],
 		},
@@ -26,17 +26,17 @@ export const linksBarMock = (query) => {
 			children: [
 				{
 					_id: "id_1",
-					name: "test link 1",
+					name: "Link 1",
 				},
 				{
 					_id: "id_2",
-					name: "test link 2",
+					name: "Link 2",
 				},
 				{
 					_id: "id_3",
 					node_type: "link",
 					url: "/",
-					display_name: "Link Text",
+					display_name: "Link 3",
 				},
 			],
 		},

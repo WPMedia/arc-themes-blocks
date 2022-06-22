7ba61373c9c41dab9d4965aaa1bf5cef5628ab3c
Brent Miller <brent.miller@washport.com>
2022-06-22 09:43:13 -0500

    progress change

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/nav-section.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/nav-section.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { hasUserConfiguredNavItems, PLACEMENT_AREAS } from "../nav-helper";
+import { hasUserConfiguredNavItems, NAV_BREAKPOINTS, PLACEMENT_AREAS } from "../nav-helper";
 import WidgetList from "./widget-list";
 
 const NavSection = ({ children, customFields, menuButtonClickAction, side, signInOrder }) =>
 	// istanbul ignore next
-	// eslint-disable-next-line no-nested-ternary
-	!side ? null : side === "right" &&
-	  !hasUserConfiguredNavItems(customFields) &&
-	  signInOrder &&
-	  Number.isInteger(signInOrder) &&
-	  children[signInOrder - 1] ? (
-		children[signInOrder - 1]
-	) : (
-		<WidgetList
-			customFields={customFields}
-			id={side}
-			menuButtonClickAction={menuButtonClickAction}
-			placement={PLACEMENT_AREAS.NAV_BAR}
-		>
-			{children}
-		</WidgetList>
+	!side ? null : (
+		<div key={side} className={`nav-${side}`}>
+			{
+				// Support for deprecated 'signInOrder' custom field
+				// "If" condition is for rendering "signIn" element
+				// "Else" condition is for standard nav bar customization logic
+				side === "right" &&
+				!hasUserConfiguredNavItems(customFields) &&
+				signInOrder &&
+				Number.isInteger(signInOrder) &&
+				children[signInOrder - 1]
+					? children[signInOrder - 1]
+					: NAV_BREAKPOINTS.map((breakpoint) => (
+							<div key={breakpoint} className={`nav-components--${breakpoint}`}>
+								<WidgetList
+									breakpoint={breakpoint}
+									customFields={customFields}
+									id={side}
+									menuButtonClickAction={menuButtonClickAction}
+									placement={PLACEMENT_AREAS.NAV_BAR}
+								>
+									{children}
+								</WidgetList>
+							</div>
+					  ))
+			}
+		</div>
 	);
 
 export default NavSection;

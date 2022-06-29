0af6251fb1372a8c63227d2d16617140e18defed
Matthew Roach <matthewroach@users.noreply.github.com>
2022-06-29 12:40:15 +0100

    Remove unused import

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.test.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/_children/queryly-search.test.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { mount } from "enzyme";
-import getProperties from "fusion:properties";
 import QuerylySearch from "./queryly-search";
 
 jest.mock("fusion:properties", () =>

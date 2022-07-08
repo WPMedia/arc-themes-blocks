eeb9a52f219ab44233f38cc3d0458f02b5fff807
Eric Carlisle <eric@ericcarlisle.com>
2022-07-08 17:09:22 -0400

    V2 edits to block logic.

--- blocks/small-promo-block/features/small-promo/default.jsx
+++ blocks/small-promo-block/features/small-promo/default.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import PropTypes from "@arc-fusion/prop-types";
 import { useContent } from "fusion:content";
-import { useFusionContext } from "fusion:context";
-import { LazyLoad, isServerSide } from "@wpmedia/engine-theme-sdk";
-import { imageRatioCustomField } from "@wpmedia/resizer-image-block";
-import { SmallPromoPresentation } from "@wpmedia/shared-styles";
+import { useComponentContext, useFusionContext } from "fusion:context";
+import getProperties from "fusion:properties";
+import {
+	formatURL,
+	Heading,
+	HeadingSection,
+	Image,
+	isServerSide,
+	Link,
+	MediaItem,
+	Grid,
+} from "@wpmedia/arc-themes-components";
+import { LazyLoad } from "@wpmedia/engine-theme-sdk";
 
-const SmallPromoItem = ({ customFields, arcSite }) => {
+const BLOCK_CLASS_NAME = "b-small-promo";
+
+const SmallPromo = ({ customFields }) => {
+	const { imagePosition, lazyLoad, showHeadline, showImage } = customFields;
+	const { registerSuccessEvent } = useComponentContext();
+	const { arcSite, isAdmin } = useFusionContext();
+	const shouldLazyLoad = lazyLoad && !isAdmin;
+
 	const content =
 		useContent({
 			source: customFields?.itemContentConfig?.contentService ?? null,
@@ -20,70 +36,121 @@ const SmallPromoItem = ({ customFields, arcSite }) => {
 			// does not need website section nor label because no overline
 			// does not need byline because no byline shown
 			filter: `{
-      _id
-      description {
-        basic
-      }
-      display_date
-      type
-      headlines {
-        basic
-      }
-      promo_items {
-        type
-        url
-        lead_art {
-          type
-          promo_items {
-            basic {
-              type
-              url
-              resized_params {
-                800x600
-                800x533
-                800x450
-                600x450
-                600x400
-                600x338
-              }
-            }
-          }
-        }
-        basic {
-          type
-          url
-          resized_params {
-            800x600
-            800x533
-            800x450
-            600x450
-            600x400
-            600x338
-          }
-        }
-      }
-      websites {
-        ${arcSite} {
-          website_url
-        }
-      }
-    }`,
+		_id
+		description {
+			basic
+		}
+		display_date
+		type
+		headlines {
+			basic
+		}
+		promo_items {
+			type
+			url
+			lead_art {
+				type
+				promo_items {
+					basic {
+						type
+						url
+						resized_params {
+							800x600
+							800x533
+							800x450
+							600x450
+							600x400
+							600x338
+						}
+					}
+				}
+			}
+			basic {
+				type
+				url
+				resized_params {
+					800x600
+					800x533
+					800x450
+					600x450
+					600x400
+					600x338
+				}
+			}
+		}
+		websites {
+			${arcSite} {
+				website_url
+			}
+		}
+	}`,
 		}) || null;
 
-	return <SmallPromoPresentation content={content} {...customFields} />;
-};
+	const linkURL = content?.websites?.[arcSite]?.website_url;
+	const imageURL =
+		content.promo_items?.basic?.type === "image" && content.promo_items?.basic?.url
+			? content.promo_items.basic.url
+			: null;
+	const headline = content?.description?.basic;
 
-const SmallPromo = ({
-	customFields = { showImage: true, showHeadline: true, imageRatio: "3:2" },
-}) => {
-	const { isAdmin, arcSite } = useFusionContext();
-	const shouldLazyLoad = customFields?.lazyLoad && !isAdmin;
 	if (shouldLazyLoad && isServerSide()) {
 		return null;
 	}
+
+	const PromoImage = () => {
+		const { fallbackImage } = getProperties(arcSite);
+		const ImageDisplay =
+			showImage && imageURL ? (
+				<MediaItem>
+					<Image alt="" src={imageURL || fallbackImage} />
+				</MediaItem>
+			) : null;
+		return showImage && linkURL ? (
+			<Link href={formatURL(linkURL)} onClick={registerSuccessEvent} assistiveHidden>
+				{ImageDisplay}
+			</Link>
+		) : (
+			ImageDisplay
+		);
+	};
+
+	const PromoHeading = () =>
+		showHeadline && headline ? (
+			<Heading>
+				{linkURL ? (
+					<Link href={formatURL(linkURL)} onClick={registerSuccessEvent}>
+						{headline}
+					</Link>
+				) : (
+					headline
+				)}
+			</Heading>
+		) : null;
+
+	const containerClassNames = [
+		BLOCK_CLASS_NAME,
+		!showImage || !showHeadline ? null : `${BLOCK_CLASS_NAME}--${imagePosition}`,
+	]
+		.filter((classString) => classString)
+		.join(" ");
+
 	return (
 		<LazyLoad enabled={shouldLazyLoad}>
-			<SmallPromoItem customFields={customFields} arcSite={arcSite} />
+			<HeadingSection>
+				<Grid as="article" className={containerClassNames}>
+					{["below", "right"].includes(imagePosition) ? (
+						<>
+							<PromoHeading />
+							<PromoImage />
+						</>
+					) : (
+						<>
+							<PromoImage />
+							<PromoHeading />
+						</>
+					)}
+				</Grid>
+			</HeadingSection>
 		</LazyLoad>
 	);
 };
@@ -120,7 +187,11 @@ SmallPromo.propTypes = {
 				below: "Image Below",
 			},
 		}).isRequired,
-		...imageRatioCustomField("imageRatio", "Art", "3:2"),
+		imageRatio: PropTypes.oneOf(["16:9", "3:2", "4:3"]).tag({
+			defaultValue: "3:2",
+			label: "Image ratio",
+			group: "Art",
+		}),
 		lazyLoad: PropTypes.bool.tag({
 			name: "Lazy Load block?",
 			defaultValue: false,

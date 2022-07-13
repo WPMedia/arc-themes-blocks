00fbdaf61ad73f61462014ab4eb5324c113b3948
Eric Carlisle <eric@ericcarlisle.com>
2022-07-13 16:57:52 -0400

    Update snapshot on unit test.

--- blocks/medium-manual-promo-block/features/medium-manual-promo/default.test.jsx
+++ blocks/medium-manual-promo-block/features/medium-manual-promo/default.test.jsx
@@ -56,7 +56,7 @@ describe("the medium promo feature", () => {
 		);
 	});
 
-	it("does not show iamge", () => {
+	it("does not show image", () => {
 		const noImage = {
 			...customFieldData,
 			showImage: false,
@@ -107,7 +107,7 @@ describe("the medium promo feature", () => {
 		};
 		const wrapper = mount(<MediumManualPromo customFields={noHeadline} />);
 		expect(wrapper.html()).toMatchInlineSnapshot(
-			`"<article class=\\"b-medium-manual-promo b-medium-manual-promo--show-image\\"><figure class=\\"c-media-item\\"><img alt=\\"Headline\\" class=\\"c-image\\" loading=\\"lazy\\" src=\\"image-url.jpg\\"></figure><h2 class=\\"c-heading\\"></h2><p class=\\"c-paragraph\\">Description</p></article>"`
+			`"<article class=\\"b-medium-manual-promo b-medium-manual-promo--show-image\\"><figure class=\\"c-media-item\\"><img alt=\\"Headline\\" class=\\"c-image\\" loading=\\"lazy\\" src=\\"image-url.jpg\\"></figure><h2 class=\\"c-heading\\">Headline</h2><p class=\\"c-paragraph\\">Description</p></article>"`
 		);
 	});
 });

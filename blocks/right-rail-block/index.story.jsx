e9138ee059b439d2920d82eb26e9e3990d20f032
Brent Miller <brent.miller@washport.com>
2022-07-14 14:51:22 -0500

    imple for commerce

--- blocks/right-rail-block/index.story.jsx
+++ blocks/right-rail-block/index.story.jsx
@@ -10,7 +10,13 @@ export default {
 	},
 };
 
-const layoutItemStyles = {
+const getStyles = (name, layoutItemStyles) => {
+	const defaults = layoutItemStyles.default;
+
+	return { ...defaults, ...layoutItemStyles[name] };
+};
+
+const layoutItemStylesBasic = {
 	navigation: {
 		backgroundColor: "rgb(236 155 223)",
 		height: "100px",
@@ -30,18 +36,57 @@ const layoutItemStyles = {
 		textAlign: "center",
 	},
 };
-const getStyles = (name) => {
-	const defaults = layoutItemStyles.default;
 
-	return { ...defaults, ...layoutItemStyles[name] };
+const layoutItemStylesCommerce = {
+	navigation: {
+		backgroundColor: "black",
+		color: "white",
+		height: "100px",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	footer: {
+		height: "300px",
+	},
+	fullWidth2: {
+		height: "250px",
+	},
+	main: {
+		height: "800px",
+	},
+	rightRail: {
+		height: "600px",
+	},
+	default: {
+		width: "100%",
+		backgroundColor: "rgb(230 230 230)",
+		textAlign: "center",
+	},
 };
 
-const layoutItem = (name) => <div style={getStyles(name)}>{name}</div>;
+const layoutItemBasic = (name) => <div style={getStyles(name, layoutItemStylesBasic)}>{name}</div>;
 
-const layoutAreas = ["navigation", "fullWidth1", "main", "rightRail", "fullWidth2", "footer"];
+const layoutItemCommerce = (name) => (
+	<div style={getStyles(name, layoutItemStylesCommerce)}>
+		{name}
+		{name === "navigation" ? <h3>Use the commerce theme when viewing this story</h3> : null}
+	</div>
+);
+
+const layoutAreasBasic = ["navigation", "fullWidth1", "main", "rightRail", "fullWidth2", "footer"];
+
+const layoutAreasCommerce = ["navigation", "", "main", "rightRail", "fullWidth2", "footer"];
 
 export const basic = () => (
 	<div id="fusion-app" className="layout-section">
-		<RightRailBlock children={layoutAreas.map((name) => layoutItem(name))} />
+		<RightRailBlock children={layoutAreasBasic.map((name) => layoutItemBasic(name))} />
+	</div>
+);
+
+export const commerce = () => (
+	<div id="fusion-app" className="layout-section">
+		<RightRailBlock children={layoutAreasCommerce.map((name) => layoutItemCommerce(name))} />
 	</div>
 );

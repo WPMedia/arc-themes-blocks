49fb21fdb70580c83477c0a06384bb17c14e0c58
Jack Howard <jackhowa@fastmail.com>
2022-07-18 15:28:46 -0400

    test: Update to include itnl change

--- blocks/identity-block/features/account-management/_children/SocialEditableSection.test.jsx
+++ blocks/identity-block/features/account-management/_children/SocialEditableSection.test.jsx
@@ -58,6 +58,6 @@ describe("SocialEditableSection", () => {
 		}));
 
 		const wrapper = mount(<SocialEditableSection hasFacebook hasGoogle hasPasswordAccount />);
-		expect(wrapper.find("span").at(0).text()).toEqual("Connected ");
+		expect(wrapper.find("span").at(0).text()).toEqual("identity-block.connect-account ");
 	});
 });

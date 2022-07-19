478487c6fd8bed73f59602e76edf393c0bd7e89d
Brent Miller <brent.miller@washport.com>
2022-07-18 20:23:22 -0500

    PR changes to fall in line with huddle discussions

--- blocks/right-rail-block/index.story.jsx
+++ blocks/right-rail-block/index.story.jsx
@@ -37,56 +37,12 @@ const layoutItemStylesBasic = {
 	},
 };
 
-const layoutItemStylesCommerce = {
-	navigation: {
-		backgroundColor: "black",
-		color: "white",
-		height: "100px",
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	footer: {
-		height: "300px",
-	},
-	fullWidth2: {
-		height: "250px",
-	},
-	main: {
-		height: "800px",
-	},
-	rightRail: {
-		height: "600px",
-	},
-	default: {
-		width: "100%",
-		backgroundColor: "rgb(230 230 230)",
-		textAlign: "center",
-	},
-};
-
 const layoutItemBasic = (name) => <div style={getStyles(name, layoutItemStylesBasic)}>{name}</div>;
 
-const layoutItemCommerce = (name) => (
-	<div style={getStyles(name, layoutItemStylesCommerce)}>
-		{name}
-		{name === "navigation" ? <h3>Use the commerce theme when viewing this story</h3> : null}
-	</div>
-);
-
 const layoutAreasBasic = ["navigation", "fullWidth1", "main", "rightRail", "fullWidth2", "footer"];
-
-const layoutAreasCommerce = ["navigation", "", "main", "rightRail", "fullWidth2", "footer"];
 
 export const basic = () => (
 	<div id="fusion-app" className="layout-section">
 		<RightRailBlock children={layoutAreasBasic.map((name) => layoutItemBasic(name))} />
-	</div>
-);
-
-export const commerce = () => (
-	<div id="fusion-app" className="layout-section">
-		<RightRailBlock children={layoutAreasCommerce.map((name) => layoutItemCommerce(name))} />
 	</div>
 );

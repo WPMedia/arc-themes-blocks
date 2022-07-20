f55e9fe98fd7db055418bb3e77d05701b5dcdb6a
Matthew Roach <matthewroach@users.noreply.github.com>
2022-07-20 16:12:07 +0100

    Add Product Information block

---
+++ blocks/product-information-block/features/product-information/default.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useFusionContext } from "fusion:context";
+
+import { Heading, HeadingSection, Price, Stack } from "@wpmedia/arc-themes-components";
+
+const BLOCK_CLASS_NAME = "b-product-information";
+
+const getPrice = (type, prices) => (prices && prices.filter((x) => x.type === type)?.[0]) || null;
+
+export const ProductInformationDisplay = ({ data }) => {
+	const pricing = (data?.pricing && data?.pricing[0]) || null;
+	const ListPrice = getPrice("List", pricing?.prices);
+
+	return (
+		<Stack className={`${BLOCK_CLASS_NAME}`}>
+			<HeadingSection>
+				{data.name ? <Heading>{data.name}</Heading> : null}
+				{ListPrice ? (
+					<Price>
+						<Price.List>
+							{new Intl.NumberFormat(pricing?.currencyLocale, {
+								style: "currency",
+								currency: pricing?.currencyCode,
+								minimumFractionDigits: 2,
+							}).format(ListPrice.amount)}
+						</Price.List>
+					</Price>
+				) : null}
+			</HeadingSection>
+		</Stack>
+	);
+};
+
+const ProductInformation = () => {
+	const { globalContent = {} } = useFusionContext();
+
+	if (!Object.keys(globalContent).length) {
+		return null;
+	}
+
+	return <ProductInformationDisplay data={globalContent} />;
+};
+
+// find matching icon in https://redirector.arcpublishing.com/pagebuilder/block-icon-library
+ProductInformation.icon = "cursor-information";
+
+export default ProductInformation;
---
+++ blocks/product-information-block/index.story.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ProductInformationDisplay } from "./features/product-information/default";
+
+// for more info on storybook and using the component explorer
+// https://storybook.js.org/
+export default {
+	title: "Blocks/Product Information",
+	parameters: {
+		chromatic: { viewports: [320, 1200] },
+		cssVariables: {
+			theme: "commerce",
+		},
+	},
+};
+
+const mockPrice = {
+	currencyCode: "USD",
+	currencyDisplayFormat: "symbol",
+	currencyLocale: "en-US",
+	schema: {},
+	prices: [
+		{
+			type: "List",
+			amount: "59.00",
+		},
+		{
+			type: "Sale",
+			amount: "49.00",
+		},
+	],
+};
+const mockData = {
+	name: "Crocker Sandals",
+	pricing: [mockPrice],
+};
+
+export const TitleAndPricing = () => <ProductInformationDisplay data={mockData} />;
+
+export const TitleOnly = () => <ProductInformationDisplay data={{ name: "Crocker Sandals" }} />;
+
+export const ListsPriceOnly = () => <ProductInformationDisplay data={{ pricing: [mockPrice] }} />;

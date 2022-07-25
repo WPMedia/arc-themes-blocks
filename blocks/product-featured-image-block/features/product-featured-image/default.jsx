bd0817e94f0953800039a2a33b316f72ca626a27
Jack Howard <jackhowa@fastmail.com>
2022-07-25 15:50:06 -0500

    feat: Add image resizer public key

--- blocks/product-featured-image-block/features/product-featured-image/default.jsx
+++ blocks/product-featured-image-block/features/product-featured-image/default.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useFusionContext } from "fusion:context";
 import { Image } from "@wpmedia/arc-themes-components";
+import { RESIZER_APP_VERSION } from "fusion:environment";
 
 const BLOCK_CLASS_NAME = "b-product-featured-image";
 
@@ -12,13 +13,16 @@ function ProductFeaturedImage() {
 		return null;
 	}
 
-	const rawImage = globalContent.schema.featuredImage.value.assets.find(
+	const featuredImageAsset = globalContent.schema.featuredImage.value.assets.find(
 		(asset) => asset.type === "image"
 	);
 
-	const { url } = rawImage;
+	const { url, auth } = featuredImageAsset;
 
-	return <Image src={url} className={BLOCK_CLASS_NAME} />;
+	// take in app version that's the public key for the auth object in resizer v2
+	const targetAuth = auth[RESIZER_APP_VERSION];
+
+	return <Image src={url} className={BLOCK_CLASS_NAME} resizedOptions={{ auth: targetAuth }} />;
 }
 
 ProductFeaturedImage.label = "Product Featured Image – Arc Block";
--- blocks/product-featured-image-block/features/product-featured-image/default.test.jsx
+++ blocks/product-featured-image-block/features/product-featured-image/default.test.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { useFusionContext } from "fusion:context";
 
 import ProductFeaturedImage from "./default";
 
+const FEATURED_IMAGE_ASSET = {
+	type: "image",
+	url: "https://example.com/image.jpg",
+	auth: {
+		1: "secret",
+	},
+};
+
 describe("Product Featured Image", () => {
-	it.skip("should render", () => {
+	it("should render null if no global content", () => {
+		useFusionContext.mockImplementation(() => ({
+			globalContent: {},
+		}));
+		const { container } = render(<ProductFeaturedImage />);
+		expect(container.firstChild).toBe(null);
+	});
+	it("should render null if no global content object", () => {
+		useFusionContext.mockImplementation(() => ({}));
+		const { container } = render(<ProductFeaturedImage />);
+		expect(container.firstChild).toBe(null);
+	});
+	it("should render a featured image", () => {
+		useFusionContext.mockImplementation(() => ({
+			globalContent: {
+				schema: {
+					featuredImage: {
+						value: {
+							assets: [FEATURED_IMAGE_ASSET],
+						},
+					},
+				},
+			},
+		}));
+
 		render(<ProductFeaturedImage />);
+
 		expect(screen.getByRole("img")).toBeInTheDocument();
 	});
 });

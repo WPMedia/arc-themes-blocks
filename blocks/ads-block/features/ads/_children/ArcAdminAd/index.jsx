471142cb5fbf851a26d313349609ca90ba8835c2
Jack Howard <jackhowa@fastmail.com>
2022-08-01 13:32:29 -0500

    docs: Add todo notes for wip direction of the admin ad display

--- blocks/ads-block/features/ads/_children/ArcAdminAd/index.jsx
+++ blocks/ads-block/features/ads/_children/ArcAdminAd/index.jsx
@@ -1,12 +1,19 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { useFusionContext } from "fusion:context";
+// todo: remove and replace
 import "./index.scss";
 
+// could I use this in sb?
+// this is only visible on the admin side
 const ArcAdminAd = ({ adClass, adType, slotName, dimensions }) => {
 	const { isAdmin } = useFusionContext();
 	return isAdmin ? (
-		<div className={["pb-ad-admin", "arcad", `ad-${adClass}`, "padding-sm-all"].join(" ")}>
+		<div
+			className={["pb-ad-admin", "arcad", `ad-${adClass}`, "padding-sm-all"].join(
+				" "
+			)} /* Remove special padding adding here */
+		>
+			{/* should be able to remove this margin bottom as we don't allow block padding that affects other blocks */}
 			<div className="margin-md-bottom">
 				<span className="ad_name margin-md-right">{adType || "Ad Name N/A"}</span>
 				<span>{slotName}</span>

6183ef3e95b3ebd4533bb8b4b6c1a988f115b141
Jack Howard <jackhowa@fastmail.com>
2022-08-01 15:52:23 -0500

    feat: Add admin view for ads view

--- blocks/ads-block/features/ads/_children/ArcAdminAd/index.jsx
+++ blocks/ads-block/features/ads/_children/ArcAdminAd/index.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-// todo: remove and replace
-import "./index.scss";
+import { Paragraph, Stack } from "@wpmedia/arc-themes-components";
 
-// could I use this in sb?
-// this is only visible on the admin side
-const ArcAdminAd = ({ adClass, adType, slotName, dimensions, isAdmin }) =>
+const ArcAdminAd = ({ adType, slotName, dimensions, isAdmin }) =>
 	isAdmin ? (
-		<div
-			className={["pb-ad-admin", "arcad", `ad-${adClass}`, "padding-sm-all"].join(
-				" "
-			)} /* Remove special padding adding here */
-		>
-			{/* should be able to remove this margin bottom as we don't allow block padding that affects other blocks */}
-			<div className="margin-md-bottom">
-				<span className="ad_name margin-md-right">{adType || "Ad Name N/A"}</span>
-				<span>{slotName}</span>
-			</div>
-			<div>{JSON.stringify(dimensions)}</div>
-		</div>
+		<Stack className="b-ads-block--admin">
+			<Stack>
+				<Paragraph>{adType || "Ad Name N/A"}</Paragraph>
+				<Paragraph>{slotName}</Paragraph>
+			</Stack>
+			<Paragraph>{JSON.stringify(dimensions)}</Paragraph>
+		</Stack>
 	) : null;
 
 export default ArcAdminAd;

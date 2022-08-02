de3d4b7549a4a963f6ca5d023f8f190686b32819
Matthew Roach <matthewroach@users.noreply.github.com>
2022-08-02 13:06:41 +0100

    Update logo alignment issues and right placement items

--- blocks/header-nav-chain-block/chains/header-nav-chain-block/default.jsx
+++ blocks/header-nav-chain-block/chains/header-nav-chain-block/default.jsx
@@ -43,11 +43,7 @@ export function PresentationalNav(props) {
 			className={`${BLOCK_CLASS_NAME} ${isScrolled ? `${BLOCK_CLASS_NAME}--scrolled` : ``}`}
 			aria-label={sectionAriaLabel}
 		>
-			<div
-				className={`${BLOCK_CLASS_NAME}__top-layout ${
-					logoAlignment === "center" ? `${BLOCK_CLASS_NAME}__top-layout--center-logo` : ``
-				}`}
-			>
+			<div className={`${BLOCK_CLASS_NAME}__top-layout`}>
 				<NavSection
 					blockClassName={BLOCK_CLASS_NAME}
 					customFields={customFields}
@@ -59,6 +55,7 @@ export function PresentationalNav(props) {
 				</NavSection>
 				<NavLogo
 					blockClassName={BLOCK_CLASS_NAME}
+					logoAlignment={logoAlignment}
 					imageSource={primaryLogoPath}
 					imageAltText={primaryLogoAlt}
 				/>

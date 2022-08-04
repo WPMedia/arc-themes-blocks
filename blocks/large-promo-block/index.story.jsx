984bd4a17620434edaed7c05369cc2c74ddc4337
Malavika Koppula <mkreddy1110@gmail.com>
2022-08-04 11:19:00 -0400

    fixed PlayVideoInPlace to use mock-content

--- blocks/large-promo-block/index.story.jsx
+++ blocks/large-promo-block/index.story.jsx
@@ -135,10 +135,6 @@ export const playVideoInPlaceOfImage = () => {
 		showImage: true,
 		playVideoInPlace: true,
 		aspectRatio: 16 / 9,
-		content: {
-			embed_html: `<div class="powa" id="powa-e924e51b-db94-492e-8346-02283a126943" data-org="corecomponents" data-env="prod" data-uuid="e924e51b-db94-492e-8346-02283a126943" data-aspect-ratio="0.562" data-api="prod"><script src="//d2w3jw6424abwq.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>`,
-			type: "video",
-		},
 	};
 
 	return <Promo customFields={updatedCustomFields} />;

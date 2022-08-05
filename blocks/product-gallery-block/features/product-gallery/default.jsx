2f6958bbacdc5d568c40ab8a00c969c29c0aee3b
Jack Howard <jackhowa@fastmail.com>
2022-08-05 15:13:40 -0500

    feat: Update to include data as presentational component

--- blocks/product-gallery-block/features/product-gallery/default.jsx
+++ blocks/product-gallery-block/features/product-gallery/default.jsx
@@ -1,28 +1,65 @@
 import React from "react";
 import PropTypes from "@arc-fusion/prop-types";
 import { useFusionContext, useComponentContext } from "fusion:context";
+import { RESIZER_APP_VERSION } from "fusion:environment";
 // import getProperties from "fusion:properties";
-import { Carousel } from "@wpmedia/arc-themes-components";
+// import getTranslatedPhrases from "fusion:intl";
+
+import { Carousel, Image } from "@wpmedia/arc-themes-components";
 
 const BLOCK_CLASS_NAME = "b-product-gallery";
 
-function ProductGallery({ customFields }) {
-	const { isFeaturedImageEnabled } = customFields;
-	const { id } = useComponentContext();
-
-	const { globalContent = {} } = useFusionContext();
-
-	if (!Object.keys(globalContent).length) {
-		return null;
-	}
+export function ProductGalleryDisplay(props) {
+	const { data, id, label, isFeaturedImageEnabled, resizerAppVersion } = props;
+	const carouselItems = data?.schema?.productGallery?.value?.assets.filter(
+		(asset) => asset.type === "image"
+	);
 
 	return (
 		<Carousel
 			className={`${BLOCK_CLASS_NAME}${
 				isFeaturedImageEnabled ? ` ${BLOCK_CLASS_NAME}--featured-image-enabled` : ""
 			}`}
+			key={id}
 			id={id}
 			label={label}
+		>
+			{carouselItems.map((item) => {
+				const { url, auth, alt_text: altText, _id: itemId } = item;
+
+				// take in app version that's the public key for the auth object in resizer v2
+				const targetAuth = auth[resizerAppVersion];
+				return (
+					<Carousel.Item key={itemId}>
+						<Image alt={altText} src={url} resizedOptions={{ auth: targetAuth }} />
+					</Carousel.Item>
+				);
+			})}
+		</Carousel>
+	);
+}
+function ProductGallery({ customFields }) {
+	const { isFeaturedImageEnabled } = customFields;
+	const { id } = useComponentContext();
+
+	const { /* arcSite , */ globalContent = {} } = useFusionContext();
+
+	if (!Object.keys(globalContent).length) {
+		return null;
+	}
+
+	// will get label from phrases
+	// const { locale } = getProperties(arcSite);
+	// const phrases = getTranslatedPhrases(locale);
+	const label = "label";
+
+	return (
+		<ProductGalleryDisplay
+			data={globalContent}
+			id={id}
+			isFeaturedImageEnabled={isFeaturedImageEnabled}
+			label={label}
+			resizerAppVersion={RESIZER_APP_VERSION}
 		/>
 	);
 }

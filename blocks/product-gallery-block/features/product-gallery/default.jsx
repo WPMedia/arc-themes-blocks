26b68ab56c45b3b281f379212b5663da5c0b7929
Jack Howard <jackhowa@fastmail.com>
2022-08-05 12:03:43 -0500

    feat: Render a carousel component if anything at all

--- blocks/product-gallery-block/features/product-gallery/default.jsx
+++ blocks/product-gallery-block/features/product-gallery/default.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "@arc-fusion/prop-types";
 import { useFusionContext } from "fusion:context";
+import { Carousel } from "@wpmedia/arc-themes-components";
 
 const BLOCK_CLASS_NAME = "b-product-gallery";
 
@@ -14,7 +15,7 @@ function ProductGallery({ customFields }) {
 	}
 
 	return (
-		<div
+		<Carousel
 			className={`${BLOCK_CLASS_NAME} ${BLOCK_CLASS_NAME}--featured-image-${
 				isFeaturedImageEnabled ? `enabled` : `disabled`
 			}`}

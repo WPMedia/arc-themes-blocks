27c02062916d4b2b7e6ef6abe55e4111fd8233be
Matthew Roach <matthewroach@users.noreply.github.com>
2022-09-01 10:47:11 +0100

    Remove resized_params

--- .storybook/mock-content/mediumPromo.js
+++ .storybook/mock-content/mediumPromo.js
@@ -41,20 +41,6 @@ export const mediumPromoMock = {
 			auth: {
 				2: "75f6b4c64c7889dc8eadf6a328999d522be2e2397c7b9a5a0704f6d9afa60fcf",
 			},
-			resized_params: {
-				"274x154":
-					"LBTiSxaxr1Eo-tEz9BKFCnZNArw=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-				"274x183":
-					"Z3YDeb5U67phnWn-jjZ1D3raqLg=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-				"274x206":
-					"bg-hZozUyDasSC82dzGY6h7OzQE=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-				"400x225":
-					"0h3vL_qyc8pjZN_cnwYgHECxICA=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-				"400x267":
-					"SBSiikbg6B9BLxiEqnZhBDbOelY=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-				"400x300":
-					"4oA14YOFAWISqfWe7GayPCW-l9k=filters:format(jpg):quality(70):focal(4335x1885:4345x1895)/",
-			},
 			type: "image",
 			url: "https://cloudfront-us-east-1.images.arcpublishing.com/sandbox.themesinternal/EM5DTGYGABDJZODV7YVFOC2DOM.jpeg",
 		},

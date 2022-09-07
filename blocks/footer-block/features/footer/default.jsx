d289846d1f56ce2d7738c2cee00486a42716579a
nschubach <nschubach@gmail.com>
2022-09-07 12:29:15 -0400

    TMEDIA 725: Convert Footer block (#1496)
    * chore: Add basic setup for v2 blocks
    
    * docs: Ensure doc is useful and does not contain placeholder info
    
    * feat: Update footer parent width to match full width and content max width based on mocks
    
    * feat: Add grid. Use matching grid gap of 24px to the preexisting
    
    * docs: Comment out copyright for now
    
    * fix: Remove spacing the block based on layout expectations
    
    * feat: Add icon and styles
    
    * fix: Remove test code for fallbacks
    
    * style: Fix spacing of scss file per stylelint requirements
    
    * feat: Update to the updated designs. Use not last child to get border correct
    
    * style: Update order of footer links
    
    * chore: Remove now deprecated style sheet
    
    * feat: Follow previous implementation of width and centering the text in footer
    
    * feat: Convert link to v2 architecture. Use Link Children text
    
    * ci: Update themes components
    
    * feat: Use default section as I need to change flex direction dynamically
    
    * TMEDIA-725: Footer
    
    * Don't render grid if no items
    
    * sort
    
    * Add list styles and remove unused copyright text class
    
    * small image adjustment
    
    * Update blocks/footer-block/features/footer/default.jsx
    
    * Remove duplicate styles and unecessary tokens
    
    * actually add the file I changed
    
    * Update blocks/footer-block/README.md
    
    Co-authored-by: Eric Carlisle <ecarlisle@users.noreply.github.com>
    
    * documentation
    
    * fix link edges
    
    * Update blocks/footer-block/_index.scss
    
    Co-authored-by: Jack Howard <jackhowa@fastmail.com>
    Co-authored-by: Eric Carlisle <ecarlisle@users.noreply.github.com>

--- blocks/footer-block/features/footer/default.jsx
+++ blocks/footer-block/features/footer/default.jsx
@@ -1,32 +1,25 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "@arc-fusion/prop-types";
 import { useContent } from "fusion:content";
 import { useFusionContext } from "fusion:context";
 import getProperties from "fusion:properties";
 import getTranslatedPhrases from "fusion:intl";
-import getThemeStyle from "fusion:themes";
-// import Link from "@wpmedia/links-bar-block";
+
+import { LazyLoad } from "@wpmedia/engine-theme-sdk";
 import {
-	FacebookAltIcon,
-	TwitterIcon,
-	RssIcon,
-	LazyLoad,
+	Grid,
+	Heading,
+	HeadingSection,
 	isServerSide,
-} from "@wpmedia/engine-theme-sdk";
-import { PrimaryFont } from "@wpmedia/shared-styles";
-import "./footer.scss";
-// switch to component link when footer converting to V2
-import Link from "./_children/link";
+	Icon,
+	Image,
+	Link,
+	MediaItem,
+	Paragraph,
+	Stack,
+} from "@wpmedia/arc-themes-components";
 
-export const StyledSocialContainer = styled.div`
-	border: ${(props) => (props.hasSocialLinks ? "1px" : "0")} solid ${(props) => props.primaryColor};
-	fill: ${(props) => props.primaryColor};
-
-	a {
-		border-right: 1px solid ${(props) => props.primaryColor};
-	}
-`;
+const BLOCK_CLASS_NAME = "b-footer";
 
 const FooterItem = ({ customFields: { navigationConfig } }) => {
 	const { arcSite, deployment, contextPath } = useFusionContext();
@@ -41,7 +34,6 @@ const FooterItem = ({ customFields: { navigationConfig } }) => {
 		primaryLogoAlt,
 		locale,
 	} = getProperties(arcSite);
-
 	const phrases = getTranslatedPhrases(locale);
 
 	// Check if URL is absolute/base64
@@ -57,141 +49,100 @@ const FooterItem = ({ customFields: { navigationConfig } }) => {
 			feature: "footer",
 		},
 		filter: `{
-      children {
-        _id
-        node_type
-        display_name
-        name
-        url
-        children {
-          _id
-          node_type
-          display_name
-          name
-          url
-        }
-      }
-    }`,
+			children {
+				_id
+				node_type
+				display_name
+				name
+				url
+				children {
+					_id
+					node_type
+					display_name
+					name
+					url
+				}
+			}
+		}`,
 	});
 
-	const footerColumns = content && content.children ? content.children : [];
-
-	const socialButtons = (
-		<>
-			{facebookPage ? (
-				<a
-					title={phrases.t("footer-block.facebook-link")}
-					target="_blank"
-					rel="noopener noreferrer"
-					href={facebookPage}
-				>
-					<FacebookAltIcon fill={getThemeStyle(arcSite)["primary-color"]} />
-				</a>
-			) : (
-				""
-			)}
-			{twitterUsername ? (
-				<a
-					title={phrases.t("footer-block.twitter-link")}
-					target="_blank"
-					rel="noopener noreferrer"
-					href={`https://twitter.com/${twitterUsername}`}
-				>
-					<TwitterIcon fill={getThemeStyle(arcSite)["primary-color"]} />
-				</a>
-			) : (
-				""
-			)}
-			{rssUrl ? (
-				<a
-					title={phrases.t("footer-block.rss-link")}
-					target="_blank"
-					rel="noopener noreferrer"
-					href={rssUrl}
-				>
-					<RssIcon fill={getThemeStyle(arcSite)["primary-color"]} />
-				</a>
-			) : (
-				""
-			)}
-		</>
-	);
-
 	return (
-		<div className="container layout-section">
-			<div className="section-separator">
-				<section className="footer-header">
-					<div className="footer-row">
-						<div className="social-column">
-							<StyledSocialContainer
-								className="socialBtn-container"
-								primaryColor={getThemeStyle(arcSite)["primary-color"]}
-								hasSocialLinks={!!(facebookPage || twitterUsername || rssUrl)}
-							>
-								{socialButtons}
-							</StyledSocialContainer>
-						</div>
-						<div className="copyright-column">
-							{/* If large screen, show copyright over border */}
-							<PrimaryFont
-								as="p"
-								className="copyright"
-								id="copyright-top"
-								style={{ width: "100%" }}
-							>
-								{copyrightText}
-							</PrimaryFont>
-						</div>
-					</div>
-				</section>
-			</div>
-			<div>
-				{/* If small screen, show copyright under border */}
-				<p className="copyright" id="copyright-bottom" style={{ width: "100%" }}>
-					{copyrightText}
-				</p>
-			</div>
-			<div className="row legacy-footer-row">
-				{/* The columns are 2D arrays of columns x column items. Iterate through both */}
-				{footerColumns.map((column) => {
-					const columnItems = column.children
-						? column.children.map((item) => (
-								<li className="footer-item" key={item._id}>
-									{item.node_type === "link" ? (
-										<Link href={item.url} name={item.display_name} />
-									) : (
-										<Link href={item._id} name={item.name} />
-									)}
-								</li>
-						  ))
-						: [];
-
-					if (columnItems.length === 0) {
-						return null;
-					}
-
-					return (
-						<div className="footer-section col-sm-12 col-md-6 col-lg-xl-3" key={column._id}>
-							{column.name ? (
-								<PrimaryFont as="h4" className="footer-header">
-									{column.name}
-								</PrimaryFont>
+		<Stack className={BLOCK_CLASS_NAME}>
+			<section className={`${BLOCK_CLASS_NAME}__top-container`}>
+				{facebookPage || twitterUsername || rssUrl ? (
+					<div className={`${BLOCK_CLASS_NAME}__social-links-container`}>
+						<Stack className={`${BLOCK_CLASS_NAME}__social-links`} direction="horizontal">
+							{facebookPage ? (
+								<Link
+									href={facebookPage}
+									openInNewTab
+									supplementalText={phrases.t("footer-block.facebook-link")}
+								>
+									<Icon name="Facebook" />
+								</Link>
 							) : null}
-							<PrimaryFont as="ul">{columnItems}</PrimaryFont>
-						</div>
-					);
-				})}
-			</div>
-			{logoUrl ? (
-				<div className="primaryLogo">
-					<img
-						src={logoUrl}
-						alt={(lightBackgroundLogo ? lightBackgroundLogoAlt : primaryLogoAlt) || ""}
-						className="footer-logo"
-					/>
-				</div>
+							{twitterUsername ? (
+								<Link
+									href={`https://twitter.com/${twitterUsername}`}
+									openInNewTab
+									supplementalText={phrases.t("footer-block.twitter-link")}
+								>
+									<Icon name="Twitter" />
+								</Link>
+							) : null}
+							{rssUrl ? (
+								<Link
+									href={rssUrl}
+									openInNewTab
+									supplementalText={phrases.t("footer-block.rss-link")}
+								>
+									<Icon name="Rss" />
+								</Link>
+							) : null}
+						</Stack>
+					</div>
+				) : null}
+				<Paragraph>{copyrightText}</Paragraph>
+			</section>
+			{content?.children?.length ? (
+				<Grid className={`${BLOCK_CLASS_NAME}__links`}>
+					{/* The columns are 2D arrays of columns x column items. Iterate through both */}
+					{content.children.map((column) =>
+						column?.children?.length ? (
+							<div className={`${BLOCK_CLASS_NAME}__links-group`} key={column._id}>
+								{column.name ? (
+									<HeadingSection>
+										<Heading>{column.name}</Heading>
+									</HeadingSection>
+								) : null}
+								<ul className={`${BLOCK_CLASS_NAME}__links-group-list`}>
+									{column.children.map((item) => (
+										<li className={`${BLOCK_CLASS_NAME}__links-group-list-item`} key={item._id}>
+											{item.node_type === "link" ? (
+												<Link href={item.url}>{item.display_name}</Link>
+											) : (
+												<Link href={item._id}>{item.name}</Link>
+											)}
+										</li>
+									))}
+								</ul>
+							</div>
+						) : null
+					)}
+				</Grid>
 			) : null}
-		</div>
+			{logoUrl ? (
+				<MediaItem>
+					<Image
+						alt={(lightBackgroundLogo ? lightBackgroundLogoAlt : primaryLogoAlt) || ""}
+						className={`${BLOCK_CLASS_NAME}__logo`}
+						src={logoUrl}
+						height={64}
+					/>
+				</MediaItem>
+			) : null}
+			<div />
+		</Stack>
 	);
 };
 

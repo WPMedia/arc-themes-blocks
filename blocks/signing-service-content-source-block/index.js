7d4832bf8e5cbe75cf4f2ba623aa61234f4f6cf4
Nick Schubach <nschubach@gmail.com>
2022-10-19 17:24:45 -0400

    Update package-lock and import/export syntax

--- blocks/signing-service-content-source-block/index.js
+++ blocks/signing-service-content-source-block/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line prefer-destructuring */
 import service from "./sources/signing-service";
-
-export const fetch = service.fetch;
+/* eslint-disable-next-line import/prefer-default-export */
+export const { fetch } = service;

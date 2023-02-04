061079c4fda3a50c2e13ca9ea8e73f2b56e3ad07
Ryan Barnett <ryan.barnett@washpost.com>
2023-02-04 12:47:17 -0500

    feat: add ANS sanitization with stub to be replaced with an engine-sdk package

--- blocks/collections-content-source-block/sources/content-api-collections.js
+++ blocks/collections-content-source-block/sources/content-api-collections.js
@@ -2,6 +2,57 @@ import axios from "axios";
 import { CONTENT_BASE, ARC_ACCESS_TOKEN } from "fusion:environment";
 import getResizedImageData from "@wpmedia/resizer-image-block";
 
+// uncomment when engine-theme-sdk canary package is available
+// import { sanitizeANS, constants } from "@wpmedia/engine-theme-sdk";
+
+// replace with above import when engine-theme-sdk canary has method available
+const sanitizeANSItem = (data) => ({
+	...data,
+	editor_note: "",
+	planning: {
+		...data.planning,
+		internal_note: "",
+	},
+	workflow: {},
+	additional_properties: {
+		...data.additional_properties,
+		clipboard: {},
+	},
+	content_elements: data.content_elements.map((el) => ({
+		...el,
+		additional_properties: {
+			...el.additional_properties,
+			inline_comments: [],
+			comments: [],
+		},
+	})),
+});
+
+/**
+ *
+ * @param data {object} ANS JSON
+ * @param schema {string("ans-item" | "ans-feed") ANS schema
+ * @returns {object} ANS data with empty values on the fields:
+ * - editor_note
+ * - planning.internal_note
+ * - workflow
+ * - additional_properties.clipboard
+ * - content_elements.additional_properties.comments
+ * - content_elements.additional_properties.inline_comments
+ */
+const sanitizeANS = (data, schema) => {
+	if (schema === "ans-feed") {
+		return {
+			...data,
+			content_elements: data.content_elements.map((el) => sanitizeANSItem(el)),
+		};
+	}
+	if (schema === "ans-item") {
+		return sanitizeANSItem(data);
+	}
+	return data;
+};
+
 const params = {
 	_id: "text",
 	content_alias: "text",
@@ -61,5 +112,6 @@ export default {
 	fetch,
 	schemaName: "ans-feed",
 	// other options null use default functionality, such as filter quality
-	transform: (data, query) => getResizedImageData(data, null, null, null, query["arc-site"]),
+	transform: (data, query) =>
+		getResizedImageData(sanitizeANS(data, "ans-feed"), null, null, null, query["arc-site"]),
 };
--- blocks/story-feed-author-content-source-block/sources/story-feed-author.js
+++ blocks/story-feed-author-content-source-block/sources/story-feed-author.js
@@ -1,4 +1,54 @@
 import getResizedImageData from "@wpmedia/resizer-image-block";
+// uncomment when engine-theme-sdk canary package is available
+// import { sanitizeANS, constants } from "@wpmedia/engine-theme-sdk";
+
+// replace with above import when engine-theme-sdk canary has method available
+const sanitizeANSItem = (data) => ({
+	...data,
+	editor_note: "",
+	planning: {
+		...data.planning,
+		internal_note: "",
+	},
+	workflow: {},
+	additional_properties: {
+		...data.additional_properties,
+		clipboard: {},
+	},
+	content_elements: data.content_elements.map((el) => ({
+		...el,
+		additional_properties: {
+			...el.additional_properties,
+			inline_comments: [],
+			comments: [],
+		},
+	})),
+});
+
+/**
+ *
+ * @param data {object} ANS JSON
+ * @param schema {string("ans-item" | "ans-feed") ANS schema
+ * @returns {object} ANS data with empty values on the fields:
+ * - editor_note
+ * - planning.internal_note
+ * - workflow
+ * - additional_properties.clipboard
+ * - content_elements.additional_properties.comments
+ * - content_elements.additional_properties.inline_comments
+ */
+const sanitizeANS = (data, schema) => {
+	if (schema === "ans-feed") {
+		return {
+			...data,
+			content_elements: data.content_elements.map((el) => sanitizeANSItem(el)),
+		};
+	}
+	if (schema === "ans-item") {
+		return sanitizeANSItem(data);
+	}
+	return data;
+};
 
 export default {
 	resolve: ({ authorSlug, feedSize = 8, feedOffset = 0, "arc-site": website }) =>
@@ -10,5 +60,6 @@ export default {
 		feedOffset: "number",
 	},
 	// other options null use default functionality, such as filter quality
-	transform: (data, query) => getResizedImageData(data, null, null, null, query["arc-site"]),
+	transform: (data, query) =>
+		getResizedImageData(sanitizeANS(data, "ans-feed"), null, null, null, query["arc-site"]),
 };

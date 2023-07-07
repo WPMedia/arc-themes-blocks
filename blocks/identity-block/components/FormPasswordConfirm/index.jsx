ae84eb0ffb961aa201a81b50827d22a4e211b2f6
Vito Galatro <vjgalatro@gmail.com>
2023-07-07 13:21:32 -0400

    THEMES-758: moved unconverted files needed for signup form into block.

---
+++ blocks/identity-block/components/FormPasswordConfirm/index.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import { FormInputField, FIELD_TYPES } from "@wpmedia/shared-styles";
+
+const FormPasswordConfirm = ({
+	autoComplete,
+	confirmLabel,
+	confirmPlaceholder,
+	confirmTip,
+	confirmValidationErrorMessage,
+	label,
+	name,
+	placeholder,
+	showDefaultError = false,
+	tip,
+	validationPattern,
+	validationErrorMessage,
+}) => {
+	const [password, setPassword] = useState("");
+
+	const fieldParameters = {
+		...(autoComplete ? { autoComplete } : {}),
+		...(placeholder ? { placeholder } : {}),
+		...(showDefaultError ? { showDefaultError } : {}),
+		...(tip ? { tip } : {}),
+		...(validationErrorMessage ? { validationErrorMessage } : {}),
+		...(validationPattern ? { validationPattern } : {}),
+	};
+
+	const confirmFieldParameters = {
+		...(autoComplete ? { autoComplete } : {}),
+		...(confirmPlaceholder ? { placeholder: confirmPlaceholder } : {}),
+		...(showDefaultError ? { showDefaultError } : {}),
+		...(confirmTip ? { tip: confirmTip } : {}),
+		...(confirmValidationErrorMessage
+			? { validationErrorMessage: confirmValidationErrorMessage }
+			: {}),
+	};
+
+	return (
+		<>
+			<FormInputField
+				{...fieldParameters}
+				label={label}
+				name={name}
+				onChange={({ value }) => setPassword(value)}
+				required
+				type={FIELD_TYPES.PASSWORD}
+			/>
+			<FormInputField
+				{...confirmFieldParameters}
+				label={confirmLabel}
+				name={`${name}-confirmation`}
+				required
+				type={FIELD_TYPES.PASSWORD}
+				validationPattern={`^${password}$`}
+			/>
+		</>
+	);
+};
+
+export default FormPasswordConfirm;

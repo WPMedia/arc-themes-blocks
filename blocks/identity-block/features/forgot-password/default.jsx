c6673234828711c289cfadad81649056419038b3
Vito Galatro <vjgalatro@gmail.com>
2023-08-09 15:50:21 -0400

    THEMES-763: converted code in forgot password block to use themes components.

--- blocks/identity-block/features/forgot-password/default.jsx
+++ blocks/identity-block/features/forgot-password/default.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-
-import { FIELD_TYPES, FormInputField, PrimaryFont } from "@wpmedia/shared-styles";
-
 import { useFusionContext } from "fusion:context";
 import getProperties from "fusion:properties";
 import getTranslatedPhrases from "fusion:intl";
+import { Heading, Input, Paragraph } from "@wpmedia/arc-themes-components";
+import HeadlinedSubmitForm from "../../components/headlined-submit-form";
+import useIdentity from "../../components/identity";
 
-import HeadlinedSubmitForm from "../../components/HeadlinedSubmitForm";
-import useIdentity from "../../components/Identity";
-import "./styles.scss";
+const BLOCK_CLASS_NAME = "b-forgot-password";
 
 const ForgotPassword = () => {
 	const { arcSite } = useFusionContext();
@@ -21,21 +19,19 @@ const ForgotPassword = () => {
 
 	if (submitted) {
 		return (
-			<section>
-				<PrimaryFont as="h2" className="xpmedia-forgot-password-headline">
-					{phrases.t("identity-block.forgot-password-headline-submitted")}
-				</PrimaryFont>
-				<PrimaryFont className="xpmedia-forgot-password-instruction">
-					{phrases.t("identity-block.forgot-password-instruction-submitted")}
-				</PrimaryFont>
+			<section className={BLOCK_CLASS_NAME}>
+				<Heading>{phrases.t("identity-block.forgot-password-headline-submitted")}</Heading>
+				<Paragraph>{phrases.t("identity-block.forgot-password-instruction-submitted")}</Paragraph>
 			</section>
 		);
 	}
 
 	return isInitialized ? (
 		<HeadlinedSubmitForm
-			headline={phrases.t("identity-block.forgot-password-headline")}
 			buttonLabel={phrases.t("identity-block.forgot-password-submit")}
+			className={BLOCK_CLASS_NAME}
+			formErrorText={error}
+			headline={phrases.t("identity-block.forgot-password-headline")}
 			onSubmit={({ email }) =>
 				Identity.requestResetPassword(email)
 					.then(() => {
@@ -43,18 +39,15 @@ const ForgotPassword = () => {
 					})
 					.catch(() => setError(phrases.t("identity-block.forgot-password-error")))
 			}
-			formErrorText={error}
 		>
-			<PrimaryFont className="xpmedia-forgot-password-instruction">
-				{phrases.t("identity-block.forgot-password-instruction")}
-			</PrimaryFont>
-			<FormInputField
+			<Paragraph>{phrases.t("identity-block.forgot-password-instruction")}</Paragraph>
+			<Input
 				autoComplete="email"
 				label={phrases.t("identity-block.email")}
 				name="email"
 				required
 				showDefaultError={false}
-				type={FIELD_TYPES.EMAIL}
+				type="email"
 				validationErrorMessage={phrases.t("identity-block.email-requirements")}
 			/>
 		</HeadlinedSubmitForm>

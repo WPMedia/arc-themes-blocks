6673c30e158f90338c30ab2451e3ce4d3b83cf53
Vito Galatro <vjgalatro@gmail.com>
2023-08-10 14:26:42 -0400

    THEMES-763: updated tests for forgot password block.

--- blocks/identity-block/features/forgot-password/default.test.jsx
+++ blocks/identity-block/features/forgot-password/default.test.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { mount } from "enzyme";
-import { act } from "react-dom/test-utils";
+import { fireEvent, render, screen } from "@testing-library/react";
 import ForgotPassword from "./default";
-
-import useIdentity from "../../components/Identity";
+import useIdentity from "../../components/identity";
 
 jest.mock("../../components/Identity");
 
@@ -11,45 +9,22 @@ const resetMock = jest.fn(() => Promise.resolve());
 const resetFailMock = jest.fn(() => Promise.reject());
 
 jest.mock("fusion:properties", () => jest.fn(() => ({})));
-jest.mock("fusion:intl", () => ({
-	__esModule: true,
-	default: jest.fn(() => ({
-		t: jest.fn(
-			(phrase) =>
-				({
-					"identity-block.email": "Email",
-					"identity-block.email-requirements": "Email Required",
-					"identity-block.forgot-password-error": "Uh oh!",
-					"identity-block.forgot-password-headline": "Headline",
-					"identity-block.forgot-password-headline-submitted": "Congratulations!",
-					"identity-block.forgot-password-instruction": "Do the email thing",
-					"identity-block.forgot-password-instruction-submitted": "You did the thing!",
-					"identity-block.forgot-password-submit": "Submit",
-					undefined: "",
-				}[phrase])
-		),
-	})),
-}));
 
 describe("Identity Password Reset Feature", () => {
-	let wrapper;
-	beforeEach(async () => {
+	beforeEach(() => {
 		useIdentity.mockImplementation(() => ({
 			isInitialized: true,
 			isLoggedIn: () => true,
 			Identity: {
-				isLoggedIn: jest.fn(async () => false),
-				getConfig: jest.fn(async () => ({})),
+				isLoggedIn: jest.fn(() => false),
+				getConfig: jest.fn(() => ({})),
 				requestResetPassword: resetMock,
 			},
 		}));
-		await act(async () => {
-			wrapper = await mount(<ForgotPassword />);
-		});
 	});
 
 	afterAll(() => {
-		jest.restoreAllMocks();
+		jest.clearAllMocks();
 	});
 
 	it("renders nothing if identity not initialized", () => {
@@ -57,76 +32,62 @@ describe("Identity Password Reset Feature", () => {
 			isInitialized: false,
 			isLoggedIn: () => true,
 			Identity: {
-				isLoggedIn: jest.fn(async () => false),
-				getConfig: jest.fn(async () => ({})),
+				isLoggedIn: jest.fn(() => false),
+				getConfig: jest.fn(() => ({})),
 			},
 		}));
-		const uninitializedWrapper = mount(<ForgotPassword />);
-		expect(uninitializedWrapper.html()).toBe(null);
+		render(<ForgotPassword />);
+		expect(screen.queryByRole("form")).toBe(null);
 	});
 
 	it("renders", () => {
-		expect(wrapper.html()).not.toBe(null);
+		render(<ForgotPassword />);
+		expect(screen.queryByRole("form")).not.toBe(null);
 	});
 
 	it("shows submit form", () => {
-		expect(wrapper.find("div.xpmedia-forgot-password-instruction")).toHaveLength(1);
-		expect(wrapper.find("input.xpmedia-form-field-input")).toHaveLength(1);
-		expect(wrapper.find("form.xpmedia-form-submittable button")).toHaveLength(1);
-
-		expect(wrapper.find("input.xpmedia-form-field-input").at(0).prop("autoComplete")).toBe("email");
+		render(<ForgotPassword />);
+		expect(screen.queryByRole("form")).not.toBe(null);
+		expect(screen.getByText("identity-block.forgot-password-instruction")).not.toBe(null);
+		expect(screen.getByLabelText("identity-block.email")).not.toBe(null);
+		expect(screen.getByRole("button")).not.toBe(null);
 	});
 
 	it("uses updates the form on submit", async () => {
-		const submitWrapper = mount(<ForgotPassword />);
-
-		submitWrapper.find('input[type="email"]').instance().value = "test@test.com";
-
-		await act(async () => {
-			submitWrapper.find("form").simulate("submit");
-			await resetMock;
-			expect(resetMock).toHaveBeenCalled();
-			submitWrapper.update();
+		render(<ForgotPassword />);
+		fireEvent.change(screen.getByLabelText("identity-block.email"), {
+			target: { value: "email@test.com" },
 		});
-
-		expect(submitWrapper.find("h2.xpmedia-forgot-password-headline")).toHaveLength(1);
-		expect(submitWrapper.find("div.xpmedia-forgot-password-instruction")).toHaveLength(1);
+		await fireEvent.click(screen.getByRole("button"));
+		expect(screen.getByText("identity-block.forgot-password-headline-submitted")).not.toBe(null);
+		expect(screen.getByText("identity-block.forgot-password-instruction-submitted")).not.toBe(null);
 	});
 });
 
 describe("Identity Password Reset Feature - Failing", () => {
-	let wrapper;
-	beforeEach(async () => {
+	beforeEach(() => {
 		useIdentity.mockImplementation(() => ({
 			isInitialized: true,
 			isLoggedIn: () => true,
 			Identity: {
-				isLoggedIn: jest.fn(async () => false),
-				getConfig: jest.fn(async () => ({})),
+				isLoggedIn: jest.fn(() => false),
+				getConfig: jest.fn(() => ({})),
 				requestResetPassword: resetFailMock,
 			},
 		}));
-		await act(async () => {
-			wrapper = await mount(<ForgotPassword />);
-		});
 	});
 
 	afterAll(() => {
-		jest.restoreAllMocks();
+		jest.clearAllMocks();
 	});
 
 	it("enter error state on failure to submit", async () => {
-		wrapper.find('input[type="email"]').instance().value = "test@test.com";
-
-		await act(async () => {
-			wrapper.find("form").simulate("submit");
-			await resetFailMock;
-			expect(resetFailMock).toHaveBeenCalled();
-			wrapper.update();
+		render(<ForgotPassword />);
+		fireEvent.change(screen.getByLabelText("identity-block.email"), {
+			target: { value: "email@test.com" },
 		});
-
-		wrapper.update();
-
-		expect(wrapper.find("section.xpmedia-form-error")).toHaveLength(1);
+		await fireEvent.click(screen.getByRole("button"));
+		await expect(resetFailMock).toHaveBeenCalled();
+		expect(screen.getByText("identity-block.forgot-password-error")).not.toBe(null);
 	});
 });

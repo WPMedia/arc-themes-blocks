57bee5086d28cde804f3fe1eda215ff2b1f97199
Nick Schubach <nschubach@gmail.com>
2023-10-17 15:14:30 -0400

    fix adapter import

--- jest/testSetupFile.js
+++ jest/testSetupFile.js
@@ -1,17 +1,17 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { configure } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
 
 configure({ adapter: new Adapter() });
 
 jest.mock("@wpmedia/placeholder-image-block", () => ({
-	__esModule: true,
-	default: function PlaceholderImage() {
-		return <img alt="placeholder" />;
-	},
+  __esModule: true,
+  default: function PlaceholderImage() {
+    return <img alt="placeholder" />;
+  },
 }));
 
 beforeEach(() => {
-	jest.resetModules();
+  jest.resetModules();
 });

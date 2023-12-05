ab13c688404161a84215dc49d5f3b35577f8c435
Laura Alejandra Pinilla Buitrago <laura.pinilla@wizeline.com>
2023-12-05 12:56:09 -0600

    attending feedback

--- blocks/subscriptions-block/components/usePaywall.js
+++ blocks/subscriptions-block/components/usePaywall.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 
 import { useFusionContext } from "fusion:context";
 import getProperties from "fusion:properties";

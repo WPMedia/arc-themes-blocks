f7190916be738bc58cf907c8af4f01655e57b6f5
Edward Cho <edwardcho1231@gmail.com>
2024-04-08 14:18:12 -0400

    update test

--- blocks/subscriptions-block/features/checkout/_children/BillingAddress/index.test.jsx
+++ blocks/subscriptions-block/features/checkout/_children/BillingAddress/index.test.jsx
@@ -1,34 +1,38 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import { useSales, usePhrases } from "@wpmedia/arc-themes-components";
 import BillingAddress from "./index";
 
-jest.mock("@wpmedia/arc-themes-components");
-
-describe('CheckoutCardDetail component', () => {
-
-  it("redners billing address component",  async () => {
-    useSales.mockImplementation(() => ({
-      isInitialized: true,
+jest.mock("@wpmedia/arc-themes-components", () => ({
+	...jest.requireActual("@wpmedia/arc-themes-components"),
+  useSales: jest.fn(() => ({
+    isInitialized: true,
       Sales: {
         getCart: jest.fn(async() => {})
       }
-    }));
-
-    usePhrases.mockImplementation(() => ({
+  })),
+  usePhrases: jest.fn(() => ({
 			t: jest.fn((phrase => phrase))
-		}))
+  }))
+}));
 
+describe('CheckoutCardDetail component', () => {
+  it("renders billing address component",  async () => {
     render(
       <BillingAddress
         billingAddress={{}}
         setBillingAddress={jest.fn()}
-        setIsOpen={jest.fn()}
+        setIsOpen={jest.fn()}      
         setIsComplete={jest.fn()}
       />
     )
-    expect(screen.getByTestId('billing-address')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.address1')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.address2')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.country-region')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.city')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.state')).toBeInTheDocument();
+    expect(screen.getByText('checkout-block.zip-code')).toBeInTheDocument();
   });
 })

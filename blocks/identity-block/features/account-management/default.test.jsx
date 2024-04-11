8ccacaa28ade234ab3be831a7c3b5c4d6ff31bff
LauraPinilla <laura.pinilla@wizeline.com>
2024-04-10 19:36:15 -0600

    fixing linting errors

--- blocks/identity-block/features/account-management/default.test.jsx
+++ blocks/identity-block/features/account-management/default.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { useIdentity } from "@wpmedia/arc-themes-components";
 import AccountManagement, { AccountManagementPresentational } from "./default";
@@ -8,20 +8,75 @@ jest.mock("./_children/EmailEditableFieldContainer", () => () => <div>Email</div
 jest.mock("./_children/PasswordEditableFieldContainer", () => () => <div>Password</div>);
 jest.mock("./_children/SocialEditableSection", () => () => <div>Social</div>);
 
-jest.mock("@wpmedia/arc-themes-components");
+jest.mock("fusion:context", () => ({
+	useFusionContext: () => ({ isAdmin: false, globalContent: { credits: {} } }),
+}));
+jest.mock("fusion:properties", () => jest.fn(() => ({})));
+
+const defaultCustomFields = {
+	redirectURL: "",
+	showEmail: true,
+	showPassword: true,
+	showSocialProfile: false,
+};
+
+// jest.mock("@wpmedia/arc-themes-components");
+jest.mock("@arc-publishing/sdk-identity", () => ({
+	__esModule: true,
+	default: {
+		apiOrigin: "http://origin/",
+		options: jest.fn(),
+	},
+}));
+
+jest.mock("fusion:properties", () =>
+	jest.fn(() => ({
+		api: {
+			identity: {
+				origin: "https://corecomponents-arc-demo-3-prod.api.cdn.arcpublishing.com",
+			},
+		},
+	})),
+);
+
+const mockLogin = jest.fn(() => Promise.resolve());
+const mockIdentity = {
+	apiOrigin: "http://origin/",
+	isLoggedIn: jest.fn(() => false),
+	getConfig: jest.fn(() => ({})),
+	getUserProfile: jest.fn(() => ({})),
+	login: mockLogin,
+};
+
+jest.mock("@wpmedia/arc-themes-components", () => ({
+	useIdentity: jest.fn(() => ({
+		isInitialized: true,
+		Identity: {
+			...mockIdentity,
+		},
+	})),
+	Heading: ({ children }) => <div data-testid="Heading"> {children} </div>,
+	Input: ({ type, label }) => (
+		<div>
+			<label>{label}</label>
+			<input type={type} />
+		</div>
+	),
+}));
 
 const userProfileMock = jest.fn(() =>
 	Promise.resolve({ email: "test@domain.com", identities: [] }),
 );
 
-describe("Account management", () => {
+describe.only("Account management", () => {
 	afterAll(() => {
 		jest.restoreAllMocks();
 	});
 
 	it("renders header text", async () => {
 		render(<AccountManagementPresentational header="header" />);
-		await expect(screen.findByText("header")).not.toBeNull(); // eslint-disable-line
+		const title = screen.getByText("header");
+		expect(title).not.toBeNull();
 	});
 
 	it("should render account management if logged in and initialized", async () => {
@@ -35,8 +90,11 @@ describe("Account management", () => {
 			},
 		}));
 
-		render(<AccountManagement customFields={{}} />);
-		await expect(screen.findByText("Account Information")).not.toBeNull(); // eslint-disable-line
+		render(<AccountManagement customFields={defaultCustomFields} />);
+
+		await waitFor(() =>
+			expect(screen.getByText("identity-block.account-information")).not.toBeNull(),
+		);
 	});
 
 	it("should not render if not logged in and not initialized", async () => {
@@ -62,7 +120,8 @@ describe("Account management", () => {
 		}));
 
 		render(<AccountManagement customFields={{ showEmail: true }} />);
-		await expect(screen.findByText("Email")).not.toBeNull(); // eslint-disable-line
+
+		await waitFor(() => expect(screen.getByText("Email")).not.toBeNull());
 	});
 
 	it("hides email input editable field if showing email", () => {
@@ -81,7 +140,8 @@ describe("Account management", () => {
 		}));
 
 		render(<AccountManagement customFields={{ showPassword: true }} />);
-		await expect(screen.findByText("Password")).not.toBeNull(); // eslint-disable-line
+
+		await waitFor(() => expect(screen.getByText("Password")).not.toBeNull());
 	});
 
 	it("hides password input editable field if showing password", async () => {
@@ -100,7 +160,8 @@ describe("Account management", () => {
 		}));
 
 		render(<AccountManagement customFields={{ showSocialProfile: true }} />);
-		await expect(screen.findByText("Password")).not.toBeNull(); // eslint-disable-line
+
+		await waitFor(() => expect(screen.getByText("Social")).not.toBeNull());
 	});
 
 	it("hides social profile if showing social", async () => {

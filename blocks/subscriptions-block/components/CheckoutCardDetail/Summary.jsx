919c5dc8711588e27c78ed8c0c14420a39ee118a
LauraPinilla <laura.pinilla@wizeline.com>
2024-04-23 13:40:06 -0600

    brands creditDebitCard and Summary

---
+++ blocks/subscriptions-block/components/CheckoutCardDetail/Summary.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { usePhrases, Paragraph } from "@wpmedia/arc-themes-components";
+import PaymentIcon from "../PaymentIcons";
+
+export const SummaryAccount = ({account}) => (
+	<Paragraph>{account?.email}</Paragraph>
+);
+
+export const SummaryBillingAddress = ({billingAddress}) => {
+	return (
+		<Paragraph>
+			{billingAddress?.line1 ||
+				(billingAddress?.line2 && (
+					<span>{`${billingAddress?.line1} ${billingAddress?.line2}, `}</span>
+				))}
+			{billingAddress?.locality && <span>{`${billingAddress?.locality}, `}</span>}
+			{billingAddress?.region && <span>{`${billingAddress?.region}, `}</span>}
+			{billingAddress?.postal && <span>{`${billingAddress?.postal}, `}</span>}
+			{billingAddress?.country && <span>{billingAddress?.country}</span>}
+		</Paragraph>
+	);
+};
+
+export const SummaryPayment = ({paymentDetails}) => {
+	const phrases = usePhrases();
+	return (
+		<Paragraph>
+			{paymentDetails?.card?.brand && (
+				<PaymentIcon type={paymentDetails?.card?.brand?.toLowerCase()} />
+			)}
+			{paymentDetails?.card?.brand && <span> {paymentDetails?.card?.brand}</span>}
+			{paymentDetails?.card?.last4 && (
+				<span>
+					{" "}
+					<strong>{"\u2219".repeat(4)}</strong> {paymentDetails?.card?.last4}
+				</span>
+			)}
+			{paymentDetails?.card?.exp_month && paymentDetails?.card?.exp_year && (
+				<span>
+					{" "}
+					{phrases.t("checkout-block.expires")} {paymentDetails?.card?.exp_month}
+					{"/"}
+					{paymentDetails?.card?.exp_year}
+				</span>
+			)}
+		</Paragraph>
+	);
+};
+
--- blocks/subscriptions-block/components/CheckoutCardDetail/index.jsx
+++ blocks/subscriptions-block/components/CheckoutCardDetail/index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { usePhrases, Heading, Stack, Divider, Paragraph } from "@wpmedia/arc-themes-components";
 import PropTypes from "@arc-fusion/prop-types";
+import { SummaryAccount, SummaryBillingAddress, SummaryPayment } from "./Summary";
 
 export const ACCOUNT = "Account";
 export const BILLING_ADDRESS = "Billing Address";
 export const PAYMENT = "Payment";
 export const REVIEW = "Review";
 
-const CheckoutCardDetail = ({ type, summary, children, link, className, isOpen, isComplete }) => {
+const CheckoutCardDetail = ({
+	type,
+	summary,
+	children,
+	link,
+	className,
+	isOpen,
+	isComplete,
+	error,
+}) => {
 	const phrases = usePhrases();
 
 	const getTitle = () => {
@@ -16,18 +26,42 @@ const CheckoutCardDetail = ({ type, summary, children, link, className, isOpen, 
 		if (type === PAYMENT) return `3. ${phrases.t("checkout-block.payment")}`;
 		if (type === REVIEW) return `4. ${phrases.t("checkout-block.review")}`;
 		return null;
-	}
+	};
+
+	const Summary = () => {
+		if (type === ACCOUNT) {
+			return <SummaryAccount account = {summary}/>
+		}
+		if (type === BILLING_ADDRESS) {
+			return <SummaryBillingAddress billingAddress = {summary}/>
+		}
+		if (type === PAYMENT) {
+			return <SummaryPayment paymentDetails={summary} />
+		}
+		return null;
+	};
 
 	return (
 		<div>
 			<Divider />
-			<Stack className={isOpen ? className : `${className}-closed`} direction="horizontal" gap="16px">
+			<Stack
+				className={isOpen ? className : `${className}-closed`}
+				direction="horizontal"
+				gap="16px"
+			>
 				<Heading>{getTitle()}</Heading>
-				{!isOpen && isComplete && <div className={`${className}-summary`}>
-					<Paragraph>{summary}</Paragraph>
-					{link}
-				</div>}
+				{!isOpen && isComplete && (
+					<div className={`${className}-summary`}>
+						<Summary />
+						{link}
+					</div>
+				)}
 			</Stack>
+			{isOpen && error && (
+				<div className={`${className}__form-error`}>
+					<Paragraph>{error?.message}</Paragraph>
+				</div>
+			)}
 			{isOpen && <div className={`${className}-children-div`}>{children}</div>}
 		</div>
 	);
@@ -35,11 +69,11 @@ const CheckoutCardDetail = ({ type, summary, children, link, className, isOpen, 
 
 CheckoutCardDetail.propTypes = {
 	type: PropTypes.oneOf([ACCOUNT, BILLING_ADDRESS, PAYMENT, REVIEW]).isRequired,
-	summary: PropTypes.string,
+	summary: PropTypes.object,
 	children: PropTypes.any,
 	link: PropTypes.any,
 	isOpen: PropTypes.boolean,
-	isComplete: PropTypes.boolean
-}
+	isComplete: PropTypes.boolean,
+};
 
 export default CheckoutCardDetail;

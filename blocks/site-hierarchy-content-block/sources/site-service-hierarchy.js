d2a9749f88d10beaa6925a09ffb8347d1e3556a9
Nick Schubach <nschubach@gmail.com>
2024-04-26 15:36:51 -0400

    THEMES-1900: Fix logic for detecting 404 for sectionId/_id

--- blocks/site-hierarchy-content-block/sources/site-service-hierarchy.js
+++ blocks/site-hierarchy-content-block/sources/site-service-hierarchy.js
@@ -1,27 +1,21 @@
 export default {
-	resolve(resolveParams) {
-		const { hierarchy, sectionId, "arc-site": arcSite } = resolveParams;
-		return `/site/v3/navigation/${arcSite}?${hierarchy ? `hierarchy=${hierarchy}` : ""}${
-			sectionId ? `&_id=${sectionId}` : ""
-		}`;
-	},
-	schemaName: "navigation-hierarchy",
-	params: {
-		hierarchy: "text",
-		sectionId: "text",
-	},
-	transform: (data, query) => {
-		let idMatch = false;
-		if (query.sectionId) {
-			idMatch = data._id !== query.sectionId;
-		}
-
-		if ((!query.hierarchy && idMatch) || (query.uri && idMatch)) {
-			const error = new Error("Not found");
-			error.statusCode = 404;
-			throw error;
-		}
-
-		return data;
-	},
+    resolve(resolveParams) {
+        const { hierarchy, sectionId, "arc-site": arcSite } = resolveParams;
+        return `/site/v3/navigation/${arcSite}?${hierarchy ? `hierarchy=${hierarchy}` : ""}${
+            sectionId ? `&_id=${sectionId}` : ""
+        }`;
+    },
+    schemaName: "navigation-hierarchy",
+    params: {
+        hierarchy: "text",
+        sectionId: "text",
+    },
+    transform: (data, query) => {
+        if (query?.sectionId === data._id) {
+            return data;
+        }
+        const error = new Error("Not found");
+        error.statusCode = 404;
+        throw error;
+    },
 };

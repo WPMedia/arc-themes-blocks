9b4cd5f76925e5b09e0d3fb950cb5af2a3ac66a9
Nick Schubach <nschubach@gmail.com>
2024-04-26 15:48:53 -0400

    fix format

--- blocks/site-hierarchy-content-block/sources/site-service-hierarchy.js
+++ blocks/site-hierarchy-content-block/sources/site-service-hierarchy.js
@@ -1,21 +1,21 @@
 export default {
-    resolve(resolveParams) {
-        const { hierarchy, sectionId, "arc-site": arcSite } = resolveParams;
-        return `/site/v3/navigation/${arcSite}?${hierarchy ? `hierarchy=${hierarchy}` : ""}${
-            sectionId ? `&_id=${sectionId}` : ""
-        }`;
-    },
-    schemaName: "navigation-hierarchy",
-    params: {
-        hierarchy: "text",
-        sectionId: "text",
-    },
-    transform: (data, query) => {
-        if (query?.sectionId === data._id) {
-            return data;
-        }
-        const error = new Error("Not found");
-        error.statusCode = 404;
-        throw error;
-    },
+	resolve(resolveParams) {
+		const { hierarchy, sectionId, "arc-site": arcSite } = resolveParams;
+		return `/site/v3/navigation/${arcSite}?${hierarchy ? `hierarchy=${hierarchy}` : ""}${
+			sectionId ? `&_id=${sectionId}` : ""
+		}`;
+	},
+	schemaName: "navigation-hierarchy",
+	params: {
+		hierarchy: "text",
+		sectionId: "text",
+	},
+	transform: (data, query) => {
+		if (query?.sectionId === data._id) {
+			return data;
+		}
+		const error = new Error("Not found");
+		error.statusCode = 404;
+		throw error;
+	},
 };

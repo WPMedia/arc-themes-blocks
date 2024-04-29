31ffcddb0e6e6eaa78cb0f9a1319069ab5891814
Bill Tran <bill@billjohntran.com>
2024-04-29 15:55:54 -0700

    remove act

--- blocks/identity-block/features/one-time-password/default.test.jsx
+++ blocks/identity-block/features/one-time-password/default.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor, act } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import OneTimePassword from "./default";
 import { useIdentity } from "@wpmedia/arc-themes-components";
 

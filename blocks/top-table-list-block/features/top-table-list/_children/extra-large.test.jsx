12caed2b962df8fb364b82096432ee40957c3485
Malavika Koppula <mkreddy1110@gmail.com>
2024-05-22 13:52:31 -0400

    Fixed tests

--- blocks/top-table-list-block/features/top-table-list/_children/extra-large.test.jsx
+++ blocks/top-table-list-block/features/top-table-list/_children/extra-large.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import mockData from "../../../mock-data";
 
 import ExtraLarge from "./extra-large";

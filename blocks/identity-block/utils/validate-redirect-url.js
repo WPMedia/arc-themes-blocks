faccb59ea46eeb4a738f1d9c6b86aaa324355c8b
Bill Tran <bill@billjohntran.com>
2024-06-14 15:24:38 -0700

    add fix for /pf bug

--- blocks/identity-block/utils/validate-redirect-url.js
+++ blocks/identity-block/utils/validate-redirect-url.js
@@ -6,7 +6,7 @@ const validateURL = (url) => {
 	if (valid) {
 		// handling preview with PB.
 		if (window.location.pathname.includes('/pf/')) {
-			return `${window.location.origin}/pf${url}`;
+			return `${window.location.origin}/pf${url.replace('/p/', '/')}`;
 		}
 
 		return `${window.location.origin}${url}`;
